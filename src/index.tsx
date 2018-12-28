4b9806b81a55b62f046d875ec6b0ed3e2899e9bc
Daniel Belohlavek <dbhvk.96@gmail.com>
2018-12-28 18:16:09 -0300

    chore: setup project (#9)

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router'
+import { store, history } from './store'
+import { Routes } from './Routes'
+import './index.css'
+
+console.log(ConnectedRouter)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Routes />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
