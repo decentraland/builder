1873664a25afec7caa459781751a960a3ed80cc0
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-01-02 16:03:15 -0300

    feat: add decentraland UI styles

---
+++ src/themes/index.js
@@ -0,0 +1,9 @@
+// Base
+import 'decentraland-ui/lib/styles.css'
+
+// Theme
+import 'decentraland-ui/lib/dark-theme.css'
+import './theme.css'
+
+// We can override Component styles themes here like this:
+// import './components/Button.css'
