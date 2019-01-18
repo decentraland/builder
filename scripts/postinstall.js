005f2ef2661eabebbe06fa5fb081b0ceb3967e4e
Daniel Belohlavek <dbhvk.96@gmail.com>
2019-01-18 16:23:11 -0300

    feat: cleanup editor mode (#55)
    * feat: cleanup client
    
    * chore: metrics
    
    * chore: modified ecs bundle
    
    * chore: metrics shape

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
-const artifactPath = process.argv[2] || '../node_modules/decentraland-ecs/artifacts/editor.js'
+const editorPath = process.argv[2] || '../node_modules/decentraland-ecs/artifacts/editor.js'
+const ecsPath = process.argv[3] || '../node_modules/decentraland-ecs/dist/src/index.js'
 
-fs.copyFileSync(path.resolve(__dirname, artifactPath), path.resolve(__dirname, '../public/editor.js'))
+fs.copyFileSync(path.resolve(__dirname, editorPath), path.resolve(__dirname, '../public/editor.js'))
+fs.copyFileSync(path.resolve(__dirname, ecsPath), path.resolve(__dirname, '../src/modules/editor/ecs.txt'))
