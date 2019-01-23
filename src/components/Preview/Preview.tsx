5791524231bc2f97669597c0b5389178014eb6c2
belohlavek <dani@decentraland.org>
2019-01-23 17:06:39 -0300

    chore: env var

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react'
+import { env } from 'decentraland-commons'
+
 import { EditorWindow } from './Preview.types'
 import './Preview.css'
 
 const editorWindow = window as EditorWindow
 
-const CONTENT_SERVER = 'https://content.decentraland.today/contents/'
+const CONTENT_SERVER = env.get('REACT_APP_CONTENT_SERVER', () => {
+  throw new Error('Missing REACT_APP_CONTENT_SERVER env variable')
+})
 
 export default class Preview extends React.PureComponent {
   private ref = React.createRef<HTMLDivElement>()
@@ -24,7 +28,7 @@ export default class Preview extends React.PureComponent {
   moveCanvas = (canvas: HTMLCanvasElement) => {
     if (this.ref.current && canvas) {
       this.ref.current.appendChild(canvas)
-      editorWindow.editor.initEngine(CONTENT_SERVER)
+      editorWindow.editor.initEngine(CONTENT_SERVER as string)
       editorWindow.editor.resize()
     }
   }
