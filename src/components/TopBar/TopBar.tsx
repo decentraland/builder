e09a1e8d27da53a6bcf2ec021986eb696c80fdd9
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-01-24 12:18:10 +0100

    feat: go back button topbar

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
-import { Header, Grid } from 'decentraland-ui'
+import { Link } from 'react-router-dom'
+import { Header, Grid, Icon } from 'decentraland-ui'
 
+import { locations } from 'routing/locations'
 import './TopBar.css'
 
 export default class TopBar extends React.PureComponent {
@@ -8,7 +10,12 @@ export default class TopBar extends React.PureComponent {
     return (
       <Grid className="TopBar">
         <Grid.Column verticalAlign="middle">
-          <Header size="medium">Land title</Header>
+          <Header size="medium">
+            <Link className="text" to={locations.root()}>
+              <Icon name="chevron left" />
+            </Link>
+            LAND title
+          </Header>
         </Grid.Column>
       </Grid>
     )
