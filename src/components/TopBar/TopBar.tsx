01f4546cafea676944ea715724a29803c5e4e0b0
Nicolas Santangelo <santangelonicolas@gmail.com>
2019-01-28 20:01:14 +0100

    feat: add the current project title to topbar (#73)

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom'
 import { Header, Grid, Icon } from 'decentraland-ui'
 
 import { locations } from 'routing/locations'
+import { Props } from './TopBar.types'
 import './TopBar.css'
 
-export default class TopBar extends React.PureComponent {
+export default class TopBar extends React.PureComponent<Props> {
   render() {
+    const { currentProject } = this.props
     return (
       <Grid className="TopBar">
         <Grid.Column verticalAlign="middle">
@@ -14,7 +16,7 @@ export default class TopBar extends React.PureComponent {
             <Link className="text" to={locations.root()}>
               <Icon name="chevron left" />
             </Link>
-            LAND title
+            {currentProject ? currentProject.title : null}
           </Header>
         </Grid.Column>
       </Grid>
