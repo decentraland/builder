614581a03f36678be7b9d8e37851d61aefcd13d6
Juan Cazala <juan@decentraland.org>
2019-01-31 09:42:33 -0300

    fix: save project on update transform

--- src/modules/common/store.ts
+++ src/modules/common/store.ts
@@ -10,7 +10,7 @@ import { createStorageMiddleware } from 'decentraland-dapps/dist/modules/storage
 import { createRootReducer } from './reducer'
 import { rootSaga } from './sagas'
 import { scenarioMiddleware, eventEmitter } from 'scenarios/helpers/middleware'
-import { CREATE_SCENE, PROVISION_SCENE } from 'modules/scene/actions'
+import { CREATE_SCENE, PROVISION_SCENE, UPDATE_TRANSFORM } from 'modules/scene/actions'
 import { CREATE_PROJECT } from 'modules/project/actions'
 import { EDITOR_UNDO, EDITOR_REDO } from 'modules/editor/actions'
 
@@ -30,7 +30,7 @@ const loggerMiddleware = createLogger({
 const { storageMiddleware, loadStorageMiddleware } = createStorageMiddleware({
   storageKey: env.get('REACT_APP_LOCAL_STORAGE_KEY'),
   paths: ['project', ['scene', 'present']] as any,
-  actions: [CREATE_PROJECT, CREATE_SCENE, PROVISION_SCENE, EDITOR_UNDO, EDITOR_REDO]
+  actions: [CREATE_PROJECT, CREATE_SCENE, PROVISION_SCENE, EDITOR_UNDO, EDITOR_REDO, UPDATE_TRANSFORM]
 })
 const middlewares = [historyMiddleware, storageMiddleware, sagasMiddleware, loggerMiddleware]
 
