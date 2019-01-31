de24db3ec754ebfaf7e66de3c8438550bb8960a7
Juan Cazala <juancazala@gmail.com>
2019-01-31 13:07:41 -0300

    feat: bump ECS (#98)
    * feat: bump ecs
    
    * chore: lock version

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
 const path = require('path')
 
-const editorPath = process.argv[2] || '../node_modules/decentraland-ecs/artifacts/editor.js'
+const editorPath = path.resolve(__dirname, process.argv[2] || require.resolve('decentraland-ecs/artifacts/editor'))
+const targetPath = path.resolve(__dirname, '../public/editor.js')
 
-fs.copyFileSync(path.resolve(__dirname, editorPath), path.resolve(__dirname, '../public/editor.js'))
+console.log(`> Copying file ${editorPath} into ${targetPath}`)
+
+fs.copyFileSync(editorPath, targetPath)
