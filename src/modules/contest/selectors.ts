1359cbe9554d1ba5f98668c34938273af0e5fbdc
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-02-08 16:36:32 +0100

    fix: selector order

--- src/modules/contest/selectors.ts
+++ src/modules/contest/selectors.ts
@@ -6,9 +6,10 @@ export const getState: (state: RootState) => ContestState = state => state.conte
 
 export const hasAcceptedTerms: (state: RootState) => ContestState['hasAcceptedTerms'] = state => getState(state).hasAcceptedTerms
 
+export const hasSubmittedProject: (state: RootState, projectId: string) => boolean = (state, projectId) =>
+  !!getState(state).projects[projectId]
+
 export const hasSubmittedCurrentProject: (state: RootState) => boolean = state => {
   const currentProject = getCurrentProject(state)
   return currentProject ? hasSubmittedProject(state, currentProject.id) : false
 }
-export const hasSubmittedProject: (state: RootState, projectId: string) => boolean = (state, projectId) =>
-  !!getState(state).projects[projectId]
