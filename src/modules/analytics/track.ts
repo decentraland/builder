bc9f1951efb31bca2eb87887b61951e83b6076e0
Juan Cazala <juancazala@gmail.com>
2019-02-15 12:04:11 -0300

    fix: segment (#176)
    Closes #175

--- src/modules/analytics/track.ts
+++ src/modules/analytics/track.ts
@@ -1,6 +1,25 @@
 import { add } from 'decentraland-dapps/dist/modules/analytics/utils'
 import { SUBMIT_PROJECT_SUCCESS, SubmitProjectSuccessAction } from 'modules/contest/actions'
+import { ADD_ITEM, DROP_ITEM, RESET_ITEM, DUPLICATE_ITEM, DELETE_ITEM, SET_GROUND } from 'modules/scene/actions'
+import {
+  EDITOR_UNDO,
+  EDITOR_REDO,
+  TOGGLE_PREVIEW,
+  TOGGLE_SIDEBAR,
+  SET_GIZMO,
+  ZOOM_IN,
+  ZOOM_OUT,
+  RESET_CAMERA
+} from 'modules/editor/actions'
+import { SEARCH_ASSETS } from 'modules/ui/sidebar/actions'
+import { OPEN_MODAL } from 'modules/modal/actions'
+import { CREATE_PROJECT, CreateProjectAction } from 'modules/project/actions'
 
+function addPayload(actionType: string) {
+  add(actionType, actionType, (action: any) => action.payload)
+}
+
+// contest actions
 add(SUBMIT_PROJECT_SUCCESS, 'Contest data', action => {
   const payload = (action as SubmitProjectSuccessAction).payload
   return {
@@ -9,3 +28,26 @@ add(SUBMIT_PROJECT_SUCCESS, 'Contest data', action => {
     ethAddress: payload.contest.ethAddress
   }
 })
+
+// item actions
+addPayload(ADD_ITEM)
+addPayload(DROP_ITEM)
+addPayload(RESET_ITEM)
+addPayload(DUPLICATE_ITEM)
+addPayload(DELETE_ITEM)
+
+// editor actions
+add(CREATE_PROJECT, CREATE_PROJECT, action => (action as CreateProjectAction).payload.project.parcelLayout)
+addPayload(EDITOR_UNDO)
+addPayload(EDITOR_REDO)
+addPayload(TOGGLE_PREVIEW)
+addPayload(TOGGLE_SIDEBAR)
+addPayload(SEARCH_ASSETS)
+addPayload(OPEN_MODAL)
+addPayload(SET_GIZMO)
+addPayload(SET_GROUND)
+
+// camera actions
+addPayload(ZOOM_IN)
+addPayload(ZOOM_OUT)
+addPayload(RESET_CAMERA)
