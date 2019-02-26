b9df11fe002349284935b87b6fdc4abe2a442d5b
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-02-26 17:36:45 +0100

    feat: only close modals if theyre open

--- src/modules/modal/sagas.ts
+++ src/modules/modal/sagas.ts
@@ -1,6 +1,18 @@
+import { getOpenModals } from 'decentraland-dapps/dist/modules/modal/selectors'
+
 import { history, store } from 'modules/common/store'
 import { closeAllModals } from './actions'
 
 export function modalSaga() {
-  history.listen(() => store.dispatch(closeAllModals()))
+  // Can't use generators inside the history listen callback
+  history.listen(() => handleURLChange())
+}
+
+function handleURLChange() {
+  const openModals = getOpenModals(store.getState())
+
+  if (Object.keys(openModals).length > 0) {
+    // Move the dispatch to the end of the queue to avoid conflicting with other running sagas
+    setTimeout(() => store.dispatch(closeAllModals()))
+  }
 }
