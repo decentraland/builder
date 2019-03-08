e31dd3c3dbdda9ea78433e64a995b1ac6d15a188
Trevor Waldorf <trevor.automated@gmail.com>
2019-03-08 15:39:03 -0500

    Update copy with revisions and remove unused copy from views

--- src/components/MobilePage/MobilePage.tsx
+++ src/components/MobilePage/MobilePage.tsx
@@ -48,7 +48,6 @@ export default class MobilePage extends React.PureComponent<Props, State> {
     return (
       <div className="MobilePage">
         <Header size="large">{t('mobile_page.title')}</Header>
-        <p className="subtitle">{t('mobile_page.subtitle')}</p>
         <Form onSubmit={this.handleSubmit}>
           <p className="message">{t('mobile_page.message')}</p>
 
@@ -71,9 +70,6 @@ export default class MobilePage extends React.PureComponent<Props, State> {
             <div className="success">{t('mobile_page.success')}</div>
           )}
         </Form>
-        <span className="suggestion">
-          Participate in our contest for a chance to <a href="http://contest.decentraland.org">earn MANA and LAND</a>
-        </span>
       </div>
     )
   }
