b56372927e117eae5aa6a559e46a8b647f671957
Daniel Belohlavek <dbhvk.96@gmail.com>
2019-03-18 10:23:08 -0300

    chore: categories env (#356)

--- src/modules/ui/sidebar/utils.ts
+++ src/modules/ui/sidebar/utils.ts
@@ -3,6 +3,7 @@ import { DataByKey } from 'decentraland-dapps/dist/lib/types'
 import { Asset, GROUND_CATEGORY } from 'modules/asset/types'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { AssetPack } from 'modules/assetPack/types'
+import { CONTENT_SERVER } from 'modules/editor/utils'
 
 export const EMPTY_GROUND_ASSET_ID = 'EMPTY_GROUND_ASSET_ID'
 export const EMPTY_GROUND_ASSET_PACK_ID = 'EMPTY_GROUND_ASSET_PACK_ID'
@@ -39,26 +40,26 @@ export function addEmptyGroundAssetPack(assetPacks: DataByKey<AssetPack>) {
 export const SIDEBAR_CATEGORIES: { name: string; thumbnail: string }[] = [
   {
     name: GROUND_CATEGORY,
-    thumbnail: 'https://content.decentraland.today/contents/QmUCDv34krVLGENkoRBuqATPwgrd6xF2k5NXLgeoNG6qW6'
+    thumbnail: `${CONTENT_SERVER}QmUCDv34krVLGENkoRBuqATPwgrd6xF2k5NXLgeoNG6qW6`
   },
   {
     name: 'tiles',
-    thumbnail: 'https://content.decentraland.today/contents/QmebeSZq8QxGZBgBgRQnm3JV94mRtt7zqUy28hzTc3ZuDH'
+    thumbnail: `${CONTENT_SERVER}QmebeSZq8QxGZBgBgRQnm3JV94mRtt7zqUy28hzTc3ZuDH`
   },
   {
     name: 'nature',
-    thumbnail: 'https://content.decentraland.today/contents/QmYFWdh5yJnrg4VuXPniDd8HHbVrfV2FPn35RPdVYVMLjg'
+    thumbnail: `${CONTENT_SERVER}QmYFWdh5yJnrg4VuXPniDd8HHbVrfV2FPn35RPdVYVMLjg`
   },
   {
     name: 'structures',
-    thumbnail: 'https://content.decentraland.today/contents/Qme15L6oTnjfAuCs9ayTDPNEBhPRJukcLVFDNw43UXUaLA'
+    thumbnail: `${CONTENT_SERVER}Qme15L6oTnjfAuCs9ayTDPNEBhPRJukcLVFDNw43UXUaLA`
   },
   {
     name: 'decorations',
-    thumbnail: 'https://content.decentraland.today/contents/Qmd7N96rupxCLr3mWwRfgsM6Pwnf7k2SUemvxTjYvSddgN'
+    thumbnail: `${CONTENT_SERVER}Qmd7N96rupxCLr3mWwRfgsM6Pwnf7k2SUemvxTjYvSddgN`
   },
   {
     name: 'year of the pig',
-    thumbnail: 'https://content.decentraland.today/contents/QmeHgdLkKaRLpFKPECUxQTEb3NEHbcKkgCcjhBq693d62n'
+    thumbnail: `${CONTENT_SERVER}QmeHgdLkKaRLpFKPECUxQTEb3NEHbcKkgCcjhBq693d62n`
   }
 ]
