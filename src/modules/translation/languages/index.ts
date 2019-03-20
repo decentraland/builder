e2f5376e7283f8a7256679dc8f8a55a8b69b731b
Nicolas Santangelo <santangelonicolas@gmail.com>
2019-03-20 23:23:32 +0100

    feat: kr i18n (#381)
    * feat: add korean i18n
    
    * feat: 😅

--- src/modules/translation/languages/index.ts
+++ src/modules/translation/languages/index.ts
@@ -1,5 +1,6 @@
 const en = require('./en.json')
 const es = require('./es.json')
+const ko = require('./ko.json')
 const zh = require('./zh.json')
 
-export { en, es, zh }
+export { en, es, ko, zh }
