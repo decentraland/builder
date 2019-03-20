ddc0192a825d23c9e660845c4dc9c2ae57fe5445
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-03-20 23:20:10 +0100

    feat: 😅

--- src/modules/translation/languages/index.ts
+++ src/modules/translation/languages/index.ts
@@ -1,6 +1,6 @@
 const en = require('./en.json')
 const es = require('./es.json')
-const es = require('./ko.json')
+const ko = require('./ko.json')
 const zh = require('./zh.json')
 
 export { en, es, ko, zh }
