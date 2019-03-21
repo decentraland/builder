941833dda939a967c0af7af80b2b0a50694366e6
Juan Cazala <juan@decentraland.org>
2019-03-21 14:07:14 -0300

    chore: remove unsued file

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -6,7 +6,6 @@ import 'decentraland-ui/lib/dark-theme.css'
 
 // Override third party Component styles here:
 import './components/Grid.css'
-import './components/Input.css'
 
 // Custom theme
 import './theme.css'
