481a9aa51c7af57a14aac1c9c316520ecb02ba10
belohlavek <dani@decentraland.org>
2019-03-25 15:38:33 -0300

    chore: hero markup

--- src/components/HomePageHero/HomePageHero.tsx
+++ src/components/HomePageHero/HomePageHero.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import { Hero, Header, Button } from 'decentraland-ui'
-
+import { Hero, Button } from 'decentraland-ui'
 import { t, T } from 'decentraland-dapps/dist/modules/translation/utils'
+
 import { Props } from './HomePageHero.types'
 import './HomePageHero.css'
 
@@ -18,29 +18,32 @@ export default class HomePageHero extends React.PureComponent<Props> {
     return (
       <Hero height={472} className="HomePageHero">
         <div className="container">
-          <div className="background">
-            <div className="message">
-              <Header size="huge">{t('home_page.title')}</Header>
-              <div className="subtitle">
-                <T
-                  id="home_page.subtitle"
-                  values={{
-                    mana: <span className="highlight">{t('contest.mana')}</span>,
-                    land: <span className="highlight">{t('contest.land')}</span>,
-                    usd: <span className="highlight">{t('contest.usd')}</span>,
-                    mana_per_scene: <span className="highlight">{t('contest.mana_per_scene')}</span>
-                  }}
-                />
-              </div>
-              <Button primary onClick={this.handleStart}>
-                Start Building
-              </Button>
-              <Button className="hollow" onClick={this.handleWatchVideo}>
-                Watch Tutorial
-              </Button>
-            </div>
-          </div>
+          <Hero.Header>{t('home_page.title')}</Hero.Header>
+          <Hero.Description>
+            <span className="description">
+              <T
+                id="home_page.subtitle"
+                values={{
+                  mana: <span className="highlight">{t('contest.mana')}</span>,
+                  land: <span className="highlight">{t('contest.land')}</span>,
+                  usd: <span className="highlight">{t('contest.usd')}</span>,
+                  mana_per_scene: <span className="highlight">{t('contest.mana_per_scene')}</span>
+                }}
+              />
+            </span>
+          </Hero.Description>
+          <Hero.Actions>
+            <Button primary onClick={this.handleStart}>
+              Start Building
+            </Button>
+            <Button className="hollow" onClick={this.handleWatchVideo}>
+              Watch Tutorial
+            </Button>
+          </Hero.Actions>
         </div>
+        <Hero.Content>
+          <div className="background" />
+        </Hero.Content>
       </Hero>
     )
   }
