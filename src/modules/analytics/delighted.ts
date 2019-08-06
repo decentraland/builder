4afba80cd5da90a8163fd303280a8f14d0b240a2
Ariel Barmat <abarmat@decentraland.org>
2019-08-05 22:05:09 -0300

    feat: add feedback tool (#556)
    * feat: add feedback tool
    
    * chore: add initial delay
    
    * chore: change survey init
    
    * chore: update survey start time
    
    * feat: add tracking info to survey
    
    * feat: added saga
    
    * chore: update survey time

---
+++ src/modules/analytics/delighted.ts
@@ -0,0 +1,36 @@
+import { delay, call } from 'redux-saga/effects'
+import { env } from 'decentraland-commons'
+import { getAnalytics } from 'decentraland-dapps/dist/modules/analytics/utils'
+
+const DELIGHTED_DELAY = 180 * 1000
+const DELIGHTED_API_KEY = env.get('REACT_APP_DELIGHTED_API_KEY', '')
+const isEnabled = !!DELIGHTED_API_KEY
+
+const init = (window as any).initDelighted
+
+export function* handleDelighted() {
+  if (!isEnabled) {
+    console.warn('Delighted is disabled because DELIGHTED_API_KEY was not found.')
+    return
+  }
+  yield call(() => {
+    init(
+      window,
+      document,
+      ['survey', 'reset', 'config', 'init', 'set', 'get', 'event', 'identify', 'track', 'page', 'screen', 'group', 'alias'],
+      DELIGHTED_API_KEY,
+      'delighted'
+    )
+  })
+  yield delay(DELIGHTED_DELAY)
+  yield call(() => {
+    const analytics = getAnalytics()
+    const delighted = (window as any).delighted
+    delighted.survey({
+      recurringPeriod: false,
+      properties: {
+        anonymous_id: analytics ? analytics.user().anonymousId() : null
+      }
+    })
+  })
+}
