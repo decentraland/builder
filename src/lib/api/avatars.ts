0896ba66a8c18a7ecdf478717768feae92449410
Juan Cazala <juan@decentraland.org>
2019-08-08 09:04:01 -0300

    fix: fetch profile on login (#581)

--- src/lib/api/avatars.ts
+++ src/lib/api/avatars.ts
@@ -1,13 +1,13 @@
 import { env } from 'decentraland-commons'
 import { BaseAPI } from 'decentraland-dapps/dist/lib/api'
-import { authorize } from './auth'
 import { User } from 'modules/auth/types'
+import { authorize } from './auth'
 
 export const AVATARS_API_URL = env.get('REACT_APP_AVATARS_API_URL', '')
 
 export class AvatarsAPI extends BaseAPI {
-  fetchUser = async () => {
-    const user: User = await this.request('get', `/profile`, null, authorize())
+  fetchUser = async (accessToken?: string) => {
+    const user: User = await this.request('get', `/profile`, null, authorize(accessToken))
     return user
   }
 }
