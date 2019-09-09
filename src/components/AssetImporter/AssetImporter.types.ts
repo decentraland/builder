e7a7050b1a0785a32e8a0e98e9417131d79dc135
belohlavek <dani@decentraland.org>
2019-09-09 12:56:08 -0300

    chore: requested changes

--- src/components/AssetImporter/AssetImporter.types.ts
+++ src/components/AssetImporter/AssetImporter.types.ts
@@ -2,13 +2,12 @@ import { RawAssetPack } from 'modules/assetPack/types'
 import { RawAsset } from 'modules/asset/types'
 
 export type Props = {
-  onAssetPack(assetPack: RawAssetPack): void
+  onSubmit(assetPack: RawAssetPack): void
 }
 
 export type State = {
   assetPackId: string
   files: Record<string, ImportedFile>
-  canImport: boolean
 }
 
 export type ImportedFile = {
--- src/components/Modals/CreateAssetPackModal/CreateAssetPackModal.tsx
+++ src/components/Modals/CreateAssetPackModal/CreateAssetPackModal.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
-import { Close, Button } from 'decentraland-ui'
+import { Close } from 'decentraland-ui'
 import { ModalProps } from 'decentraland-dapps/dist/providers/ModalProvider/ModalProvider.types'
-import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import Modal from 'decentraland-dapps/dist/containers/Modal'
 import AssetImport from 'components/AssetImporter'
 import { RawAssetPack } from 'modules/assetPack/types'
@@ -28,24 +27,7 @@ export default class CreateAssetPackModal extends React.PureComponent<ModalProps
   }
 
   renderAssetImport = () => {
-    const { assetPack } = this.state
-    const buttonText =
-      assetPack && assetPack.assets && assetPack.assets.length > 1
-        ? t('asset_pack.import.action_many', { count: assetPack.assets.length })
-        : t('asset_pack.import.action')
-
-    const buttonDisabled = !assetPack || !assetPack.assets.length
-
-    return (
-      <>
-        <AssetImport onAssetPack={this.handleAssetPackChange} />
-        <div className="actions">
-          <Button disabled={buttonDisabled} primary={!buttonDisabled}>
-            {buttonText}
-          </Button>
-        </div>
-      </>
-    )
+    return <AssetImport onSubmit={this.handleAssetPackChange} />
   }
 
   render() {
