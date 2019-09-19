adbced3b1cecf138e811b1609f2334851b3cfd05
Fede <i@2fd.me>
2019-09-19 10:39:54 -0300

    docs: change banner name and format file

--- src/components/HomePage/TopBanner/TopBanner.tsx
+++ src/components/HomePage/TopBanner/TopBanner.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 import Banner from 'components/Banner'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 
-import "./TopBanner.css"
+import './TopBanner.css'
 
 export default class TopBanner extends React.PureComponent {
-
   render() {
-    return <Banner className="TopBanner" isClosable={true} name="dcl-top-banner">
-      <p>
-      <span>{t('banners.top_banner_left')}</span>
-      <a href="https://hack.decentraland.org/">{' '}{t('banners.top_banner_action')}{' '}</a>
-      <span>{t('banners.top_banner_right')}</span>
-      </p>
-    </Banner>
+    return (
+      <Banner className="TopBanner" isClosable={true} name="dcl-hackathon-banner">
+        <p>
+          <span>{t('banners.top_banner_left')}</span>
+          <a href="https://hack.decentraland.org/"> {t('banners.top_banner_action')} </a>
+          <span>{t('banners.top_banner_right')}</span>
+        </p>
+      </Banner>
+    )
   }
 }
