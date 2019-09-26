fdcad778e7d928d91b3cd54a9e06f784109aff16
Juan Cazala <juan@decentraland.org>
2019-09-26 16:05:33 -0300

    feat: added default asset pack name (#726)
    * feat: added default asset pack name;
    
    * chore: translation

--- src/components/Modals/CreateAssetPackModal/CreateAssetPackModal.types.ts
+++ src/components/Modals/CreateAssetPackModal/CreateAssetPackModal.types.ts
@@ -1,6 +1,6 @@
 import { Dispatch } from 'redux'
 import { ModalProps } from 'decentraland-dapps/dist/providers/ModalProvider/ModalProvider.types'
-import { RawAssetPack } from 'modules/assetPack/types'
+import { RawAssetPack, AssetPack } from 'modules/assetPack/types'
 import { LoginAction, login } from 'modules/auth/actions'
 import { saveAssetPackRequest, SaveAssetPackRequestAction } from 'modules/assetPack/actions'
 import { AssetPackState } from 'modules/assetPack/reducer'
@@ -26,12 +26,13 @@ export type Props = ModalProps & {
   project: Project | null
   progress: AssetPackState['progress']
   error: AssetPackState['error']
+  assetPacks: AssetPack[]
   isLoggedIn: boolean
   isLoading: boolean
   onCreateAssetPack: typeof saveAssetPackRequest
   onLogin: typeof login
 }
 
-export type MapStateProps = Pick<Props, 'project' | 'progress' | 'error' | 'isLoggedIn' | 'isLoading'>
+export type MapStateProps = Pick<Props, 'project' | 'progress' | 'error' | 'assetPacks' | 'isLoggedIn' | 'isLoading'>
 export type MapDispatchProps = Pick<Props, 'onCreateAssetPack' | 'onLogin'>
 export type MapDispatch = Dispatch<SaveAssetPackRequestAction | LoginAction>
