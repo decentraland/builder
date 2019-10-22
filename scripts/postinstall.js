7ee693736e0b54b51e9f172437a7a821a22593c4
Juan Cazala <juan@decentraland.org>
2019-10-22 14:04:02 -0300

    fix: deploy to LAND

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,9 +1,24 @@
 const fs = require('fs')
 const path = require('path')
+const prettier = require('prettier')
 
 const editorPath = path.resolve(__dirname, process.argv[2] || require.resolve('decentraland-ecs/artifacts/editor'))
-const targetPath = path.resolve(__dirname, '../public/editor.js')
+const targetEditorPath = path.resolve(__dirname, '../public/editor.js')
 
-console.log(`> Copying file ${editorPath} into ${targetPath}`)
+const ecsPath = path.resolve(__dirname, process.argv[2] || require.resolve('decentraland-ecs/dist/src/index.js'))
+const targetEcsPath = path.resolve(__dirname, '../src/ecsScene/ecs.js.raw')
 
-fs.copyFileSync(editorPath, targetPath)
+console.log(`> Copying file ${editorPath} into ${targetEditorPath}`)
+fs.copyFileSync(editorPath, targetEditorPath)
+
+console.log(`> Reading file ${ecsPath}`)
+const ecs = fs.readFileSync(ecsPath, 'utf8')
+console.log(`Prettifying file...`)
+const pretty = prettier.format(ecs, {
+  parser: 'babel',
+  semi: false,
+  singleQuote: true,
+  printWidth: 140
+})
+console.log(`Saving file to ${targetEcsPath}`)
+fs.writeFileSync(targetEcsPath, pretty, { encoding: 'utf8' })
