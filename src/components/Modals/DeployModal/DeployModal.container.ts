25ce11ffe4db828da3cb88790f1f3d38b246a793
Fede <i@2fd.me>
2019-12-03 19:29:34 -0300

    fix: skip pool deploy when a group is active

--- src/components/Modals/DeployModal/DeployModal.container.ts
+++ src/components/Modals/DeployModal/DeployModal.container.ts
@@ -2,11 +2,13 @@ import { Dispatch } from 'redux'
 import { connect } from 'react-redux'
 import { RootState } from 'modules/common/types'
 import { getData as getDeployments } from 'modules/deployment/selectors'
+import { getActivePoolGroup } from 'modules/poolGroup/selectors'
 import { MapStateProps, MapDispatchProps, OwnProps } from './DeployModal.types'
 import DeployModal from './DeployModal'
 
 const mapState = (state: RootState, ownProps: OwnProps): MapStateProps => ({
-  deployment: getDeployments(state)[ownProps.metadata.projectId]
+  deployment: getDeployments(state)[ownProps.metadata.projectId],
+  currentPoolGroup: getActivePoolGroup(state)
 })
 
 const mapDispatch = (_: Dispatch): MapDispatchProps => ({})
