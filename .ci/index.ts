6abbf523a2da176b34b18e0de3c6a4bdf18b6630
Ignacio Mazzara <nachomazzara@gmail.com>
2020-06-16 12:12:56 -0300

    feat: add pulumi files

---
+++ .ci/index.ts
@@ -0,0 +1,18 @@
+import { env, envTLD } from "dcl-ops-lib/domain";
+import { buildStatic } from "dcl-ops-lib/buildStatic";
+import { globalConfig } from "dcl-ops-lib/values";
+
+const { defaultSecurityGroupName } = globalConfig[env]
+
+async function main() {
+  const builder = buildStatic({
+    domain: `builder.decentraland.${envTLD}`,
+    defaultPath: 'index.html'
+  });
+
+  return {
+    cloudfrontDistribution: builder.cloudfrontDistribution,
+    bucketName: builder.contentBucket,
+  };
+}
+export = main;
