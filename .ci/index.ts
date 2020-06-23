bbd3803ab9a9015024428e831de00b09525310a7
Esteban Ordano <esteban@decentraland.org>
2020-06-23 15:03:02 -0300

    fix: additional domain .org (#996)

--- .ci/index.ts
+++ .ci/index.ts
@@ -1,18 +1,16 @@
-import { env, envTLD } from "dcl-ops-lib/domain";
-import { buildStatic } from "dcl-ops-lib/buildStatic";
-import { globalConfig } from "dcl-ops-lib/values";
-
-const { defaultSecurityGroupName } = globalConfig[env]
+import { env, envTLD } from 'dcl-ops-lib/domain'
+import { buildStatic } from 'dcl-ops-lib/buildStatic'
 
 async function main() {
   const builder = buildStatic({
     domain: `builder.decentraland.${envTLD}`,
-    defaultPath: 'index.html'
-  });
+    defaultPath: 'index.html',
+    additionalDomains: env === 'prd' ? ['builder.decentraland.org'] : [],
+  })
 
   return {
     cloudfrontDistribution: builder.cloudfrontDistribution,
     bucketName: builder.contentBucket,
-  };
+  }
 }
-export = main;
+export = main
