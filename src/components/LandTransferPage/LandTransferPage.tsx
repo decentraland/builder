6aa58617ecc3ffe64621500f27b24246c185d4a8
Juan Cazala <juan@decentraland.org>
2020-07-22 18:30:13 -0300

    feat: prevent sending to owner

--- src/components/LandTransferPage/LandTransferPage.tsx
+++ src/components/LandTransferPage/LandTransferPage.tsx
@@ -7,7 +7,7 @@ import { Props, State } from './LandTransferPage.types'
 import { T, t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { RoleType } from 'modules/land/types'
 import { locations } from 'routing/locations'
-import { isValid } from 'lib/address'
+import { isValid, isEqual } from 'lib/address'
 import './LandTransferPage.css'
 
 export default class LandTransferPage extends React.PureComponent<Props, State> {
@@ -25,30 +25,51 @@ export default class LandTransferPage extends React.PureComponent<Props, State> 
     const { address, isValid } = this.state
     return (
       <LandProviderPage className="LandTransferPage">
-        {land => (
-          <LandAction
-            land={land}
-            title={t('transfer_page.title')}
-            subtitle={<T id="transfer_page.subtitle" values={{ name: <strong>{land.name}</strong> }} />}
-          >
-            <Form onSubmit={() => onTransfer(land, address)}>
-              <Field label="Address" type="address" value={address} onChange={this.handleChange} placeholder="0x..." error={!isValid} />
-              <Section className="disclaimer">
-                <p className="danger-text">
-                  <T id="transfer_page.disclaimer" values={{ br: <br /> }} />
-                </p>
-              </Section>
-              <Row>
-                <Button type="submit" primary disabled={!address || !isValid || land.role !== RoleType.OWNER}>
-                  {t('global.submit')}
-                </Button>
-                <Link className="cancel" to={locations.landDetail(land.id)}>
-                  <Button>{t('global.cancel')}</Button>
-                </Link>
-              </Row>
-            </Form>
-          </LandAction>
-        )}
+        {land => {
+          let message
+          const isOwner = isEqual(land.owner, address)
+          if (isOwner) {
+            message = t('transfer_page.same_owner')
+          } else if (!isValid) {
+            message = t('transfer_page.invalid_address')
+          }
+          return (
+            <LandAction
+              land={land}
+              title={t('transfer_page.title')}
+              subtitle={<T id="transfer_page.subtitle" values={{ name: <strong>{land.name}</strong> }} />}
+            >
+              <Form onSubmit={() => onTransfer(land, address)}>
+                <Field
+                  label="Address"
+                  type="address"
+                  value={address}
+                  onChange={this.handleChange}
+                  placeholder="0x..."
+                  error={!isValid || isOwner}
+                  message={message}
+                />
+                <Section className="disclaimer">
+                  <p className="danger-text">
+                    <T id="transfer_page.disclaimer" values={{ br: <br /> }} />
+                  </p>
+                </Section>
+                <Row>
+                  <Button
+                    type="submit"
+                    primary
+                    disabled={!address || !isValid || land.role !== RoleType.OWNER || land.owner === address.toLowerCase()}
+                  >
+                    {t('global.submit')}
+                  </Button>
+                  <Link className="cancel" to={locations.landDetail(land.id)}>
+                    <Button>{t('global.cancel')}</Button>
+                  </Link>
+                </Row>
+              </Form>
+            </LandAction>
+          )
+        }}
       </LandProviderPage>
     )
   }
