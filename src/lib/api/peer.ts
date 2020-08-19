630f70fbb9d7737ffb2a06802e100da94973df32
Juan Cazala <juan@decentraland.org>
2020-08-19 16:00:09 -0300

    fix: cache profile requests

--- src/lib/api/peer.ts
+++ src/lib/api/peer.ts
@@ -6,6 +6,8 @@ import { Profile } from 'modules/profile/types'
 export const PEER_URL = env.get('REACT_APP_PEER_URL', '')
 
 export class PeerAPI extends BaseAPI {
+  cache: Record<string, Promise<Profile>> = {}
+
   fetchScene = async (x: number, y: number) => {
     const req = await fetch(`${this.url}/content/entities/scene?pointer=${x},${y}`)
     const res = await req.json()
@@ -13,9 +15,12 @@ export class PeerAPI extends BaseAPI {
   }
 
   fetchProfile = async (address: string) => {
-    const req = await fetch(`${this.url}/lambdas/profile/${address.toLowerCase()}`)
-    const res = await req.json()
-    return res as Profile
+    if (address in this.cache) {
+      return this.cache[address]
+    }
+    const promise = fetch(`${this.url}/lambdas/profile/${address.toLowerCase()}`).then(res => res.json()) as Promise<Profile>
+    this.cache[address] = promise
+    return promise
   }
 }
 
