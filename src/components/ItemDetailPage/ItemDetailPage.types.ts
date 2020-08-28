532a4910d39bc64de07bdbc9c331fd8e1da0a084
Nicolás Santángelo <santangelonicolas@gmail.com>
2020-08-28 17:59:28 -0300

    feat: item detail page

--- src/components/ItemDetailPage/ItemDetailPage.types.ts
+++ src/components/ItemDetailPage/ItemDetailPage.types.ts
@@ -1,14 +1,14 @@
-// import { Dispatch } from 'redux'
-// import { CallHistoryMethodAction } from 'connected-react-router'
+import { Dispatch } from 'redux'
+import { CallHistoryMethodAction } from 'connected-react-router'
 import { Item } from 'modules/item/types'
 
 export type Props = {
   item: Item | null
   isLoggedIn: boolean
   isLoading: boolean
-  // onNavigate: (path: string) => void
+  onNavigate: (path: string) => void
 }
 
 export type MapStateProps = Pick<Props, 'item' | 'isLoggedIn' | 'isLoading'>
-export type MapDispatchProps = {} //Pick<Props, 'onNavigate'>
-export type MapDispatch = {} // Dispatch<CallHistoryMethodAction>
+export type MapDispatchProps = Pick<Props, 'onNavigate'>
+export type MapDispatch = Dispatch<CallHistoryMethodAction>
