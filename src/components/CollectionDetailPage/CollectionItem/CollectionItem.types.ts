dc72f751efdd5867c19198ebcaf5c9949979adc0
Nicolás Santángelo <santangelonicolas@gmail.com>
2020-09-03 18:42:17 -0300

    feat: open edit item modal

--- src/components/CollectionDetailPage/CollectionItem/CollectionItem.types.ts
+++ src/components/CollectionDetailPage/CollectionItem/CollectionItem.types.ts
@@ -1,12 +1,14 @@
 import { Dispatch } from 'redux'
+import { CallHistoryMethodAction } from 'connected-react-router'
 import { Item } from 'modules/item/types'
 import { openModal, OpenModalAction } from 'modules/modal/actions'
 
 export type Props = {
   item: Item
   onOpenModal: typeof openModal
+  onNavigate: (path: string) => void
 }
 
 export type MapStateProps = {}
-export type MapDispatchProps = Pick<Props, 'onOpenModal'>
-export type MapDispatch = Dispatch<OpenModalAction>
+export type MapDispatchProps = Pick<Props, 'onOpenModal' | 'onNavigate'>
+export type MapDispatch = Dispatch<OpenModalAction | CallHistoryMethodAction>
--- src/components/Modals/EditItemModal/EditItemModal.types.ts
+++ src/components/Modals/EditItemModal/EditItemModal.types.ts
@@ -1,13 +1,13 @@
 import { Dispatch } from 'redux'
 import { ModalProps } from 'decentraland-dapps/dist/providers/ModalProvider/ModalProvider.types'
 import { Item } from 'modules/item/types'
-import { editItemRequest, EditItemRequestAction } from 'modules/item/actions'
+import { saveItemRequest, SaveItemRequestAction } from 'modules/item/actions'
 
 export type Props = ModalProps & {
   item: Item | null
   isLoading: boolean
   metadata: EditItemModalMetadata
-  onEdit: typeof editItemRequest
+  onSave: typeof saveItemRequest
 }
 
 export type EditItemModalMetadata = {
@@ -16,5 +16,5 @@ export type EditItemModalMetadata = {
 
 export type OwnProps = Pick<Props, 'metadata'>
 export type MapStateProps = Pick<Props, 'item' | 'isLoading'>
-export type MapDispatchProps = Pick<Props, 'onEdit'>
-export type MapDispatch = Dispatch<EditItemRequestAction>
+export type MapDispatchProps = Pick<Props, 'onSave'>
+export type MapDispatch = Dispatch<SaveItemRequestAction>
