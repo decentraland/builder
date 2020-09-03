c09c004bf9c39a9c1b4187af868a7b68a9b449b4
Nicolás Santángelo <santangelonicolas@gmail.com>
2020-09-03 17:17:54 -0300

    feat: add edit modal for setting price

--- src/components/CollectionDetailPage/CollectionItem/CollectionItem.types.ts
+++ src/components/CollectionDetailPage/CollectionItem/CollectionItem.types.ts
@@ -1,5 +1,12 @@
+import { Dispatch } from 'redux'
 import { Item } from 'modules/item/types'
+import { openModal, OpenModalAction } from 'modules/modal/actions'
 
 export type Props = {
   item: Item
+  onOpenModal: typeof openModal
 }
+
+export type MapStateProps = {}
+export type MapDispatchProps = Pick<Props, 'onOpenModal'>
+export type MapDispatch = Dispatch<OpenModalAction>
--- src/components/CollectionDetailPage/CollectionItem/index.ts
+++ src/components/CollectionDetailPage/CollectionItem/index.ts
@@ -1,3 +1,3 @@
-import CollectionItem from './CollectionItem'
+import CollectionItem from './CollectionItem.container'
 
 export default CollectionItem
