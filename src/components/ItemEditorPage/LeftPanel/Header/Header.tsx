03324a06ab6811860f7b3a459aa7fe4da83f23d5
Juan Cazala <juan@decentraland.org>
2020-09-16 20:56:29 -0300

    refactor: handlers

--- src/components/ItemEditorPage/LeftPanel/Header/Header.tsx
+++ src/components/ItemEditorPage/LeftPanel/Header/Header.tsx
@@ -7,23 +7,52 @@ import './Header.css'
 import { locations } from 'routing/locations'
 
 export default class Header extends React.PureComponent<Props> {
-  renderSelectedCollection() {
-    const { collection, onNavigate, onOpenModal, onDeleteCollection } = this.props
+  handleHome() {
+    const { onNavigate } = this.props
+    onNavigate(locations.avatar())
+  }
 
+  handleBack() {
+    const { onNavigate } = this.props
+    onNavigate(locations.itemEditor())
+  }
+
+  handleNewItem() {
+    const { onOpenModal } = this.props
+    onOpenModal('CreateItemModal')
+  }
+
+  handleNewCollection() {
+    const { onOpenModal } = this.props
+    onOpenModal('CreateCollectionModal')
+  }
+
+  handleAddNewItem() {
+    const { collection, onOpenModal } = this.props
+    onOpenModal('CreateItemModal', { collectionId: collection!.id })
+  }
+
+  handleAddExistingItem() {
+    const { collection, onOpenModal } = this.props
+    onOpenModal('AddExistingItemModal', { collectionId: collection!.id })
+  }
+
+  handleDelete() {
+    const { collection, onDeleteCollection } = this.props
+    onDeleteCollection(collection!)
+  }
+
+  renderSelectedCollection() {
+    const { collection } = this.props
     return (
       <>
-        <div className="block back" onClick={() => onNavigate(locations.itemEditor())} />
+        <div className="block back" onClick={this.handleBack} />
         <div className="title">{collection!.name}</div>
         <Dropdown trigger={<div className="block actions" />} inline direction="left">
           <Dropdown.Menu>
-            <Dropdown.Item onClick={() => onOpenModal('CreateItemModal', { collectionId: collection!.id })}>
-              {t('item_editor.left_panel.actions.new_item')}
-            </Dropdown.Item>
-            <ConfirmDelete
-              name={collection!.name}
-              onDelete={() => onDeleteCollection(collection!)}
-              trigger={<Dropdown.Item text={t('global.delete')} />}
-            />
+            <Dropdown.Item onClick={this.handleAddNewItem}>{t('item_editor.left_panel.actions.new_item')}</Dropdown.Item>
+            <Dropdown.Item onClick={this.handleAddExistingItem}>{t('item_editor.left_panel.actions.add_existing_item')}</Dropdown.Item>
+            <ConfirmDelete name={collection!.name} onDelete={this.handleDelete} trigger={<Dropdown.Item text={t('global.delete')} />} />
           </Dropdown.Menu>
         </Dropdown>
       </>
@@ -31,17 +60,14 @@ export default class Header extends React.PureComponent<Props> {
   }
 
   renderHeader() {
-    const { onNavigate, onOpenModal } = this.props
     return (
       <>
-        <div className="block home" onClick={() => onNavigate(locations.avatar())} />
+        <div className="block home" onClick={this.handleHome} />
         <div className="title">{t('item_editor.left_panel.title')}</div>
         <Dropdown trigger={<div className="block add" />} inline direction="left">
           <Dropdown.Menu>
-            <Dropdown.Item onClick={() => onOpenModal('CreateItemModal')}>{t('item_editor.left_panel.actions.new_item')}</Dropdown.Item>
-            <Dropdown.Item onClick={() => onOpenModal('CreateCollectionModal')}>
-              {t('item_editor.left_panel.actions.new_collection')}
-            </Dropdown.Item>
+            <Dropdown.Item onClick={this.handleNewItem}>{t('item_editor.left_panel.actions.new_item')}</Dropdown.Item>
+            <Dropdown.Item onClick={this.handleNewCollection}>{t('item_editor.left_panel.actions.new_collection')}</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </>
