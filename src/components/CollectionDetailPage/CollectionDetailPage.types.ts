914b28ce45a0b16cd4d65e18025e559d7e4430e6
Nicolás Santángelo <santangelonicolas@gmail.com>
2020-09-23 13:40:56 -0300

    feat: add on sale modal step

--- src/components/CollectionDetailPage/CollectionDetailPage.types.ts
+++ src/components/CollectionDetailPage/CollectionDetailPage.types.ts
@@ -1,11 +1,6 @@
 import { Dispatch } from 'redux'
 import { CallHistoryMethodAction } from 'connected-react-router'
-import {
-  deleteCollectionRequest,
-  DeleteCollectionRequestAction,
-  setCollectionMintersRequest,
-  SetCollectionMintersRequestAction
-} from 'modules/collection/actions'
+import { deleteCollectionRequest, DeleteCollectionRequestAction } from 'modules/collection/actions'
 import { openModal, OpenModalAction } from 'modules/modal/actions'
 import { Collection } from 'modules/collection/types'
 import { Item } from 'modules/item/types'
@@ -18,11 +13,8 @@ export type Props = {
   onNavigate: (path: string) => void
   onOpenModal: typeof openModal
   onDelete: typeof deleteCollectionRequest
-  onSetMinters: typeof setCollectionMintersRequest
 }
 
 export type MapStateProps = Pick<Props, 'collection' | 'items' | 'isOnSaleLoading' | 'isLoading'>
-export type MapDispatchProps = Pick<Props, 'onNavigate' | 'onOpenModal' | 'onDelete' | 'onSetMinters'>
-export type MapDispatch = Dispatch<
-  CallHistoryMethodAction | OpenModalAction | DeleteCollectionRequestAction | SetCollectionMintersRequestAction
->
+export type MapDispatchProps = Pick<Props, 'onNavigate' | 'onOpenModal' | 'onDelete'>
+export type MapDispatch = Dispatch<CallHistoryMethodAction | OpenModalAction | DeleteCollectionRequestAction>
---
+++ src/components/Modals/SellCollectionModal/SellCollectionModal.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import { ModalNavigation, Button } from 'decentraland-ui'
+import Modal from 'decentraland-dapps/dist/containers/Modal'
+import { t } from 'decentraland-dapps/dist/modules/translation/utils'
+
+import { setOnSale } from 'modules/collection/utils'
+import { Props } from './SellCollectionModal.types'
+import './SellCollectionModal.css'
+
+export default class SellCollectionModal extends React.PureComponent<Props> {
+  handleSell = () => {
+    const { collection, metadata, onSetMinters } = this.props
+    onSetMinters(collection, setOnSale(collection, metadata.isOnSale))
+  }
+
+  render() {
+    const { metadata, onClose } = this.props
+    return (
+      <Modal name={name} className="SellCollectionModal" size="tiny" onClose={onClose}>
+        <ModalNavigation title={t('sell_collection_modal.title')} onClose={onClose} />
+        <Modal.Content>
+          {metadata.isOnSale ? (
+            <>
+              {t('sell_collection_modal.turn_on_description')}
+              <Button primary fluid onClick={this.handleSell}>
+                {t('sell_collection_modal.turn_on')}
+              </Button>
+            </>
+          ) : (
+            <>
+              {t('sell_collection_modal.turn_off_description')}
+              <Button primary fluid onClick={this.handleSell}>
+                {t('sell_collection_modal.turn_off')}
+              </Button>
+            </>
+          )}
+          <Button secondary fluid onClick={onClose}>
+            {t('global.cancel')}
+          </Button>
+        </Modal.Content>
+      </Modal>
+    )
+  }
+}
