789951bcd358b9dd264a800fcbfc7ec9b76bef14
Marcelo Alaniz <alanizmarcelo@gmail.com>
2020-09-28 13:40:04 -0300

    wip: fix for set_link instead of set_name and other translations

--- src/components/LandDetailPage/LandDetailPage.tsx
+++ src/components/LandDetailPage/LandDetailPage.tsx
@@ -96,7 +96,7 @@ export default class LandDetailPage extends React.PureComponent<Props, State> {
                             onClick={() => onNavigate(locations.landOperator(land.id))}
                           />
                           <Dropdown.Item
-                            text={t('land_detail_page.set_name')}
+                            text={t('land_detail_page.set_link')}
                             onClick={() => onNavigate(locations.landEns(land.id))}
                           />
                           {canBuildEstate ? (
