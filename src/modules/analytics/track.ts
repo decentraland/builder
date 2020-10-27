d2f58d8428afae5ff190093ded2719806bc4ec8f
Juan Cazala <juan@decentraland.org>
2020-10-27 20:24:46 -0300

    chore: track action related to wearables editor

--- src/modules/analytics/track.ts
+++ src/modules/analytics/track.ts
@@ -34,6 +34,50 @@ import {
   SET_UPDATE_MANAGER_SUCCESS,
   SetUpdateManagerSuccessAction
 } from 'modules/land/actions'
+import {
+  DeleteItemFailureAction,
+  DeleteItemSuccessAction,
+  DELETE_ITEM_FAILURE,
+  DELETE_ITEM_SUCCESS,
+  DeployItemContentsFailureAction,
+  DeployItemContentsSuccessAction,
+  DEPLOY_ITEM_CONTENTS_FAILURE,
+  DEPLOY_ITEM_CONTENTS_SUCCESS,
+  SaveItemFailureAction,
+  SaveItemSuccessAction,
+  SavePublishedItemFailureAction,
+  SavePublishedItemSuccessAction,
+  SAVE_ITEM_FAILURE,
+  SAVE_ITEM_SUCCESS,
+  SAVE_PUBLISHED_ITEM_FAILURE,
+  SAVE_PUBLISHED_ITEM_SUCCESS
+} from 'modules/item/actions'
+import {
+  DeleteCollectionFailureAction,
+  DeleteCollectionSuccessAction,
+  DELETE_COLLECTION_FAILURE,
+  DELETE_COLLECTION_SUCCESS,
+  MintCollectionItemsFailureAction,
+  MintCollectionItemsSuccessAction,
+  MINT_COLLECTION_ITEMS_FAILURE,
+  MINT_COLLECTION_ITEMS_SUCCESS,
+  PublishCollectionFailureAction,
+  PublishCollectionSuccessAction,
+  PUBLISH_COLLECTION_FAILURE,
+  PUBLISH_COLLECTION_SUCCESS,
+  SaveCollectionFailureAction,
+  SaveCollectionSuccessAction,
+  SAVE_COLLECTION_FAILURE,
+  SAVE_COLLECTION_SUCCESS,
+  SetCollectionManagersFailureAction,
+  SetCollectionManagersSuccessAction,
+  SetCollectionMintersFailureAction,
+  SetCollectionMintersSuccessAction,
+  SET_COLLECTION_MANAGERS_FAILURE,
+  SET_COLLECTION_MANAGERS_SUCCESS,
+  SET_COLLECTION_MINTERS_FAILURE,
+  SET_COLLECTION_MINTERS_SUCCESS
+} from 'modules/collection/actions'
 
 function addPayload(actionType: string, eventName: string, getPayload = (action: any) => action.payload) {
   add(actionType, eventName, getPayload)
@@ -175,6 +219,176 @@ add(DISSOLVE_ESTATE_SUCCESS, 'Dissolve estate', action => {
 })
 
 // Set Update Manager
+add(
+  SET_UPDATE_MANAGER_SUCCESS,
+  action => (action.payload.isApproved ? 'Add manager' : 'Remove manager'),
+  action => {
+    const { payload } = action as SetUpdateManagerSuccessAction
+    return {
+      address: payload.address,
+      type: payload.type
+    }
+  }
+)
+
+// Item Editor
+
+add(SAVE_ITEM_SUCCESS, 'Save item', action => {
+  const { payload } = action as SaveItemSuccessAction
+  return {
+    item: payload.item
+  }
+})
+
+add(SAVE_ITEM_FAILURE, 'Save item error', action => {
+  const { payload } = action as SaveItemFailureAction
+  return {
+    item: payload.item,
+    error: payload.error
+  }
+})
+
+add(DELETE_ITEM_SUCCESS, 'Delete item', action => {
+  const { payload } = action as DeleteItemSuccessAction
+  return {
+    itemId: payload.item.id
+  }
+})
+
+add(DELETE_ITEM_FAILURE, 'Delete item error', action => {
+  const { payload } = action as DeleteItemFailureAction
+  return {
+    item: payload.item,
+    error: payload.error
+  }
+})
+
+add(SAVE_PUBLISHED_ITEM_SUCCESS, 'Edit item on chain', action => {
+  const { payload } = action as SavePublishedItemSuccessAction
+  return {
+    item: payload.item
+  }
+})
+
+add(SAVE_PUBLISHED_ITEM_FAILURE, 'Edit item on chain error', action => {
+  const { payload } = action as SavePublishedItemFailureAction
+  return {
+    item: payload.item,
+    error: payload.error
+  }
+})
+
+add(DEPLOY_ITEM_CONTENTS_SUCCESS, 'Deploy item contents', action => {
+  const { payload } = action as DeployItemContentsSuccessAction
+  return {
+    item: payload.item
+  }
+})
+
+add(DEPLOY_ITEM_CONTENTS_FAILURE, 'Deploy item contents error', action => {
+  const { payload } = action as DeployItemContentsFailureAction
+  return {
+    item: payload.item,
+    error: payload.error
+  }
+})
+
+add(SAVE_COLLECTION_SUCCESS, 'Save collection', action => {
+  const { payload } = action as SaveCollectionSuccessAction
+  return {
+    collection: payload.collection
+  }
+})
+
+add(SAVE_COLLECTION_FAILURE, 'Save collection error', action => {
+  const { payload } = action as SaveCollectionFailureAction
+  return {
+    collection: payload.collection,
+    error: payload.error
+  }
+})
+
+add(DELETE_COLLECTION_SUCCESS, 'Delete collection', action => {
+  const { payload } = action as DeleteCollectionSuccessAction
+  return {
+    collection: payload.collection
+  }
+})
+
+add(DELETE_COLLECTION_FAILURE, 'Delete collection error', action => {
+  const { payload } = action as DeleteCollectionFailureAction
+  return {
+    collection: payload.collection,
+    error: payload.error
+  }
+})
+
+add(PUBLISH_COLLECTION_SUCCESS, 'Publish collection', action => {
+  const { payload } = action as PublishCollectionSuccessAction
+  return {
+    collection: payload.collection
+  }
+})
+
+add(PUBLISH_COLLECTION_FAILURE, 'Publish collection error', action => {
+  const { payload } = action as PublishCollectionFailureAction
+  return {
+    collection: payload.collection,
+    error: payload.error
+  }
+})
+
+add(MINT_COLLECTION_ITEMS_SUCCESS, 'Mint items', action => {
+  const { payload } = action as MintCollectionItemsSuccessAction
+  return {
+    collection: payload.collection,
+    mints: payload.mints
+  }
+})
+
+add(MINT_COLLECTION_ITEMS_FAILURE, 'Mint items error', action => {
+  const { payload } = action as MintCollectionItemsFailureAction
+  return {
+    collection: payload.collection,
+    mints: payload.mints,
+    error: payload.error
+  }
+})
+
+add(SET_COLLECTION_MINTERS_SUCCESS, 'Set minters', action => {
+  const { payload } = action as SetCollectionMintersSuccessAction
+  return {
+    collection: payload.collection,
+    minters: payload.minters
+  }
+})
+
+add(SET_COLLECTION_MINTERS_FAILURE, 'Set minters error', action => {
+  const { payload } = action as SetCollectionMintersFailureAction
+  return {
+    collection: payload.collection,
+    accessList: payload.accessList,
+    error: payload.error
+  }
+})
+
+add(SET_COLLECTION_MANAGERS_SUCCESS, 'Set collaborators', action => {
+  const { payload } = action as SetCollectionManagersSuccessAction
+  return {
+    collection: payload.collection,
+    collaborators: payload.managers
+  }
+})
+
+add(SET_COLLECTION_MANAGERS_FAILURE, 'Set collaborators error', action => {
+  const { payload } = action as SetCollectionManagersFailureAction
+  return {
+    collection: payload.collection,
+    accessList: payload.accessList,
+    error: payload.error
+  }
+})
+
 add(
   SET_UPDATE_MANAGER_SUCCESS,
   action => (action.payload.isApproved ? 'Add manager' : 'Remove manager'),
