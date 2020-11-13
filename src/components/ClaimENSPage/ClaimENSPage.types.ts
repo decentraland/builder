60642ddaf41c0e8b510ab4d4a795267ea09eb4c2
Nicolás Santángelo <santangelonicolas@gmail.com>
2020-11-13 12:04:25 -0300

    feat: claim name UI (missing I18n and interaction)

---
+++ src/components/ClaimENSPage/ClaimENSPage.types.ts
@@ -0,0 +1,15 @@
+import { Dispatch } from 'redux'
+import { CallHistoryMethodAction, goBack } from 'connected-react-router'
+
+export type Props = {
+  onNavigate: (path: string) => void
+  onBack: typeof goBack
+}
+
+export type State = {
+  name?: string
+}
+
+export type MapStateProps = {}
+export type MapDispatchProps = Pick<Props, 'onNavigate' | 'onBack'>
+export type MapDispatch = Dispatch<CallHistoryMethodAction>
