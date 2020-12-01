7795f0390a7d225bd7294aff75545186ff3c023e
Marcelo Alaniz <alanizmarcelo@gmail.com>
2020-12-01 14:51:26 -0300

    wip: using max value of 2^255

--- src/components/ClaimENSPage/ClaimENSPage.tsx
+++ src/components/ClaimENSPage/ClaimENSPage.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { toBN } from 'web3x-es/utils'
 import { Address } from 'web3x-es/address'
 import { Eth } from 'web3x-es/eth'
 import { Row, Column, Section, Narrow, InputOnChangeData, Header, Form, Field, Button, Mana, Radio } from 'decentraland-ui'
@@ -49,6 +50,10 @@ export default class ClaimENSPage extends React.PureComponent<Props, State> {
     } catch (error) {
       throw error
     }
+  }
+
+  getTokenAmountToApprove() {
+    return toBN(2).pow(toBN(255))
   }
 
   handleManaApprove = async () => {
