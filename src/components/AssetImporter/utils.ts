d2da655718f2cd63b6a9044552503c354391ac23
Juan Cazala <juan@decentraland.org>
2020-12-02 18:09:34 -0300

    fix: update code separator to the latest version of the sdk

--- src/components/AssetImporter/utils.ts
+++ src/components/AssetImporter/utils.ts
@@ -5,7 +5,8 @@ import { ModelMetrics } from 'modules/scene/types'
 
 export const ASSET_MANIFEST = 'asset.json'
 
-export const CODE_SEPARATOR = 'global.dclamd=loader;'
+export const CODE_SEPARATOR = /\/\*! \"src\/game\.ts\" [a-f0-9]+ \*\//
+
 export const SOURCE_MAPS_SEPARATOR = '//# sourceMappingURL'
 
 export function createDefaultImportedFile(id: string, assetPackId: string, file: File): ImportedFile {
@@ -48,7 +49,7 @@ export async function prepareScript(scriptPath: string, namespace: string, conte
     let text = await new Response(blob).text()
 
     // remove ecs and amd loader
-    if (text.includes(CODE_SEPARATOR)) {
+    if (CODE_SEPARATOR.test(text)) {
       text = text.split(CODE_SEPARATOR).pop()!
     }
 
