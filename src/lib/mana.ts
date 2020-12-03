6708569dae40ace43bb194e5d0777fb1cb33f007
Marcelo Alaniz <alanizmarcelo@gmail.com>
2020-12-03 14:56:51 -0300

    feat: Claim approve functionality (#1186)
    * wip: working with loading and enterely coded in the component.
    
    * fixing contracts
    
    * wip: updating decentraland-dapps
    
    * wip: .env.example with new environment variables
    
    * wip: adding to .ci enviroments DCL Contract address variable
    
    * wip: fixes based in review on PR
    
    * wip: i18n for en/es/zsh
    
    * wip: just a little improve in the code
    
    * wip: isLoading working
    
    * wip: using max value of 2^255
    
    * wip: using getMaximumValue from lib/mana.ts
    
    * wip: using function to instantiate mana contract
    
    * ref: manaToApprove on transaction for toggle
    
    * wip: adding try catch && refactoring code
    
    * wip: message explaining why mana approvement is needed
    
    * wip: fixing toggle message for mana approve
    
    * wip: better name for the contract DCLController

--- src/lib/mana.ts
+++ src/lib/mana.ts
@@ -1,5 +1,11 @@
+import { toBN } from 'web3x-es/utils'
+
 const MANA_SYMBOL = '⏣'
 
 export function addSymbol(num: number) {
   return num > 0 ? `${MANA_SYMBOL} ${num.toString()}` : ''
+}
+
+export function getMaximumValue() {
+  return toBN(2).pow(toBN(255))
 }
