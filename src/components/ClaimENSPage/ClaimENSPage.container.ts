64893af525caec763aa08246ade0386979ac1b5f
Marcelo Alaniz <alanizmarcelo@gmail.com>
2020-12-03 14:56:51 -0300

    feat: Claim approve functionality (#1186)
    * wip: working with loading and enterely coded in the component.
    
    * fixing contracts
    
    * wip: updating decentraland-dapps
    
    * wip: .env.example with new environment variables
    
    * wip: adding to .ci enviroments DCL Contract address variable
    
    * wip: fixes based in review on PR
    
    * wip: i18n for en/es/zsh
    
    * wip: just a little improve in the code
    
    * wip: isLoading working
    
    * wip: using max value of 2^255
    
    * wip: using getMaximumValue from lib/mana.ts
    
    * wip: using function to instantiate mana contract
    
    * ref: manaToApprove on transaction for toggle
    
    * wip: adding try catch && refactoring code
    
    * wip: message explaining why mana approvement is needed
    
    * wip: fixing toggle message for mana approve
    
    * wip: better name for the contract DCLController

--- src/components/ClaimENSPage/ClaimENSPage.container.ts
+++ src/components/ClaimENSPage/ClaimENSPage.container.ts
@@ -3,8 +3,11 @@ import { push, goBack } from 'connected-react-router'
 import { RootState } from 'modules/common/types'
 import { MapStateProps, MapDispatchProps, MapDispatch } from './ClaimENSPage.types'
 import ClaimENSPage from './ClaimENSPage'
+import { getAddress } from 'decentraland-dapps/dist/modules/wallet/selectors'
 
-const mapState = (_state: RootState): MapStateProps => ({})
+const mapState = (state: RootState): MapStateProps => ({
+  address: getAddress(state) || ''
+})
 
 const mapDispatch = (dispatch: MapDispatch): MapDispatchProps => ({
   onNavigate: path => dispatch(push(path)),
--- src/components/ClaimENSPage/ClaimENSPage.types.ts
+++ src/components/ClaimENSPage/ClaimENSPage.types.ts
@@ -1,15 +1,20 @@
 import { Dispatch } from 'redux'
 import { CallHistoryMethodAction, goBack } from 'connected-react-router'
+import { ERC20TransactionReceipt } from 'contracts/ERC20'
 
 export type Props = {
+  address: string
   onNavigate: (path: string) => void
   onBack: typeof goBack
 }
 
 export type State = {
   name: string
+  amountApproved: number
+  isLoading: boolean
+  receiptTx: ERC20TransactionReceipt | undefined
 }
 
-export type MapStateProps = {}
+export type MapStateProps = Pick<Props, 'address'>
 export type MapDispatchProps = Pick<Props, 'onNavigate' | 'onBack'>
 export type MapDispatch = Dispatch<CallHistoryMethodAction>
