a6d080eb0701eac545a1cbcd3e07d89d95a56945
Marcelo Alaniz <alanizmarcelo@gmail.com>
2020-12-04 16:23:15 -0300

    wip: fixing imports and routes

--- src/components/ClaimENSPage/ClaimENSPage.container.ts
+++ src/components/ClaimENSPage/ClaimENSPage.container.ts
@@ -1,10 +1,10 @@
 import { connect } from 'react-redux'
 import { push, goBack } from 'connected-react-router'
+import { getAddress } from 'decentraland-dapps/dist/modules/wallet/selectors'
 import { RootState } from 'modules/common/types'
 import { openModal } from 'modules/modal/actions'
 import { MapStateProps, MapDispatchProps, MapDispatch } from './ClaimENSPage.types'
 import ClaimENSPage from './ClaimENSPage'
-import { getAddress } from 'decentraland-dapps/dist/modules/wallet/selectors'
 
 const mapState = (state: RootState): MapStateProps => ({
   address: getAddress(state) || ''
