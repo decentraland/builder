c4aee354e1ace51d95cbbbd26f3018217bb199ae
Marcelo Alaniz <alanizmarcelo@gmail.com>
2020-12-15 12:09:09 -0300

    fix: without massive loading in fullscreen

--- src/components/LandAssignENSPage/LandAssignENSPage.tsx
+++ src/components/LandAssignENSPage/LandAssignENSPage.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
-import { Loader } from 'decentraland-ui'
 import { T, t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { locations } from 'routing/locations'
 import LandProviderPage from 'components/LandProviderPage'
@@ -27,32 +26,28 @@ export default class LandAssignENSPage extends React.PureComponent<Props> {
 
     return (
       <LandProviderPage>
-        {land =>
-          isLoading ? (
-            <Loader active size="massive" />
-          ) : (
-            <LandAction
+        {land => (
+          <LandAction
+            land={land}
+            title={t('land_assign_ens_page.title', { name: ens.subdomain })}
+            subtitle={
+              <T id="land_assign_ens_page.subtitle" values={{ land: <Link to={locations.landDetail(land.id)}>{land.name}</Link> }} />
+            }
+          >
+            <LandAssignENSForm
               land={land}
-              title={t('land_assign_ens_page.title', { name: ens.subdomain })}
-              subtitle={
-                <T id="land_assign_ens_page.subtitle" values={{ land: <Link to={locations.landDetail(land.id)}>{land.name}</Link> }} />
-              }
-            >
-              <LandAssignENSForm
-                land={land}
-                ens={ens}
-                error={error}
-                isLoading={isLoading}
-                isWaitingTxSetResolver={isWaitingTxSetResolver}
-                isWaitingTxSetContent={isWaitingTxSetContent}
-                onSetENSContent={onSetENSContent}
-                onSetENSResolver={onSetENSResolver}
-                onBack={this.handleGoBack}
-                onNavigate={onNavigate}
-              />
-            </LandAction>
-          )
-        }
+              ens={ens}
+              error={error}
+              isLoading={isLoading}
+              isWaitingTxSetResolver={isWaitingTxSetResolver}
+              isWaitingTxSetContent={isWaitingTxSetContent}
+              onSetENSContent={onSetENSContent}
+              onSetENSResolver={onSetENSResolver}
+              onBack={this.handleGoBack}
+              onNavigate={onNavigate}
+            />
+          </LandAction>
+        )}
       </LandProviderPage>
     )
   }
