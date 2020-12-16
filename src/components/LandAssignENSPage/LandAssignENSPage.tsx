5fa4321e10294ee3ac73c99675e92ecaed37042c
Marcelo Alaniz <alanizmarcelo@gmail.com>
2020-12-16 09:45:29 -0300

    fix: fixing reload error

--- src/components/LandAssignENSPage/LandAssignENSPage.tsx
+++ src/components/LandAssignENSPage/LandAssignENSPage.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import { Loader } from 'decentraland-ui'
 import { T, t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { locations } from 'routing/locations'
 import LandProviderPage from 'components/LandProviderPage'
@@ -26,28 +27,32 @@ export default class LandAssignENSPage extends React.PureComponent<Props> {
 
     return (
       <LandProviderPage>
-        {land => (
-          <LandAction
-            land={land}
-            title={t('land_assign_ens_page.title', { name: ens.subdomain })}
-            subtitle={
-              <T id="land_assign_ens_page.subtitle" values={{ land: <Link to={locations.landDetail(land.id)}>{land.name}</Link> }} />
-            }
-          >
-            <LandAssignENSForm
+        {land =>
+          !ens ? (
+            <Loader active size="massive" />
+          ) : (
+            <LandAction
               land={land}
-              ens={ens}
-              error={error}
-              isLoading={isLoading}
-              isWaitingTxSetResolver={isWaitingTxSetResolver}
-              isWaitingTxSetContent={isWaitingTxSetContent}
-              onSetENSContent={onSetENSContent}
-              onSetENSResolver={onSetENSResolver}
-              onBack={this.handleGoBack}
-              onNavigate={onNavigate}
-            />
-          </LandAction>
-        )}
+              title={t('land_assign_ens_page.title', { name: ens.subdomain })}
+              subtitle={
+                <T id="land_assign_ens_page.subtitle" values={{ land: <Link to={locations.landDetail(land.id)}>{land.name}</Link> }} />
+              }
+            >
+              <LandAssignENSForm
+                land={land}
+                ens={ens}
+                error={error}
+                isLoading={isLoading}
+                isWaitingTxSetResolver={isWaitingTxSetResolver}
+                isWaitingTxSetContent={isWaitingTxSetContent}
+                onSetENSContent={onSetENSContent}
+                onSetENSResolver={onSetENSResolver}
+                onBack={this.handleGoBack}
+                onNavigate={onNavigate}
+              />
+            </LandAction>
+          )
+        }
       </LandProviderPage>
     )
   }
