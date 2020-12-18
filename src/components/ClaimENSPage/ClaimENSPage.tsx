4ff951ae0e57a0938cec191a5fb980bfe76e87d2
Marcelo Alaniz <alanizmarcelo@gmail.com>
2020-12-18 10:18:00 -0300

    fix: without throw error

--- src/components/ClaimENSPage/ClaimENSPage.tsx
+++ src/components/ClaimENSPage/ClaimENSPage.tsx
@@ -91,7 +91,6 @@ export default class ClaimENSPage extends React.PureComponent<Props, State> {
       }
     } catch (error) {
       this.setState({ isLoading: false })
-      throw error
     }
   }
 
