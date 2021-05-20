b354f7e760956c2212f6ea686d9e4aed3d39ee76
Juan Cazala <juancazala@gmail.com>
2021-05-20 12:26:03 -0300

    chore: use env var

--- src/components/IntroPopup/IntroPopup.tsx
+++ src/components/IntroPopup/IntroPopup.tsx
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import { Button, Column, Header, Popup } from 'decentraland-ui'
 import { T, t } from 'decentraland-dapps/dist/modules/translation/utils'
+import { env } from 'decentraland-commons'
 import { Props, State } from './IntroPopup.types'
 import './IntroPopup.css'
 
@@ -44,7 +45,11 @@ export default class IntroPopup extends PureComponent<Props, State> {
                       </a>
                     ),
                     account_link: (
-                      <a href="https://account.decentraland.org" target="_blank" rel="noopener noreferrer">
+                      <a
+                        href={env.get('REACT_APP_ACCOUNT_URL', 'https://account.decentraland.org')}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
                         {t('intro_popup.account_link')}
                       </a>
                     ),
