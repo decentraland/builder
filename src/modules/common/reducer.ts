95fd5e9b8b26164490d51a5089a0d3e08f5b9e27
Nicolas Santangelo <santangelonicolas@gmail.com>
2021-05-31 17:29:46 -0300

    feat: add toasts from dapps and handle meta transaction errors (#1401)
    * feat: add toasts from dapps and handle meta transaction errors
    
    * chore: env var on now.json
    
    * chore: bump

--- src/modules/common/reducer.ts
+++ src/modules/common/reducer.ts
@@ -1,67 +1,69 @@
 import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
 import { History } from 'history'
-import { translationReducer as translation } from 'decentraland-dapps/dist/modules/translation/reducer'
-import { storageReducer as storage, storageReducerWrapper } from 'decentraland-dapps/dist/modules/storage/reducer'
-import { transactionReducer as transaction } from 'decentraland-dapps/dist/modules/transaction/reducer'
-import { walletReducer as wallet } from 'decentraland-dapps/dist/modules/wallet/reducer'
+import { authorizationReducer as authorization } from 'decentraland-dapps/dist/modules/authorization/reducer'
 import { modalReducer as modal } from 'decentraland-dapps/dist/modules/modal/reducer'
 import { profileReducer as profile } from 'decentraland-dapps/dist/modules/profile/reducer'
-import { authorizationReducer as authorization } from 'decentraland-dapps/dist/modules/authorization/reducer'
+import { storageReducer as storage, storageReducerWrapper } from 'decentraland-dapps/dist/modules/storage/reducer'
+import { toastReducer as toast } from 'decentraland-dapps/dist/modules/toast/reducer'
+import { transactionReducer as transaction } from 'decentraland-dapps/dist/modules/transaction/reducer'
+import { translationReducer as translation } from 'decentraland-dapps/dist/modules/translation/reducer'
+import { walletReducer as wallet } from 'decentraland-dapps/dist/modules/wallet/reducer'
 
 import { RootState } from 'modules/common/types'
 import { assetPackReducer as assetPack } from 'modules/assetPack/reducer'
-import { editorReducer as editor } from 'modules/editor/reducer'
 import { assetReducer as asset } from 'modules/asset/reducer'
-import { uiReducer as ui } from 'modules/ui/reducer'
-import { projectReducer as project } from 'modules/project/reducer'
+import { collectionReducer as collection } from 'modules/collection/reducer'
+import { committeeReducer as committee } from 'modules/committee/reducer'
+import { deploymentReducer as deployment } from 'modules/deployment/reducer'
+import { editorReducer as editor } from 'modules/editor/reducer'
+import { ensReducer as ens } from 'modules/ens/reducer'
+import { identityReducer as identity } from 'modules/identity/reducer'
+import { itemReducer as item } from 'modules/item/reducer'
+import { landReducer as land } from 'modules/land/reducer'
+import { locationReducer as location } from 'modules/location/reducer'
+import { mediaReducer as media } from 'modules/media/reducer'
 import { poolGroupReducer as poolGroup } from 'modules/poolGroup/reducer'
 import { poolReducer as pool } from 'modules/pool/reducer'
+import { projectReducer as project } from 'modules/project/reducer'
 import { sceneReducer as scene } from 'modules/scene/reducer'
-import { deploymentReducer as deployment } from 'modules/deployment/reducer'
-import { mediaReducer as media } from 'modules/media/reducer'
-import { syncReducer as sync } from 'modules/sync/reducer'
-import { identityReducer as identity } from 'modules/identity/reducer'
-import { landReducer as land } from 'modules/land/reducer'
-import { ensReducer as ens } from 'modules/ens/reducer'
-import { tileReducer as tile } from 'modules/tile/reducer'
-import { committeeReducer as committee } from 'modules/committee/reducer'
-import { collectionReducer as collection } from 'modules/collection/reducer'
-import { itemReducer as item } from 'modules/item/reducer'
-import { locationReducer as location } from 'modules/location/reducer'
 import { statsReducer as stats } from 'modules/stats/reducer'
+import { syncReducer as sync } from 'modules/sync/reducer'
+import { tileReducer as tile } from 'modules/tile/reducer'
+import { uiReducer as ui } from 'modules/ui/reducer'
 
 export function createRootReducer(history: History) {
   return storageReducerWrapper(
     combineReducers<RootState>({
-      storage,
-      editor,
-      translation,
-      transaction,
-      wallet,
-      authorization,
-      modal,
-      assetPack,
       asset,
-      ui,
-      project,
-      poolGroup,
-      pool,
-      profile,
-      scene,
-      deployment,
-      media,
-      sync,
-      identity,
-      land,
-      ens,
-      tile,
-      committee,
+      assetPack,
+      authorization,
       collection,
+      committee,
+      deployment,
+      editor,
+      ens,
+      identity,
       item,
+      land,
       location,
+      media,
+      modal,
+      pool,
+      poolGroup,
+      profile,
+      project,
+      router: connectRouter(history),
+      scene,
       stats,
-      router: connectRouter(history)
+      storage,
+      sync,
+      tile,
+      toast,
+      transaction,
+      translation,
+      ui,
+      wallet
     })
   )
 }
--- src/modules/common/sagas.ts
+++ src/modules/common/sagas.ts
@@ -1,35 +1,36 @@
 import { all } from 'redux-saga/effects'
 
 import { authorizationSaga } from 'decentraland-dapps/dist/modules/authorization/sagas'
-import { transactionSaga } from 'decentraland-dapps/dist/modules/transaction/sagas'
 import { createProfileSaga } from 'decentraland-dapps/dist/modules/profile/sagas'
+import { transactionSaga } from 'decentraland-dapps/dist/modules/transaction/sagas'
 
-import { walletSaga } from 'modules/wallet/sagas'
-import { translationSaga } from 'modules/translation/sagas'
-import { assetPackSaga } from 'modules/assetPack/sagas'
-import { modalSaga } from 'modules/modal/sagas'
-import { sceneSaga } from 'modules/scene/sagas'
-import { projectSaga } from 'modules/project/sagas'
-import { editorSaga } from 'modules/editor/sagas'
-import { keyboardSaga } from 'modules/keyboard/sagas'
 import { analyticsSaga } from 'modules/analytics/sagas'
+import { assetPackSaga } from 'modules/assetPack/sagas'
 import { assetSaga } from 'modules/asset/sagas'
-import { deploymentSaga } from 'modules/deployment/sagas'
-import { mediaSaga } from 'modules/media/sagas'
-import { locationSaga } from 'modules/location/sagas'
-import { syncSaga } from 'modules/sync/sagas'
-import { uiSaga } from 'modules/ui/sagas'
-import { poolGroupSaga } from 'modules/poolGroup/sagas'
-import { poolSaga } from 'modules/pool/sagas'
-import { identitySaga } from 'modules/identity/sagas'
-import { landSaga } from 'modules/land/sagas'
-import { ensSaga } from 'modules/ens/sagas'
-import { tileSaga } from 'modules/tile/sagas'
 import { collectionSaga } from 'modules/collection/sagas'
 import { committeeSaga } from 'modules/committee/sagas'
+import { deploymentSaga } from 'modules/deployment/sagas'
+import { editorSaga } from 'modules/editor/sagas'
+import { ensSaga } from 'modules/ens/sagas'
 import { forumSaga } from 'modules/forum/sagas'
+import { identitySaga } from 'modules/identity/sagas'
 import { itemSaga } from 'modules/item/sagas'
+import { keyboardSaga } from 'modules/keyboard/sagas'
+import { landSaga } from 'modules/land/sagas'
+import { locationSaga } from 'modules/location/sagas'
+import { mediaSaga } from 'modules/media/sagas'
+import { modalSaga } from 'modules/modal/sagas'
+import { poolGroupSaga } from 'modules/poolGroup/sagas'
+import { poolSaga } from 'modules/pool/sagas'
+import { projectSaga } from 'modules/project/sagas'
+import { sceneSaga } from 'modules/scene/sagas'
 import { statsSaga } from 'modules/stats/sagas'
+import { syncSaga } from 'modules/sync/sagas'
+import { tileSaga } from 'modules/tile/sagas'
+import { toastSaga } from 'modules/toast/sagas'
+import { translationSaga } from 'modules/translation/sagas'
+import { uiSaga } from 'modules/ui/sagas'
+import { walletSaga } from 'modules/wallet/sagas'
 
 import { PEER_URL } from 'lib/api/peer'
 
@@ -38,33 +39,34 @@ const profileSaga = createProfileSaga({ peerUrl: PEER_URL })
 export function* rootSaga() {
   yield all([
     analyticsSaga(),
-    transactionSaga(),
-    translationSaga(),
-    walletSaga(),
-    authorizationSaga(),
     assetPackSaga(),
+    assetSaga(),
+    authorizationSaga(),
+    collectionSaga(),
+    committeeSaga(),
+    deploymentSaga(),
+    editorSaga(),
+    ensSaga(),
+    forumSaga(),
+    identitySaga(),
+    itemSaga(),
+    keyboardSaga(),
+    landSaga(),
+    locationSaga(),
+    mediaSaga(),
     modalSaga(),
-    sceneSaga(),
-    projectSaga(),
     poolGroupSaga(),
     poolSaga(),
     profileSaga(),
-    editorSaga(),
-    keyboardSaga(),
-    assetSaga(),
-    deploymentSaga(),
-    mediaSaga(),
-    locationSaga(),
+    projectSaga(),
+    sceneSaga(),
+    statsSaga(),
     syncSaga(),
-    uiSaga(),
-    identitySaga(),
-    landSaga(),
     tileSaga(),
-    collectionSaga(),
-    committeeSaga(),
-    forumSaga(),
-    itemSaga(),
-    ensSaga(),
-    statsSaga()
+    toastSaga(),
+    transactionSaga(),
+    translationSaga(),
+    uiSaga(),
+    walletSaga()
   ])
 }
