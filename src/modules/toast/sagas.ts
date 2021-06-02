c2feb6e2d73ccf4ac12162676ed3bb52ec65e6f3
Nicolas Santangelo <santangelonicolas@gmail.com>
2021-06-02 17:21:35 -0300

    feat: don't show toast on rejected signature (#1409)

--- src/modules/toast/sagas.ts
+++ src/modules/toast/sagas.ts
@@ -1,5 +1,7 @@
 import { all, takeEvery, put } from 'redux-saga/effects'
+import { PayloadAction } from 'typesafe-actions'
 import { toastSaga as baseToastSaga } from 'decentraland-dapps/dist/modules/toast/sagas'
+import { showToast } from 'decentraland-dapps/dist/modules/toast/actions'
 import {
   APPROVE_COLLECTION_FAILURE,
   MINT_COLLECTION_ITEMS_FAILURE,
@@ -8,9 +10,8 @@ import {
   SET_COLLECTION_MANAGERS_FAILURE,
   SET_COLLECTION_MINTERS_FAILURE
 } from 'modules/collection/actions'
-import { showToast } from 'decentraland-dapps/dist/modules/toast/actions'
-import { getMetaTransactionFailureToast } from './toasts'
 import { SAVE_PUBLISHED_ITEM_FAILURE } from 'modules/item/actions'
+import { getMetaTransactionFailureToast } from './toasts'
 
 export function* toastSaga() {
   yield all([baseToastSaga(), customToastSaga()])
@@ -26,6 +27,14 @@ function* customToastSaga() {
   yield takeEvery(SAVE_PUBLISHED_ITEM_FAILURE, handleMetaTransactionFailure)
 }
 
-function* handleMetaTransactionFailure() {
-  yield put(showToast(getMetaTransactionFailureToast()))
+function* handleMetaTransactionFailure(action: PayloadAction<any, { error: string }>) {
+  const { error } = action.payload
+
+  if (!isUserDeniedSignature(error)) {
+    yield put(showToast(getMetaTransactionFailureToast()))
+  }
+}
+
+function isUserDeniedSignature(message: string) {
+  return message.indexOf('User denied message signature') !== -1
 }
