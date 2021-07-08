e9595b22a4e8cb258be4a64e102e3c3e23214dae
Nicolás Santángelo <santangelonicolas@gmail.com>
2021-07-08 14:10:19 -0300

    chore: lint

--- src/modules/item/export.ts
+++ src/modules/item/export.ts
@@ -2,7 +2,7 @@ import { Authenticator, AuthIdentity } from 'dcl-crypto'
 import { ChainId } from '@dcl/schemas'
 import { CatalystClient } from 'dcl-catalyst-client'
 import { EntityType } from 'dcl-catalyst-commons'
-import { builder, getContentsStorageUrl } from 'lib/api/builder'
+import { getContentsStorageUrl } from 'lib/api/builder'
 import { PEER_URL } from 'lib/api/peer'
 import { getCatalystItemURN } from 'modules/item/utils'
 import { makeContentFiles, computeHashes } from 'modules/deployment/contentUtils'
@@ -89,7 +89,7 @@ export async function calculateFinalSize(item: Item, newContents: Record<string,
   try {
     const image = await generateImage(item)
     imageSize = image.size
-  } catch (error) { }
+  } catch (error) {}
 
   const finalSize = imageSize + calculateFilesSize(blobs) + calculateFilesSize(newContents)
   return finalSize
