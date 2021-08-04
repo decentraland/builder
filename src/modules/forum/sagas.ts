b83da5935767fbba7ac485e83ba8cc535a07f912
Lautaro Petaccio <lausuper@gmail.com>
2021-08-04 17:39:13 -0300

    Use call

--- src/modules/forum/sagas.ts
+++ src/modules/forum/sagas.ts
@@ -21,14 +21,18 @@ export function* forumSaga() {
 function* handleCreateForumPostRequest(action: CreateCollectionForumPostRequestAction) {
   const { collection, forumPost } = action.payload
   try {
+    console.log('Creating forum post request')
     const forumLink: string = yield call([builder, 'createCollectionForumPost'], collection, forumPost)
     yield put(createCollectionForumPostSuccess(collection, forumLink))
   } catch (error) {
+    console.log('Failed forum post request', error)
     const forumPostAlreadyExists = error?.response?.status === 409
     const forumLink = error?.response?.data?.data?.forum_link
     if (forumPostAlreadyExists && forumLink) {
+      console.log('It was a 409')
       yield put(createCollectionForumPostSuccess(collection, error?.response?.data?.data?.forum_link))
     }
+    console.log('It wasnt a 409')
     yield put(createCollectionForumPostFailure(collection, forumPost, error.message))
   }
 }
@@ -41,7 +45,8 @@ function* handleCreateForumPostRequest(action: CreateCollectionForumPostRequestA
  */
 function* handleCreateForumPostFailure(action: CreateCollectionForumPostFailureAction) {
   const { collection, forumPost } = action.payload
+  console.log('Doing a retry')
 
-  yield delay(RETRY_DELAY)
+  yield call(delay, RETRY_DELAY)
   yield put(createCollectionForumPostRequest(collection, forumPost))
 }
