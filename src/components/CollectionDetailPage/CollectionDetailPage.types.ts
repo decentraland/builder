e52ad5b65d72b614e9aa8e1f487b3a785c664168
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-09-30 17:24:41 -0300

    chore: Remove unused authorizations from CollectionDetailPage

--- src/components/CollectionDetailPage/CollectionDetailPage.types.ts
+++ src/components/CollectionDetailPage/CollectionDetailPage.types.ts
@@ -1,6 +1,5 @@
 import { Dispatch } from 'redux'
 import { CallHistoryMethodAction } from 'connected-react-router'
-import { Authorization } from 'decentraland-dapps/dist/modules/authorization/types'
 import { Wallet } from 'decentraland-dapps/dist/modules/wallet/types'
 import { openModal, OpenModalAction } from 'modules/modal/actions'
 import { Collection } from 'modules/collection/types'
@@ -10,13 +9,12 @@ export type Props = {
   wallet: Wallet
   collection: Collection | null
   items: Item[]
-  authorizations: Authorization[]
   isOnSaleLoading: boolean
   isLoading: boolean
   onNavigate: (path: string) => void
   onOpenModal: typeof openModal
 }
 
-export type MapStateProps = Pick<Props, 'wallet' | 'collection' | 'items' | 'authorizations' | 'isOnSaleLoading' | 'isLoading'>
+export type MapStateProps = Pick<Props, 'wallet' | 'collection' | 'items' | 'isOnSaleLoading' | 'isLoading'>
 export type MapDispatchProps = Pick<Props, 'onNavigate' | 'onOpenModal'>
 export type MapDispatch = Dispatch<CallHistoryMethodAction | OpenModalAction>
