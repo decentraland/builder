173d0ec02b6dd6870a1752cc5bb77f561a2dd374
Juan Cazala <juan@decentraland.org>
2021-09-30 12:21:30 -0300

    fix: stats (#1584)
    * wipardo
    
    * refactor: cache promise and remove unused data from type
    
    * Update src/lib/api/builder.ts
    
    Co-authored-by: Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
    
    * Update src/lib/api/builder.ts
    
    Co-authored-by: Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
    
    * refactor: move analytics to its own client
    
    * chore: remove unnecessary param
    
    Co-authored-by: Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>

--- src/modules/stats/sagas.ts
+++ src/modules/stats/sagas.ts
@@ -1,5 +1,5 @@
-import { BuilderAPI } from 'lib/api/builder'
 import { call, put, takeEvery } from 'redux-saga/effects'
+import { analytics } from 'lib/api/analytics'
 import {
   fetchWeeklySceneStatsFailure,
   FetchWeeklySceneStatsRequestAction,
@@ -8,13 +8,13 @@ import {
 } from './action'
 import { WeeklyStats } from './types'
 
-export function* statsSaga(builder: BuilderAPI) {
+export function* statsSaga() {
   yield takeEvery(FETCH_WEEKLY_SCENE_STATS_REQUEST, handleFetchSceneStatsRequest)
 
   function* handleFetchSceneStatsRequest(action: FetchWeeklySceneStatsRequestAction) {
     const { base } = action.payload
     try {
-      const stats: WeeklyStats = yield call(() => builder.fetchWeeklyStats(base))
+      const stats: WeeklyStats = yield call(() => analytics.fetchWeeklyStats(base))
       yield put(fetchWeeklySceneStatsSuccess(base, stats))
     } catch (error) {
       yield put(fetchWeeklySceneStatsFailure(base, error.message))
