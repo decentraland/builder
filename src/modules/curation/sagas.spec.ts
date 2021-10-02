693c61066873acaabf23d86f482b72b0cf888a26
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-10-02 12:34:22 -0300

    refactor: Abstract mock builder

--- src/modules/curation/sagas.spec.ts
+++ src/modules/curation/sagas.spec.ts
@@ -1,4 +1,5 @@
 import { call } from '@redux-saga/core/effects'
+import { BuilderAPI } from 'lib/api/builder'
 import { expectSaga } from 'redux-saga-test-plan'
 import { throwError } from 'redux-saga-test-plan/providers'
 import {
@@ -17,12 +18,21 @@ import { curationSaga } from './sagas'
 const mockCollectionId = 'collectionId'
 const mockErrorMessage = 'Some Error'
 
+const mockBuilder = ({
+  fetchCurations: jest.fn(),
+  fetchCuration: jest.fn(),
+  pushCuration: jest.fn()
+} as any) as BuilderAPI
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
 describe('when fetching curations', () => {
   describe('when the api request fails', () => {
     it('should put the fetch curations fail action with an error', () => {
-      const builder = { fetchCurations: jest.fn() } as any
-      return expectSaga(curationSaga, builder)
-        .provide([[call([builder, builder.fetchCurations]), throwError(new Error(mockErrorMessage))]])
+      return expectSaga(curationSaga, mockBuilder)
+        .provide([[call([mockBuilder, mockBuilder.fetchCurations]), throwError(new Error(mockErrorMessage))]])
         .put(fetchCurationsFailure(mockErrorMessage))
         .dispatch(fetchCurationsRequest())
         .run({ silenceTimeout: true })
@@ -31,9 +41,8 @@ describe('when fetching curations', () => {
 
   describe('when the api request succeeds', () => {
     it('should put the fetch curations success action with the curations', () => {
-      const builder = { fetchCurations: jest.fn() } as any
-      return expectSaga(curationSaga, builder)
-        .provide([[call([builder, builder.fetchCurations]), [{}]]])
+      return expectSaga(curationSaga, mockBuilder)
+        .provide([[call([mockBuilder, mockBuilder.fetchCurations]), [{}]]])
         .put(fetchCurationsSuccess([{}] as any[]))
         .dispatch(fetchCurationsRequest())
         .run({ silenceTimeout: true })
@@ -44,9 +53,8 @@ describe('when fetching curations', () => {
 describe('when fetching a single curation', () => {
   describe('when the api request fails', () => {
     it('should put the fetch curation fail action with an error', () => {
-      const builder = { fetchCuration: jest.fn() } as any
-      return expectSaga(curationSaga, builder)
-        .provide([[call([builder, builder.fetchCuration], mockCollectionId), throwError(new Error(mockErrorMessage))]])
+      return expectSaga(curationSaga, mockBuilder)
+        .provide([[call([mockBuilder, mockBuilder.fetchCuration], mockCollectionId), throwError(new Error(mockErrorMessage))]])
         .put(fetchCurationFailure(mockErrorMessage))
         .dispatch(fetchCurationRequest(mockCollectionId))
         .run({ silenceTimeout: true })
@@ -55,9 +63,8 @@ describe('when fetching a single curation', () => {
 
   describe('when the api request succeeds', () => {
     it('should put the fetch curation success action with the curation', () => {
-      const builder = { fetchCuration: jest.fn() } as any
-      return expectSaga(curationSaga, builder)
-        .provide([[call([builder, builder.fetchCuration], mockCollectionId), {}]])
+      return expectSaga(curationSaga, mockBuilder)
+        .provide([[call([mockBuilder, mockBuilder.fetchCuration], mockCollectionId), {}]])
         .put(fetchCurationSuccess(mockCollectionId, {} as any))
         .dispatch(fetchCurationRequest(mockCollectionId))
         .run({ silenceTimeout: true })
@@ -68,9 +75,8 @@ describe('when fetching a single curation', () => {
 describe('when pushing a curation a new curation', () => {
   describe('when the api request fails', () => {
     it('should put the push curation fail action with an error', () => {
-      const builder = { pushCuration: jest.fn() } as any
-      return expectSaga(curationSaga, builder)
-        .provide([[call([builder, builder.pushCuration], mockCollectionId), throwError(new Error(mockErrorMessage))]])
+      return expectSaga(curationSaga, mockBuilder)
+        .provide([[call([mockBuilder, mockBuilder.pushCuration], mockCollectionId), throwError(new Error(mockErrorMessage))]])
         .put(pushCurationFailure(mockErrorMessage))
         .dispatch(pushCurationRequest(mockCollectionId))
         .run({ silenceTimeout: true })
@@ -79,9 +85,8 @@ describe('when pushing a curation a new curation', () => {
 
   describe('when the api request succeeds', () => {
     it('should put the push curation success and fetch curation request', () => {
-      const builder = { pushCuration: jest.fn() } as any
-      return expectSaga(curationSaga, builder)
-        .provide([[call([builder, builder.pushCuration], mockCollectionId), {}]])
+      return expectSaga(curationSaga, mockBuilder)
+        .provide([[call([mockBuilder, mockBuilder.pushCuration], mockCollectionId), {}]])
         .put(pushCurationSuccess())
         .put(fetchCurationRequest(mockCollectionId))
         .dispatch(pushCurationRequest(mockCollectionId))
