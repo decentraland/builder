98fb9eb013f065c4f6e41530010485387108e407
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-10-02 12:26:28 -0300

    chore: Add push curation saga tests

--- src/modules/curation/sagas.spec.ts
+++ src/modules/curation/sagas.spec.ts
@@ -7,17 +7,23 @@ import {
   fetchCurationsFailure,
   fetchCurationsRequest,
   fetchCurationsSuccess,
-  fetchCurationSuccess
+  fetchCurationSuccess,
+  pushCurationFailure,
+  pushCurationRequest,
+  pushCurationSuccess
 } from './actions'
 import { curationSaga } from './sagas'
 
+const mockCollectionId = 'collectionId'
+const mockErrorMessage = 'Some Error'
+
 describe('when fetching curations', () => {
   describe('when the api request fails', () => {
-    it('should put the fetch curations fail action with the error', () => {
+    it('should put the fetch curations fail action with an error', () => {
       const builder = { fetchCurations: jest.fn() } as any
       return expectSaga(curationSaga, builder)
-        .provide([[call([builder, builder.fetchCurations]), throwError(new Error('Some Error'))]])
-        .put(fetchCurationsFailure('Some Error'))
+        .provide([[call([builder, builder.fetchCurations]), throwError(new Error(mockErrorMessage))]])
+        .put(fetchCurationsFailure(mockErrorMessage))
         .dispatch(fetchCurationsRequest())
         .run({ silenceTimeout: true })
     })
@@ -37,12 +43,12 @@ describe('when fetching curations', () => {
 
 describe('when fetching a single curation', () => {
   describe('when the api request fails', () => {
-    it('should put the fetch curation fail action with the error', () => {
+    it('should put the fetch curation fail action with an error', () => {
       const builder = { fetchCuration: jest.fn() } as any
       return expectSaga(curationSaga, builder)
-        .provide([[call([builder, builder.fetchCuration], 'collectionId'), throwError(new Error('Some Error'))]])
-        .put(fetchCurationFailure('Some Error'))
-        .dispatch(fetchCurationRequest('collectionId'))
+        .provide([[call([builder, builder.fetchCuration], mockCollectionId), throwError(new Error(mockErrorMessage))]])
+        .put(fetchCurationFailure(mockErrorMessage))
+        .dispatch(fetchCurationRequest(mockCollectionId))
         .run({ silenceTimeout: true })
     })
   })
@@ -51,9 +57,34 @@ describe('when fetching a single curation', () => {
     it('should put the fetch curation success action with the curation', () => {
       const builder = { fetchCuration: jest.fn() } as any
       return expectSaga(curationSaga, builder)
-        .provide([[call([builder, builder.fetchCuration], 'collectionId'), {}]])
-        .put(fetchCurationSuccess('collectionId', {} as any))
-        .dispatch(fetchCurationRequest('collectionId'))
+        .provide([[call([builder, builder.fetchCuration], mockCollectionId), {}]])
+        .put(fetchCurationSuccess(mockCollectionId, {} as any))
+        .dispatch(fetchCurationRequest(mockCollectionId))
+        .run({ silenceTimeout: true })
+    })
+  })
+})
+
+describe('when pushing a curation a new curation', () => {
+  describe('when the api request fails', () => {
+    it('should put the push curation fail action with an error', () => {
+      const builder = { pushCuration: jest.fn() } as any
+      return expectSaga(curationSaga, builder)
+        .provide([[call([builder, builder.pushCuration], mockCollectionId), throwError(new Error(mockErrorMessage))]])
+        .put(pushCurationFailure(mockErrorMessage))
+        .dispatch(pushCurationRequest(mockCollectionId))
+        .run({ silenceTimeout: true })
+    })
+  })
+
+  describe('when the api request succeeds', () => {
+    it('should put the push curation success and fetch curation request', () => {
+      const builder = { pushCuration: jest.fn() } as any
+      return expectSaga(curationSaga, builder)
+        .provide([[call([builder, builder.pushCuration], mockCollectionId), {}]])
+        .put(pushCurationSuccess())
+        .put(fetchCurationRequest(mockCollectionId))
+        .dispatch(pushCurationRequest(mockCollectionId))
         .run({ silenceTimeout: true })
     })
   })
