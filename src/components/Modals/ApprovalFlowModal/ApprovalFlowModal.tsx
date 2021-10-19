e7e4edb698ed1186eaadd93ab3cb9e97f19a6953
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-10-19 14:39:14 -0300

    feat: Format bytes into their proper size in approval modal flow (#1623)
    * Add number lib with formatBytes function
    
    * Format Bytes in upload items approval flow step

--- src/components/Modals/ApprovalFlowModal/ApprovalFlowModal.tsx
+++ src/components/Modals/ApprovalFlowModal/ApprovalFlowModal.tsx
@@ -3,15 +3,15 @@ import * as React from 'react'
 import { Modal } from 'decentraland-dapps/dist/containers'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { Button, Center, Loader, ModalActions, ModalContent, ModalNavigation } from 'decentraland-ui'
-
 import ItemImage from 'components/ItemImage'
+import { formatBytes } from 'lib/number'
 import { ApprovalFlowModalMetadata, ApprovalFlowModalView, Props, State } from './ApprovalFlowModal.types'
+
 import './ApprovalFlowModal.css'
 
 export default class ApprovalFlowModal extends React.PureComponent<Props> {
-
   state: State = {
-    didRescue: false,
+    didRescue: false
   }
 
   renderHash(hash: string) {
@@ -19,14 +19,16 @@ export default class ApprovalFlowModal extends React.PureComponent<Props> {
   }
 
   renderLoadingView() {
-    return <>
-      <ModalNavigation title={t('approval_flow.loading.title')} subtitle={t('approval_flow.loading.subtitle')} />
-      <ModalContent className="loading">
-        <Center>
-          <Loader active size="huge" />
-        </Center>
-      </ModalContent>
-    </>
+    return (
+      <>
+        <ModalNavigation title={t('approval_flow.loading.title')} subtitle={t('approval_flow.loading.subtitle')} />
+        <ModalContent className="loading">
+          <Center>
+            <Loader active size="huge" />
+          </Center>
+        </ModalContent>
+      </>
+    )
   }
 
   renderRescueView() {
@@ -37,111 +39,150 @@ export default class ApprovalFlowModal extends React.PureComponent<Props> {
       this.setState({ didRescue: true })
       onRescueItems(collection, items, contentHashes)
     }
-    return <>
-      <ModalNavigation title={t('approval_flow.rescue.title')} subtitle={t('approval_flow.rescue.subtitle')} onClose={onClose} />
-      <ModalContent className="rescue">
-        {items.map((item, index) => (
-          <div className="item" key={item.id}>
-            <div className="name"><ItemImage item={item} />{item.name}</div>
-            <div className="hash" title={contentHashes[index]}>{this.renderHash(contentHashes[index])}</div>
-          </div>
-        ))}
-      </ModalContent>
-      <ModalActions>
-        <Button primary disabled={didRescue || isConfirmingRescueTx} loading={didRescue} onClick={onConfirm}>{t('approval_flow.rescue.confirm')}</Button>
-        <Button secondary onClick={onClose}>{t('global.cancel')}</Button>
-      </ModalActions>
-    </>
+    return (
+      <>
+        <ModalNavigation title={t('approval_flow.rescue.title')} subtitle={t('approval_flow.rescue.subtitle')} onClose={onClose} />
+        <ModalContent className="rescue">
+          {items.map((item, index) => (
+            <div className="item" key={item.id}>
+              <div className="name">
+                <ItemImage item={item} />
+                {item.name}
+              </div>
+              <div className="hash" title={contentHashes[index]}>
+                {this.renderHash(contentHashes[index])}
+              </div>
+            </div>
+          ))}
+        </ModalContent>
+        <ModalActions>
+          <Button primary disabled={didRescue || isConfirmingRescueTx} loading={didRescue} onClick={onConfirm}>
+            {t('approval_flow.rescue.confirm')}
+          </Button>
+          <Button secondary onClick={onClose}>
+            {t('global.cancel')}
+          </Button>
+        </ModalActions>
+      </>
+    )
   }
 
   renderDeployView() {
     const { onClose, metadata, onDeployItems, isDeployingItems } = this.props
     const { items, entities } = metadata as ApprovalFlowModalMetadata<ApprovalFlowModalView.DEPLOY>
-    return <>
-      <ModalNavigation title={t('approval_flow.upload.title')} subtitle={t('approval_flow.upload.subtitle')} onClose={onClose} />
-      <ModalContent className="deploy">
-        {items.map((item, index) =>
-          <div className="item" key={item.id}>
-            <div className="name"><ItemImage item={item} />{item.name}</div>
-            <div className="size">
-              {Array.from(
-                entities[index].files.values()
-              ).reduce<number>((size, file) => size + file.content.length, 0)
-                .toLocaleString()} bytes</div>
-          </div>
-        )}
-      </ModalContent>
-      <ModalActions>
-        <Button primary disabled={isDeployingItems} loading={isDeployingItems} onClick={() => onDeployItems(entities)}>{t('approval_flow.upload.confirm')}</Button>
-        <Button secondary onClick={onClose}>{t('global.cancel')}</Button>
-      </ModalActions>
-    </>
+    return (
+      <>
+        <ModalNavigation title={t('approval_flow.upload.title')} subtitle={t('approval_flow.upload.subtitle')} onClose={onClose} />
+        <ModalContent className="deploy">
+          {items.map((item, index) => (
+            <div className="item" key={item.id}>
+              <div className="name">
+                <ItemImage item={item} />
+                {item.name}
+              </div>
+              <div className="size">
+                {formatBytes(Array.from(entities[index].files.values()).reduce<number>((size, file) => size + file.content.length, 0))}
+              </div>
+            </div>
+          ))}
+        </ModalContent>
+        <ModalActions>
+          <Button primary disabled={isDeployingItems} loading={isDeployingItems} onClick={() => onDeployItems(entities)}>
+            {t('approval_flow.upload.confirm')}
+          </Button>
+          <Button secondary onClick={onClose}>
+            {t('global.cancel')}
+          </Button>
+        </ModalActions>
+      </>
+    )
   }
 
   renderApproveView() {
     const { onClose, metadata, onApproveCollection, isConfirmingApproveTx, isAwaitingApproveTx } = this.props
     const { collection } = metadata as ApprovalFlowModalMetadata<ApprovalFlowModalView.APPROVE>
-    return <>
-      <ModalNavigation title={t('approval_flow.approve.title')} subtitle={t('approval_flow.approve.subtitle')} onClose={onClose} />
-      <ModalActions>
-        <Button primary disabled={isConfirmingApproveTx || isAwaitingApproveTx} loading={isAwaitingApproveTx} onClick={() => onApproveCollection(collection)}>{t('approval_flow.approve.confirm')}</Button>
-        <Button secondary onClick={onClose}>{t('global.cancel')}</Button>
-      </ModalActions>
-    </>
+    return (
+      <>
+        <ModalNavigation title={t('approval_flow.approve.title')} subtitle={t('approval_flow.approve.subtitle')} onClose={onClose} />
+        <ModalActions>
+          <Button
+            primary
+            disabled={isConfirmingApproveTx || isAwaitingApproveTx}
+            loading={isAwaitingApproveTx}
+            onClick={() => onApproveCollection(collection)}
+          >
+            {t('approval_flow.approve.confirm')}
+          </Button>
+          <Button secondary onClick={onClose}>
+            {t('global.cancel')}
+          </Button>
+        </ModalActions>
+      </>
+    )
   }
 
   renderErrorView() {
     const { onClose, metadata } = this.props
     const { error } = metadata as ApprovalFlowModalMetadata<ApprovalFlowModalView.ERROR>
-    return <>
-      <ModalNavigation title={t('approval_flow.error.title')} subtitle={t('approval_flow.error.subtitle')} onClose={onClose} />
-      <ModalContent className="error">
-        {error}
-      </ModalContent>
-      <ModalActions>
-        <Button secondary onClick={onClose}>{t('global.close')}</Button>
-      </ModalActions>
-    </>
+    return (
+      <>
+        <ModalNavigation title={t('approval_flow.error.title')} subtitle={t('approval_flow.error.subtitle')} onClose={onClose} />
+        <ModalContent className="error">{error}</ModalContent>
+        <ModalActions>
+          <Button secondary onClick={onClose}>
+            {t('global.close')}
+          </Button>
+        </ModalActions>
+      </>
+    )
   }
 
   renderSuccessView() {
     const { onClose } = this.props
-    return <>
-      <ModalNavigation title={t('approval_flow.success.title')} subtitle={t('approval_flow.success.subtitle')} onClose={onClose} />
-      <ModalActions>
-        <Button secondary onClick={onClose}>{t('global.close')}</Button>
-      </ModalActions>
-    </>
+    return (
+      <>
+        <ModalNavigation title={t('approval_flow.success.title')} subtitle={t('approval_flow.success.subtitle')} onClose={onClose} />
+        <ModalActions>
+          <Button secondary onClick={onClose}>
+            {t('global.close')}
+          </Button>
+        </ModalActions>
+      </>
+    )
   }
 
   render() {
     const { name, onClose, metadata } = this.props
     const { view } = metadata as ApprovalFlowModalMetadata
     let content: React.ReactNode
-    let size: string = "small"
+    let size: string = 'small'
     switch (view) {
       case ApprovalFlowModalView.LOADING:
-        content = this.renderLoadingView();
-        size = "tiny"
+        content = this.renderLoadingView()
+        size = 'tiny'
         break
       case ApprovalFlowModalView.RESCUE:
-        content = this.renderRescueView();
+        content = this.renderRescueView()
         break
       case ApprovalFlowModalView.DEPLOY:
-        content = this.renderDeployView();
+        content = this.renderDeployView()
         break
       case ApprovalFlowModalView.APPROVE:
-        content = this.renderApproveView();
-        size = "tiny"
+        content = this.renderApproveView()
+        size = 'tiny'
         break
       case ApprovalFlowModalView.ERROR:
         content = this.renderErrorView()
         break
       case ApprovalFlowModalView.SUCCESS:
         content = this.renderSuccessView()
-        size = "tiny"
+        size = 'tiny'
         break
     }
-    return <Modal name={name} onClose={onClose} className="ApprovalFlowModal" size={size}>{content}</Modal>
+    return (
+      <Modal name={name} onClose={onClose} className="ApprovalFlowModal" size={size}>
+        {content}
+      </Modal>
+    )
   }
 }
