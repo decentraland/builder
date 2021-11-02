8dab808abd30d6b5fafb6582bfb613b1cbf96c95
Nicolás Santángelo <santangelonicolas@gmail.com>
2021-11-02 16:19:02 -0300

    chore: typo

--- src/modules/thirdParty/actions.ts
+++ src/modules/thirdParty/actions.ts
@@ -3,9 +3,9 @@ import { ThirdParty } from './types'
 
 // Fetch Third Party Records
 
-export const FETCH_THIRD_PARTIES_REQUEST = '[Request] Fetch Thrid Parties'
-export const FETCH_THIRD_PARTIES_SUCCESS = '[Success] Fetch Thrid Parties'
-export const FETCH_THIRD_PARTIES_FAILURE = '[Failure] Fetch Thrid Parties'
+export const FETCH_THIRD_PARTIES_REQUEST = '[Request] Fetch Third Parties'
+export const FETCH_THIRD_PARTIES_SUCCESS = '[Success] Fetch Third Parties'
+export const FETCH_THIRD_PARTIES_FAILURE = '[Failure] Fetch Third Parties'
 
 export const fetchThirdPartiesRequest = (address?: string) => action(FETCH_THIRD_PARTIES_REQUEST, { address })
 export const fetchThirdPartiesSuccess = (thirdParties: ThirdParty[]) => action(FETCH_THIRD_PARTIES_SUCCESS, { thirdParties })
