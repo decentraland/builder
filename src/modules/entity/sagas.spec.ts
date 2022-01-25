2fd7b1f20c281a36b991ab05b6a855d6624f3620
Juan Cazala <juan@decentraland.org>
2022-01-25 04:30:22 -0300

    fix: use fetchEntitiesByPointers instead of fetchAllDeployments (#1719)
    * fix: use fetchEntitiesByPointers instead of fetchAllDeployments
    
    * chore: added tests
    
    * chore: asset entire state.data

--- src/modules/entity/sagas.spec.ts
+++ src/modules/entity/sagas.spec.ts
@@ -1,50 +1,102 @@
-import { CatalystClient, DeploymentOptions, DeploymentWithMetadataContentAndPointers } from 'dcl-catalyst-client'
-import { EntityType } from 'dcl-catalyst-commons'
+import { CatalystClient } from 'dcl-catalyst-client'
+import { Entity, EntityType, EntityVersion } from 'dcl-catalyst-commons'
 import { expectSaga } from 'redux-saga-test-plan'
 import { call } from 'redux-saga/effects'
-import { fetchEntitiesFailure, fetchEntitiesRequest, fetchEntitiesSuccess } from './actions'
+import {
+  fetchEntitiesByIdsFailure,
+  fetchEntitiesByIdsRequest,
+  fetchEntitiesByIdsSuccess,
+  fetchEntitiesByPointersFailure,
+  fetchEntitiesByPointersRequest,
+  fetchEntitiesByPointersSuccess
+} from './actions'
 import { entitySaga } from './sagas'
 
 describe('Entity sagas', () => {
-  describe('when handling the fetch entities action', () => {
+  describe('when handling the FETCH_ENTITIES_BY_POINTERS_REQUEST action', () => {
     const client = ({
-      fetchAllDeployments: jest.fn()
+      fetchEntitiesByPointers: jest.fn()
     } as unknown) as CatalystClient
 
     it('should dispatch a failue action if the client throws', () => {
-      const options: DeploymentOptions<DeploymentWithMetadataContentAndPointers> = { filters: { pointers: ['aPointer', 'anotherPointer'] } }
+      const pointers = ['aPointer', 'anotherPointer']
       const anErrorMessage = 'Something happened'
       return expectSaga(entitySaga, client)
-        .provide([[call([client, 'fetchAllDeployments'], options), Promise.reject(new Error(anErrorMessage))]])
-        .put(fetchEntitiesFailure(anErrorMessage, options))
-        .dispatch(fetchEntitiesRequest(options))
+        .provide([[call([client, 'fetchEntitiesByPointers'], EntityType.WEARABLE, pointers), Promise.reject(new Error(anErrorMessage))]])
+        .put(fetchEntitiesByPointersFailure(EntityType.WEARABLE, pointers, anErrorMessage))
+        .dispatch(fetchEntitiesByPointersRequest(EntityType.WEARABLE, pointers))
         .run({ silenceTimeout: true })
     })
 
     it('should dispatch a success action if the response returns successfully', () => {
-      const options: DeploymentOptions<DeploymentWithMetadataContentAndPointers> = { filters: { pointers: ['aPointer', 'anotherPointer'] } }
-      const entities: DeploymentWithMetadataContentAndPointers[] = [
+      const pointers = ['aPointer', 'anotherPointer']
+      const entities: Entity[] = [
         {
-          entityId: 'Qmhash',
-          deployedBy: '0xhash',
-          entityTimestamp: 1234,
-          entityType: EntityType.WEARABLE,
+          id: 'Qmhash',
+          timestamp: 1234,
+          type: EntityType.WEARABLE,
           pointers: ['aPointer'],
           content: [
             {
               hash: 'Qmhash',
-              key: 'pepito.jpg'
+              file: 'pepito.jpg'
             }
           ],
           metadata: {
+            owner: '0xpepito',
             some: 'thing'
-          }
+          },
+          version: EntityVersion.V3
         }
       ]
       return expectSaga(entitySaga, client)
-        .provide([[call([client, 'fetchAllDeployments'], options), Promise.resolve(entities)]])
-        .put(fetchEntitiesSuccess(entities, options))
-        .dispatch(fetchEntitiesRequest(options))
+        .provide([[call([client, 'fetchEntitiesByPointers'], EntityType.WEARABLE, pointers), Promise.resolve(entities)]])
+        .put(fetchEntitiesByPointersSuccess(EntityType.WEARABLE, pointers, entities))
+        .dispatch(fetchEntitiesByPointersRequest(EntityType.WEARABLE, pointers))
+        .run({ silenceTimeout: true })
+    })
+  })
+
+  describe('when handling the FETCH_ENTITIES_BY_IDS_REQUEST action', () => {
+    const client = ({
+      fetchEntitiesByIds: jest.fn()
+    } as unknown) as CatalystClient
+
+    it('should dispatch a failue action if the client throws', () => {
+      const ids = ['QmHash']
+      const anErrorMessage = 'Something happened'
+      return expectSaga(entitySaga, client)
+        .provide([[call([client, 'fetchEntitiesByIds'], EntityType.WEARABLE, ids), Promise.reject(new Error(anErrorMessage))]])
+        .put(fetchEntitiesByIdsFailure(EntityType.WEARABLE, ids, anErrorMessage))
+        .dispatch(fetchEntitiesByIdsRequest(EntityType.WEARABLE, ids))
+        .run({ silenceTimeout: true })
+    })
+
+    it('should dispatch a success action if the response returns successfully', () => {
+      const ids = ['QmHash']
+      const entities: Entity[] = [
+        {
+          id: 'Qmhash',
+          timestamp: 1234,
+          type: EntityType.WEARABLE,
+          pointers: ['aPointer'],
+          content: [
+            {
+              hash: 'Qmhash',
+              file: 'pepito.jpg'
+            }
+          ],
+          metadata: {
+            owner: '0xpepito',
+            some: 'thing'
+          },
+          version: EntityVersion.V3
+        }
+      ]
+      return expectSaga(entitySaga, client)
+        .provide([[call([client, 'fetchEntitiesByIds'], EntityType.WEARABLE, ids), Promise.resolve(entities)]])
+        .put(fetchEntitiesByIdsSuccess(EntityType.WEARABLE, ids, entities))
+        .dispatch(fetchEntitiesByIdsRequest(EntityType.WEARABLE, ids))
         .run({ silenceTimeout: true })
     })
   })
