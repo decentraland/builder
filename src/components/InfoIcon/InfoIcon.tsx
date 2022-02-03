be5221c7ea748d0dac6783671e8b7d20f9333903
Lautaro Petaccio <lausuper@gmail.com>
2022-02-03 17:02:46 -0300

    fix: Remove omit

--- src/components/InfoIcon/InfoIcon.tsx
+++ src/components/InfoIcon/InfoIcon.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react'
 import classnames from 'classnames'
-import { omit } from 'decentraland-commons/dist/utils'
 import styles from './InfoIcon.module.css'
 import { Props } from './InfoIcon.types'
 
 export default class InfoIcon extends React.PureComponent<Props> {
   render() {
     const { className } = this.props
-    return <i {...omit(this.props, ['className'])} className={classnames(styles.info, className)} />
+    return <i {...this.props} className={classnames(styles.info, className)} />
   }
 }
