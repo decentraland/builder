f2da7544301beb8f1515c14101e5e2f496f59d97
Lautaro Petaccio <lausuper@gmail.com>
2022-02-03 16:56:20 -0300

    fix: This PR fixes the popups

--- src/components/InfoIcon/InfoIcon.tsx
+++ src/components/InfoIcon/InfoIcon.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import classnames from 'classnames'
+import { omit } from 'decentraland-commons/dist/utils'
 import styles from './InfoIcon.module.css'
 import { Props } from './InfoIcon.types'
 
 export default class InfoIcon extends React.PureComponent<Props> {
   render() {
     const { className } = this.props
-    return <i className={classnames(styles.info, className)} />
+    return <i {...omit(this.props, ['className'])} className={classnames(styles.info, className)} />
   }
 }
