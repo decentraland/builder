47601764377fee7cde6ed9e253fc922cb80dcab9
Juanma Hidalgo <juanma06@gmail.com>
2022-02-17 10:26:30 +0100

    feat: Use getCollectionType instead of isThirdParty helper

--- src/components/CollectionsPage/CollectionCard/CollectionCard.dnd.ts
+++ src/components/CollectionsPage/CollectionCard/CollectionCard.dnd.ts
@@ -1,5 +1,6 @@
 import { ConnectDropTarget, DropTargetSpec } from 'react-dnd'
-import { isThirdParty } from 'lib/urn'
+import { CollectionType } from 'modules/collection/types'
+import { getCollectionType } from 'modules/collection/utils'
 import { ItemCardDragObject } from '../ItemCard/ItemCard.dnd'
 import { Props } from './CollectionCard.types'
 
@@ -15,7 +16,8 @@ export const collectionTarget: DropTargetSpec<Props> = {
     props.onSetCollection(item, props.collection.id)
   },
   canDrop(props) {
-    return !props.collection.isPublished && !isThirdParty(props.collection.urn)
+    const isThirdParty = getCollectionType(props.collection) === CollectionType.THIRD_PARTY
+    return !props.collection.isPublished && !isThirdParty
   }
 }
 
