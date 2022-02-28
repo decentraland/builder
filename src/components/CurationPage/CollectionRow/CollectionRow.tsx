b4ca0a8c1bccf29052c7e6515dace30276bb9e01
Juanma Hidalgo <juanma06@gmail.com>
2022-02-28 16:11:20 -0500

    feat: Add the collection type column to the curation table (#1811)
    * feat: Add the collection type column to the curation table
    
    * feat: add - to TP owner column
    
    * chore: change translation keys

--- src/components/CurationPage/CollectionRow/CollectionRow.tsx
+++ src/components/CurationPage/CollectionRow/CollectionRow.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { format } from 'date-fns'
-import { Grid, Icon } from 'decentraland-ui'
-import { Link } from 'react-router-dom'
+import { Icon, Table } from 'decentraland-ui'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { locations } from 'routing/locations'
-import { hasReviews } from 'modules/collection/utils'
+import { getCollectionType, hasReviews } from 'modules/collection/utils'
+import { CollectionType } from 'modules/collection/types'
 import CollectionStatus from 'components/CollectionStatus'
 import CollectionImage from 'components/CollectionImage'
 import Profile from 'components/Profile'
@@ -18,6 +18,12 @@ export default class CollectionRow extends React.PureComponent<Props> {
     const { collection } = this.props
     window.open(collection.forumLink!, '_blank')
     event.preventDefault()
+    event.stopPropagation()
+  }
+
+  handleTableRowClick = () => {
+    const { onNavigate, collection } = this.props
+    onNavigate(locations.itemEditor({ collectionId: collection.id, isReviewing: 'true' }))
   }
 
   renderCurationState = () => {
@@ -61,51 +67,50 @@ export default class CollectionRow extends React.PureComponent<Props> {
     const createdAtDate = new Date(curation?.createdAt || collection.createdAt)
 
     return (
-      <Link className="CollectionRow" to={locations.itemEditor({ collectionId: collection.id, isReviewing: 'true' })}>
-        <Grid>
-          <Grid.Row>
-            <Grid.Column width={5}>
-              <div className="image-column">
-                <CollectionImage collectionId={collection.id} />
-                <div className="info">
-                  <div className="title">
-                    <CollectionStatus collection={collection} />
-                    {collection.name}
-                  </div>
-                  <div className="subtitle name">{t('collection_row.items', { count: items.length })}</div>
-                </div>
+      <Table.Row className="CollectionRow" key={collection.id} onClick={this.handleTableRowClick}>
+        <Table.Cell width={5}>
+          <div className="image-column">
+            <CollectionImage collectionId={collection.id} />
+            <div className="info">
+              <div className="title">
+                <CollectionStatus collection={collection} />
+                {collection.name}
               </div>
-            </Grid.Column>
-            <Grid.Column width={4}>
-              <div className="title">{t('collection_row.owner')}</div>
-              <div className="subtitle">
-                <Profile textOnly address={collection.owner} />
-              </div>
-            </Grid.Column>
-            <Grid.Column width={2}>
-              <div className="title">{t('collection_row.forum_post')}</div>
-              <div className="subtitle">
-                {collection.forumLink ? (
-                  <span className="link" onClick={this.handleNavigateToForum}>
-                    {t('collection_row.visit')}
-                  </span>
-                ) : (
-                  t('collection_row.no_forum_post')
-                )}
-              </div>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <div className="title">{t(curation ? 'collection_row.review_request_date' : 'collection_row.publication_date')}</div>
-              <div className="subtitle" title={format(createdAtDate, 'd MMMM yyyy HH:mm')}>
-                {formatDistanceToNow(createdAtDate, { addSuffix: true })}
-              </div>
-            </Grid.Column>
-            <Grid.Column width={2}>
-              <div className="actions">{this.renderCurationState()}</div>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Link>
+              <div className="subtitle name">{t('collection_row.items', { count: items.length })}</div>
+            </div>
+          </div>
+        </Table.Cell>
+        <Table.Cell width={2}>
+          <div>
+            {getCollectionType(collection) === CollectionType.THIRD_PARTY
+              ? t('collection_row.type_third_party')
+              : t('collection_row.type_standard')}
+          </div>
+        </Table.Cell>
+        <Table.Cell width={3}>
+          <div>{getCollectionType(collection) === CollectionType.THIRD_PARTY ? '-' : <Profile textOnly address={collection.owner} />}</div>
+        </Table.Cell>
+        <Table.Cell width={3}>
+          <div title={format(createdAtDate, 'd MMMM yyyy HH:mm')}>
+            <span>{t(curation ? 'collection_row.review_request' : 'collection_row.published')}</span>{' '}
+            {formatDistanceToNow(createdAtDate, { addSuffix: true })}
+          </div>
+        </Table.Cell>
+        <Table.Cell width={2}>
+          <div>
+            {collection.forumLink ? (
+              <span className="link" onClick={this.handleNavigateToForum}>
+                {t('collection_row.link')}
+              </span>
+            ) : (
+              t('collection_row.no_forum_post')
+            )}
+          </div>
+        </Table.Cell>
+        <Table.Cell width={2}>
+          <div className="actions">{this.renderCurationState()}</div>
+        </Table.Cell>
+      </Table.Row>
     )
   }
 }
--- src/components/CurationPage/CollectionRow/CollectionRow.types.ts
+++ src/components/CurationPage/CollectionRow/CollectionRow.types.ts
@@ -7,9 +7,10 @@ export type Props = {
   collection: Collection
   curation: CollectionCuration | null
   items: Item[]
+  onNavigate: (path: string) => void
 }
 
 export type MapStateProps = Pick<Props, 'items'>
-export type MapDispatchProps = {}
+export type MapDispatchProps = Pick<Props, 'onNavigate'>
 export type MapDispatch = Dispatch
 export type OwnProps = Pick<Props, 'collection' | 'curation'>
