6eb886346e25b255cfe4c502854432ba80d8ca39
Juanma Hidalgo <juanma06@gmail.com>
2022-02-28 12:57:49 -0500

    feat: Add the collection type column to the curation table

--- src/components/CurationPage/CollectionRow/CollectionRow.container.ts
+++ src/components/CurationPage/CollectionRow/CollectionRow.container.ts
@@ -1,13 +1,16 @@
 import { connect } from 'react-redux'
+import { push } from 'connected-react-router'
 import { RootState } from 'modules/common/types'
 import { getCollectionItems } from 'modules/item/selectors'
-import { OwnProps, MapStateProps, MapDispatchProps } from './CollectionRow.types'
+import { OwnProps, MapStateProps, MapDispatchProps, MapDispatch } from './CollectionRow.types'
 import CollectionRow from './CollectionRow'
 
 const mapState = (state: RootState, ownProps: OwnProps): MapStateProps => ({
   items: getCollectionItems(state, ownProps.collection.id)
 })
 
-const mapDispatch = (): MapDispatchProps => ({})
+const mapDispatch = (dispatch: MapDispatch): MapDispatchProps => ({
+  onNavigate: path => dispatch(push(path))
+})
 
 export default connect(mapState, mapDispatch)(CollectionRow)
