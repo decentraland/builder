7cb6e4b21438fe446dd4b80162defdb1fdd0b4e4
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-03-18 15:21:42 -0300

    chore: Remove OS in favor of builder client

--- src/modules/asset/sagas.ts
+++ src/modules/asset/sagas.ts
@@ -6,57 +6,69 @@ import {
   loadCollectiblesRequest,
   loadCollectiblesFailure
 } from './actions'
-import { Asset, OpenSeaAsset } from './types'
+import { Asset } from './types'
 import { COLLECTIBLE_ASSET_PACK_ID } from 'modules/ui/sidebar/utils'
 import { CONNECT_WALLET_SUCCESS } from 'decentraland-dapps/dist/modules/wallet/actions'
 import { getAddress } from 'decentraland-dapps/dist/modules/wallet/selectors'
-import { opensea } from 'lib/api/opensea'
 import { TRANSPARENT_PIXEL } from 'lib/getModelData'
+import { BuilderClient, NFT } from '@dcl/builder-client'
+import { sleep } from 'decentraland-commons/dist/utils'
 
-export function* assetSaga() {
+export function* assetSaga(client: BuilderClient) {
   yield takeLatest(LOAD_COLLECTIBLES_REQUEST, handleLoadCollectibles)
   yield takeLatest(CONNECT_WALLET_SUCCESS, handleConnectWallet)
-}
 
-function* handleConnectWallet() {
-  yield put(loadCollectiblesRequest())
-}
+  function* handleConnectWallet() {
+    yield put(loadCollectiblesRequest())
+  }
 
-function* handleLoadCollectibles(_: LoadCollectiblesRequestAction) {
-  const address: string | null = yield select(getAddress)
+  function* handleLoadCollectibles(_: LoadCollectiblesRequestAction) {
+    const address: string | null = yield select(getAddress)
 
-  try {
-    if (!address) {
-      throw new Error(`Invalid address: ${address}`)
+    try {
+      if (!address) {
+        throw new Error(`Invalid address: ${address}`)
+      }
+      const assets: Asset[] = []
+      const serverNFTs: NFT[] = yield call(getNFTs, address)
+      for (const openseaAsset of serverNFTs) {
+        const uri = `ethereum://${openseaAsset.contract.address}/${openseaAsset.tokenId}`
+        assets.push({
+          assetPackId: COLLECTIBLE_ASSET_PACK_ID,
+          id: uri,
+          tags: [],
+          category: openseaAsset.contract.name,
+          contents: {},
+          name: openseaAsset.name || '',
+          model: uri,
+          script: null,
+          thumbnail: openseaAsset.imageThumbnailUrl || TRANSPARENT_PIXEL,
+          metrics: {
+            triangles: 0,
+            materials: 0,
+            meshes: 0,
+            bodies: 0,
+            entities: 0,
+            textures: 0
+          },
+          parameters: [],
+          actions: []
+        })
+      }
+      yield put(loadCollectiblesSuccess(assets))
+    } catch (error) {
+      yield put(loadCollectiblesFailure(error.message))
     }
-    const assets: Asset[] = []
-    const openseaAssets: OpenSeaAsset[] = yield call(() => opensea.fetchAssets(address))
-    for (const openseaAsset of openseaAssets) {
-      const uri = `ethereum://${openseaAsset.asset_contract.address}/${openseaAsset.token_id}`
-      assets.push({
-        assetPackId: COLLECTIBLE_ASSET_PACK_ID,
-        id: uri,
-        tags: [],
-        category: openseaAsset.asset_contract.name,
-        contents: {},
-        name: openseaAsset.name || '',
-        model: uri,
-        script: null,
-        thumbnail: openseaAsset.image_thumbnail_url || TRANSPARENT_PIXEL,
-        metrics: {
-          triangles: 0,
-          materials: 0,
-          meshes: 0,
-          bodies: 0,
-          entities: 0,
-          textures: 0
-        },
-        parameters: [],
-        actions: []
-      })
+  }
+
+  async function getNFTs(owner: string, cursor?: string) {
+    const response = await client.getNFTs({ owner, cursor })
+
+    if (response.next) {
+      await sleep(400)
+      return [...response.nfts, client.getNFTs({ owner, cursor: response.next })]
     }
-    yield put(loadCollectiblesSuccess(assets))
-  } catch (error) {
-    yield put(loadCollectiblesFailure(error.message))
+
+    return response.nfts
   }
 }
