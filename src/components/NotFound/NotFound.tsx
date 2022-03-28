e7d754812e9d4e04140663f4436786b86c40c977
Juani Gallo <ji.gallo93@gmail.com>
2022-03-28 19:01:20 -0300

    Refactoring `not found` pages to functional components (#1762)
    * Base migration from class to functional components in NotFound components
    
    * Removing code that is not neccesary after refactor
    
    * Fixing PR comment
    
    Co-authored-by: Nicolas Santangelo <santangelonicolas@gmail.com>

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -2,12 +2,10 @@ import * as React from 'react'
 import { Center } from 'decentraland-ui'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 
-export default class NotFound extends React.PureComponent {
-  render() {
-    return (
-      <Center>
-        <span className="secondary-text">{t('global.not_found')}&hellip;</span>
-      </Center>
-    )
-  }
+export default function NotFound() {
+  return (
+    <Center>
+      <span className="secondary-text">{t('global.not_found')}&hellip;</span>
+    </Center>
+  )
 }
