88a59d077ec973ebe5fa64c7b25ee1288be78752
Juanma Hidalgo <juanma06@gmail.com>
2022-04-08 13:38:41 +0200

    fix: Allow TP manager to edit TP items

--- src/components/ItemEditorPage/RightPanel/RightPanel.container.ts
+++ src/components/ItemEditorPage/RightPanel/RightPanel.container.ts
@@ -5,7 +5,7 @@ import { getItem, getError as getItemError, isDownloading } from 'modules/item/s
 import { deleteItemRequest, downloadItemRequest, saveItemRequest, setCollection } from 'modules/item/actions'
 import { openModal } from 'modules/modal/actions'
 import { getSelectedItemId } from 'modules/location/selectors'
-import { getCollection } from 'modules/collection/selectors'
+import { getCollection, hasViewAndEditRights } from 'modules/collection/selectors'
 import { isWalletCommitteeMember } from 'modules/committee/selectors'
 import { MapStateProps, MapDispatchProps, MapDispatch } from './RightPanel.types'
 import RightPanel from './RightPanel'
@@ -14,12 +14,13 @@ const mapState = (state: RootState): MapStateProps => {
   const selectedItemId = getSelectedItemId(state) || ''
   const selectedItem = getItem(state, selectedItemId)
   const address = getAddress(state) || ''
-
+  const collection = selectedItemId && selectedItem && selectedItem.collectionId ? getCollection(state, selectedItem.collectionId) : null
   return {
     address,
-    collection: selectedItemId && selectedItem && selectedItem.collectionId ? getCollection(state, selectedItem.collectionId) : null,
+    collection,
     selectedItem,
     selectedItemId,
+    canEditSelectedItem: !!selectedItem && !!collection && hasViewAndEditRights(state, address, collection),
     error: getItemError(state),
     isConnected: isConnected(state),
     isDownloading: isDownloading(state),
