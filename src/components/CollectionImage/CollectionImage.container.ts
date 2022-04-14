a2f0e6c27c3ce3d7de67aa79f236c8167d0890d3
Juanma Hidalgo <juanma06@gmail.com>
2022-04-14 13:15:15 +0200

    feat: Add logic to fetch collection items for images if needed (#1926)
    * feat: Add logic to fetch collection items for images if needed
    
    * feat: Remove CollectionProvider in favor of fetch prop

--- src/components/CollectionImage/CollectionImage.container.ts
+++ src/components/CollectionImage/CollectionImage.container.ts
@@ -2,17 +2,19 @@ import { connect } from 'react-redux'
 import { isLoadingType } from 'decentraland-dapps/dist/modules/loading/selectors'
 import { RootState } from 'modules/common/types'
 import { getCollectionItems, getLoading as getLoadingItem } from 'modules/item/selectors'
-import { getLoading as getLoadingCollection } from 'modules/collection/selectors'
+import { getCollection, getLoading as getLoadingCollection } from 'modules/collection/selectors'
 import { FETCH_COLLECTIONS_REQUEST } from 'modules/collection/actions'
-import { FETCH_COLLECTION_ITEMS_REQUEST, FETCH_ITEMS_REQUEST } from 'modules/item/actions'
-import { OwnProps, MapStateProps } from './CollectionImage.types'
+import { fetchCollectionItemsRequest, FETCH_COLLECTION_ITEMS_REQUEST, FETCH_ITEMS_REQUEST } from 'modules/item/actions'
+import { OwnProps, MapStateProps, MapDispatch, MapDispatchProps } from './CollectionImage.types'
 import CollectionImage from './CollectionImage'
 
 const mapState = (state: RootState, ownProps: OwnProps): MapStateProps => {
   const { collectionId } = ownProps
+  const itemCount = getCollection(state, collectionId)?.itemCount
   const items = getCollectionItems(state, collectionId)
   return {
     items,
+    itemCount,
     isLoading:
       isLoadingType(getLoadingCollection(state), FETCH_COLLECTIONS_REQUEST) ||
       isLoadingType(getLoadingItem(state), FETCH_ITEMS_REQUEST) ||
@@ -20,6 +22,8 @@ const mapState = (state: RootState, ownProps: OwnProps): MapStateProps => {
   }
 }
 
-const mapDispatch = () => ({})
+const mapDispatch = (dispatch: MapDispatch): MapDispatchProps => ({
+  onFetchCollectionItems: (id, page, limit) => dispatch(fetchCollectionItemsRequest(id, page, limit))
+})
 
 export default connect(mapState, mapDispatch)(CollectionImage)
--- src/lib/api/pagination.ts
+++ src/lib/api/pagination.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_PAGE = 1
-export const DEFAULT_PAGE_SIZE = 20
+export const DEFAULT_PAGE_SIZE = 60
 
 export type PaginationStats = {
   total: number
