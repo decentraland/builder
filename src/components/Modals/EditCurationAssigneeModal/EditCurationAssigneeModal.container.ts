c808c5e4f20d98f5d6037a2284076585a2daeb00
Juanma Hidalgo <juanma06@gmail.com>
2022-04-28 17:30:55 +0200

    fix: Removes Profile JSX for text so the user can autocomplete

--- src/components/Modals/EditCurationAssigneeModal/EditCurationAssigneeModal.container.ts
+++ src/components/Modals/EditCurationAssigneeModal/EditCurationAssigneeModal.container.ts
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux'
 import { getAddress } from 'decentraland-dapps/dist/modules/wallet/selectors'
 import { isLoadingType } from 'decentraland-dapps/dist/modules/loading/selectors'
+import { getData as getProfiles, getLoading as getProfilesLoading } from 'decentraland-dapps/dist/modules/profile/selectors'
+import { LOAD_PROFILE_REQUEST } from 'decentraland-dapps/dist/modules/profile/actions'
 import { RootState } from 'modules/common/types'
 import { getCommitteeMembers } from 'modules/committee/selectors'
 import {
@@ -13,10 +15,13 @@ import { MapDispatch, MapDispatchProps, OwnProps, MapState } from './EditCuratio
 import EditCurationAssigneeModal from './EditCurationAssigneeModal'
 
 const mapState = (state: RootState, ownProps: OwnProps): MapState => ({
+  profiles: getProfiles(state),
   collection: getCollection(state, ownProps.metadata.collectionId),
   curation: getCurationsByCollectionId(state)[ownProps.metadata.collectionId],
   committeeMembers: getCommitteeMembers(state),
-  isLoading: isLoadingType(getLoading(state), SET_COLLECTION_CURATION_ASSIGNEE_REQUEST),
+  isLoading:
+    isLoadingType(getLoading(state), SET_COLLECTION_CURATION_ASSIGNEE_REQUEST) ||
+    isLoadingType(getProfilesLoading(state), LOAD_PROFILE_REQUEST),
   address: getAddress(state)
 })
 
--- src/components/Modals/EditCurationAssigneeModal/EditCurationAssigneeModal.types.ts
+++ src/components/Modals/EditCurationAssigneeModal/EditCurationAssigneeModal.types.ts
@@ -1,5 +1,6 @@
 import { Dispatch } from 'redux'
 import { ModalProps } from 'decentraland-dapps/dist/providers/ModalProvider/ModalProvider.types'
+import { Profile } from 'decentraland-dapps/dist/modules/profile/types'
 import { setCollectionCurationAssigneeRequest } from 'modules/curations/collectionCuration/actions'
 import { CollectionCuration } from 'modules/curations/collectionCuration/types'
 import { Collection } from 'modules/collection/types'
@@ -10,6 +11,7 @@ export enum AssignModalOperationType {
 }
 
 export type Props = Omit<ModalProps, 'metadata'> & {
+  profiles: Record<string, Profile>
   collection: Collection | null
   curation: CollectionCuration | null
   isLoading: boolean
@@ -26,7 +28,7 @@ export type State = {
   assignee: string | null
 }
 
-export type MapState = Pick<Props, 'address' | 'isLoading' | 'committeeMembers' | 'curation' | 'collection'>
+export type MapState = Pick<Props, 'profiles' | 'address' | 'isLoading' | 'committeeMembers' | 'curation' | 'collection'>
 export type MapDispatch = Dispatch
 export type MapDispatchProps = Pick<Props, 'onSetAssignee'>
 export type OwnProps = Pick<Props, 'metadata'>
