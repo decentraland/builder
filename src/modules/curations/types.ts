1d8ded9de4fb38a8af0c6660c7d49da964e5e6fc
Juanma Hidalgo <juanma06@gmail.com>
2022-04-28 17:05:00 +0200

    feat: Add filter by assignee and order by relevance

--- src/modules/curations/types.ts
+++ src/modules/curations/types.ts
@@ -1,7 +1,10 @@
 export enum CurationStatus {
+  UNDER_REVIEW = 'under_review',
+  TO_REVIEW = 'to_review',
   PENDING = 'pending',
   APPROVED = 'approved',
-  REJECTED = 'rejected'
+  REJECTED = 'rejected',
+  DISABLED = 'disabled'
 }
 
 export type BaseCuration = {
