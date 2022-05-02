bfdc7aa2018bc6a534c43909e5bede0d9954ed3c
Juanma Hidalgo <juanma06@gmail.com>
2022-05-02 15:18:45 +0200

    feat: Sorts the assignees list

--- src/components/CurationPage/CurationPage.tsx
+++ src/components/CurationPage/CurationPage.tsx
@@ -59,7 +59,7 @@ export default class CurationPage extends React.PureComponent<Props, State> {
   }
 
   renderAssigneeFilterDropdown = () => {
-    const { committeeMembers } = this.props
+    const { committeeMembers, wallet } = this.props
     const { assigneeFilter } = this.state
     return (
       <Dropdown
@@ -67,7 +67,13 @@ export default class CurationPage extends React.PureComponent<Props, State> {
         value={assigneeFilter}
         options={[
           { value: ALL_ASSIGNEES_KEY, text: t('curation_page.filter.all_assignees') },
-          ...committeeMembers.map(address => ({ value: address, text: <Profile textOnly address={address} /> }))
+          ...[
+            ...committeeMembers.filter(member => member === wallet.address),
+            ...committeeMembers.filter(member => member !== wallet.address)
+          ].map(address => ({
+            value: address,
+            text: <Profile textOnly address={address} />
+          }))
         ]}
         onChange={(_event, { value }) => this.setState({ assigneeFilter: `${value}` })}
       />
