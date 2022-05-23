c91892986ff69008a7ee6559ef2b3615a5b64a83
Nando <24811313+fzavalia@users.noreply.github.com>
2022-05-23 15:30:10 -0300

    feat: Add selector to check if rarities with oracle is enabled

--- src/modules/features/selectors.ts
+++ src/modules/features/selectors.ts
@@ -3,6 +3,9 @@ import { ApplicationName } from 'decentraland-dapps/dist/modules/features/types'
 import { RootState } from 'modules/common/types'
 import { FeatureName } from './types'
 
+export const getIsRaritiesWithOracleEnabled = (state: RootState) =>
+  getIsFeatureEnabled(state, ApplicationName.BUILDER, FeatureName.RARITIES_WITH_ORACLE)
+
 export const getIsMaintenanceEnabled = (state: RootState) => {
   // As this is called by the routes component which is rendered when the user enters the application,
   // Features might have not yet been requested and will throw in that case.
