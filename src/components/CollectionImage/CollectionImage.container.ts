2451e1315f50fff104413d93937217e950525dc5
Juanma Hidalgo <juanma06@gmail.com>
2022-05-27 21:29:35 +0200

    feat: Paginates the collection list and removes the virtualized one (#2091)
    Co-authored-by: Juan Cazala <juancazala@gmail.com>

--- src/components/CollectionImage/CollectionImage.container.ts
+++ src/components/CollectionImage/CollectionImage.container.ts
@@ -2,8 +2,7 @@ import { connect } from 'react-redux'
 import { isLoadingType } from 'decentraland-dapps/dist/modules/loading/selectors'
 import { RootState } from 'modules/common/types'
 import { getCollectionItems, getLoading as getLoadingItem } from 'modules/item/selectors'
-import { getCollection, getLoading as getLoadingCollection } from 'modules/collection/selectors'
-import { FETCH_COLLECTIONS_REQUEST } from 'modules/collection/actions'
+import { getCollection } from 'modules/collection/selectors'
 import { fetchCollectionItemsRequest, FETCH_COLLECTION_ITEMS_REQUEST, FETCH_ITEMS_REQUEST } from 'modules/item/actions'
 import { OwnProps, MapStateProps, MapDispatch, MapDispatchProps } from './CollectionImage.types'
 import CollectionImage from './CollectionImage'
@@ -12,13 +11,13 @@ const mapState = (state: RootState, ownProps: OwnProps): MapStateProps => {
   const { collectionId } = ownProps
   const itemCount = getCollection(state, collectionId)?.itemCount
   const items = getCollectionItems(state, collectionId)
+  const isLoading = !!getLoadingItem(state).find(
+    action => action.type === FETCH_COLLECTION_ITEMS_REQUEST && action.payload.collectionId === collectionId
+  )
   return {
     items,
     itemCount,
-    isLoading:
-      isLoadingType(getLoadingCollection(state), FETCH_COLLECTIONS_REQUEST) ||
-      isLoadingType(getLoadingItem(state), FETCH_ITEMS_REQUEST) ||
-      isLoadingType(getLoadingItem(state), FETCH_COLLECTION_ITEMS_REQUEST)
+    isLoading: isLoadingType(getLoadingItem(state), FETCH_ITEMS_REQUEST) || isLoading
   }
 }
 
