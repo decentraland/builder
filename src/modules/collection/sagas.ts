cc41cd26bd1b36254f12f0c86d87fe4414a1e399
Gabriel Diaz <gabrieldiaz31@gmail.com>
2022-06-30 13:09:58 -0300

    feat: Remove the fields createdAt and updatedAt from the payload that'll send to the backend

--- src/modules/collection/sagas.ts
+++ src/modules/collection/sagas.ts
@@ -241,7 +241,7 @@ export function* collectionSaga(legacyBuilderClient: BuilderAPI, client: Builder
   }
 
   function* handleSaveCollectionRequest(action: SaveCollectionRequestAction) {
-    const { collection } = action.payload
+    const { collection: {createdAt, updatedAt, ...collection} } = action.payload
     try {
       if (!isValidText(collection.name)) {
         throw new Error(yield call(t, 'sagas.collection.invalid_character'))
