bded15fce3092de1b87ff85eae2b4f3bad16084c
Gabriel Diaz <gabrieldiaz31@gmail.com>
2022-08-05 14:20:24 -0300

    feat: Keep one emote at once

--- src/components/ItemEditorPage/LeftPanel/Items/Items.tsx
+++ src/components/ItemEditorPage/LeftPanel/Items/Items.tsx
@@ -72,6 +72,8 @@ export default class Items extends React.PureComponent<Props, State> {
     const { bodyShape, visibleItems, wearableController, isPlayingEmote, onSetItems } = this.props
     if (!hasBodyShape(item, bodyShape)) return
 
+    let newVisibleItemIds = visibleItems.filter(_item => _item.id !== item.id)
+
     if (item.type === ItemType.EMOTE) {
       if (this.isVisible(item)) {
         if (isPlayingEmote) {
@@ -81,10 +83,10 @@ export default class Items extends React.PureComponent<Props, State> {
         }
 
         return
+      } else {
+        newVisibleItemIds = newVisibleItemIds.filter(_item => _item.type !== ItemType.EMOTE)
       }
     }
-
-    const newVisibleItemIds = visibleItems.filter(_item => _item.id !== item.id && _item.type !== ItemType.EMOTE)
 
     if (!this.isVisible(item)) {
       newVisibleItemIds.push(item)
