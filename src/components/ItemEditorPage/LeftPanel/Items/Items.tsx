7c7e2cecbbba1904b5f91f2b1d7ab65e606165a3
Gabriel Diaz <gabrieldiaz31@gmail.com>
2022-08-05 10:35:50 -0300

    feat: Handle show one emote per time

--- src/components/ItemEditorPage/LeftPanel/Items/Items.tsx
+++ src/components/ItemEditorPage/LeftPanel/Items/Items.tsx
@@ -72,6 +72,8 @@ export default class Items extends React.PureComponent<Props, State> {
     const { bodyShape, visibleItems, wearableController, isPlayingEmote, onSetItems } = this.props
     if (!hasBodyShape(item, bodyShape)) return
 
+    let newVisibleItemIds = visibleItems.filter(_item => _item.id !== item.id)
+
     if (item.type === ItemType.EMOTE) {
       if (this.isVisible(item)) {
         if (isPlayingEmote) {
@@ -81,10 +83,11 @@ export default class Items extends React.PureComponent<Props, State> {
         }
 
         return
+      } else {
+        newVisibleItemIds = visibleItems.filter(_item => _item.type !== ItemType.EMOTE)
+        newVisibleItemIds.push(item)
       }
     }
-
-    const newVisibleItemIds = visibleItems.filter(_item => _item.id !== item.id)
 
     if (!this.isVisible(item)) {
       newVisibleItemIds.push(item)
