ddfbf92b4a456d6afc6019bcacfd3b5f240c460d
Juan Cazala <juancazala@gmail.com>
2022-09-14 16:42:53 -0300

    fix: compute hashes in frontend

--- src/modules/collection/sagas.ts
+++ src/modules/collection/sagas.ts
@@ -826,30 +826,25 @@ export function* collectionSaga(legacyBuilderClient: BuilderAPI, client: Builder
       const emotesFeatureFlag: boolean = yield select(getIsEmotesFlowEnabled)
 
       for (const item of items) {
-        if (!item.currentContentHash) {
-          const v0ContentHash: string = yield call(
-            buildStandardWearableContentHash,
-            collection,
-            item,
-            EntityHashingType.V0,
-            emotesFeatureFlag
-          )
-          const v1ContentHash: string = yield call(
-            buildStandardWearableContentHash,
-            collection,
-            item,
-            EntityHashingType.V1,
-            emotesFeatureFlag
-          )
+        const v0ContentHash: string = yield call(
+          buildStandardWearableContentHash,
+          collection,
+          item,
+          EntityHashingType.V0,
+          emotesFeatureFlag
+        )
+        const v1ContentHash: string = yield call(
+          buildStandardWearableContentHash,
+          collection,
+          item,
+          EntityHashingType.V1,
+          emotesFeatureFlag
+        )
 
-          // As there could be older hashes in the blockchain, check if both of them are different to see if they need an update
-          if (v0ContentHash !== item.blockchainContentHash && v1ContentHash !== item.blockchainContentHash) {
-            itemsToRescue.push(item)
-            contentHashes.push(v1ContentHash)
-          }
-        } else if (item.currentContentHash !== item.blockchainContentHash) {
+        // As there could be older hashes in the blockchain, check if both of them are different to see if they need an update
+        if (v0ContentHash !== item.blockchainContentHash && v1ContentHash !== item.blockchainContentHash) {
           itemsToRescue.push(item)
-          contentHashes.push(item.currentContentHash)
+          contentHashes.push(v1ContentHash)
         }
       }
 
