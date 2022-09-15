0d33c3350b76b54f458a1ac768113dd5be78ff8e
Juan Cazala <juancazala@gmail.com>
2022-09-15 18:34:15 -0300

    Revert "fix: always compute the hash in the frontend"
    This reverts commit 79ae49eb091f3fb17d88fce42e32ef8d4fd1c029.

--- src/modules/collection/sagas.ts
+++ src/modules/collection/sagas.ts
@@ -826,25 +826,30 @@ export function* collectionSaga(legacyBuilderClient: BuilderAPI, client: Builder
       const emotesFeatureFlag: boolean = yield select(getIsEmotesFlowEnabled)
 
       for (const item of items) {
-        const v0ContentHash: string = yield call(
-          buildStandardWearableContentHash,
-          collection,
-          item,
-          EntityHashingType.V0,
-          emotesFeatureFlag
-        )
-        const v1ContentHash: string = yield call(
-          buildStandardWearableContentHash,
-          collection,
-          item,
-          EntityHashingType.V1,
-          emotesFeatureFlag
-        )
+        if (!item.currentContentHash) {
+          const v0ContentHash: string = yield call(
+            buildStandardWearableContentHash,
+            collection,
+            item,
+            EntityHashingType.V0,
+            emotesFeatureFlag
+          )
+          const v1ContentHash: string = yield call(
+            buildStandardWearableContentHash,
+            collection,
+            item,
+            EntityHashingType.V1,
+            emotesFeatureFlag
+          )
 
-        // As there could be older hashes in the blockchain, check if both of them are different to see if they need an update
-        if (v0ContentHash !== item.blockchainContentHash && v1ContentHash !== item.blockchainContentHash) {
+          // As there could be older hashes in the blockchain, check if both of them are different to see if they need an update
+          if (v0ContentHash !== item.blockchainContentHash && v1ContentHash !== item.blockchainContentHash) {
+            itemsToRescue.push(item)
+            contentHashes.push(v1ContentHash)
+          }
+        } else if (item.currentContentHash !== item.blockchainContentHash) {
           itemsToRescue.push(item)
-          contentHashes.push(v1ContentHash)
+          contentHashes.push(item.currentContentHash)
         }
       }
 
