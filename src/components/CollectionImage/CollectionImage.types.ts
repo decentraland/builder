a56335c36220eaaef8e14a2330fe075448aed483
Juanma Hidalgo <juanma06@gmail.com>
2022-10-17 15:07:49 +0200

    fix: Avoids fetching all items for TP collections (#2372)

--- src/components/CollectionImage/CollectionImage.types.ts
+++ src/components/CollectionImage/CollectionImage.types.ts
@@ -1,17 +1,19 @@
 import { Dispatch } from 'redux'
 import { fetchCollectionItemsRequest, FetchCollectionItemsRequestAction } from 'modules/item/actions'
+import { Collection } from 'modules/collection/types'
 import { Item } from 'modules/item/types'
 
 export type Props = {
   className?: string
   collectionId: string
+  collection: Collection | null
   items: Item[]
   itemCount: number | undefined
   isLoading: boolean
   onFetchCollectionItems: typeof fetchCollectionItemsRequest
 }
 
-export type MapStateProps = Pick<Props, 'items' | 'itemCount' | 'isLoading'>
+export type MapStateProps = Pick<Props, 'collection' | 'items' | 'itemCount' | 'isLoading'>
 export type MapDispatchProps = Pick<Props, 'onFetchCollectionItems'>
 export type MapDispatch = Dispatch<FetchCollectionItemsRequestAction>
 export type OwnProps = Pick<Props, 'collectionId'>
