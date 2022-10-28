4c9cd468c5f41ab8e04695147406ea1266563ba0
Gabriel Diaz <gabrieldiaz31@gmail.com>
2022-10-28 18:45:19 -0300

    feat: Remove unused FF

--- src/modules/features/types.ts
+++ src/modules/features/types.ts
@@ -1,9 +1,6 @@
 export enum FeatureName {
   MAINTENANCE = 'maintenance',
-  NEW_EMOTE_FLOW = 'new-emote-flow',
-  PUBLISH_NEW_EMOTES = 'publish-new-emotes',
   RENTALS = 'rentals',
-  EMOTE_PLAY_MODE = 'emote-play-mode',
   NEW_PUBLISH_WIZARD = 'new-publish-wizard',
   MVMF = 'mvmf-2022-announcement'
 }
