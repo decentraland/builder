acfefbd8fa66894413f3370c9aa397ebb08a72df
Gabriel Diaz <gabrieldiaz31@gmail.com>
2022-11-10 16:14:17 -0300

    chore: Add TODO to support imports in languages module

--- src/modules/translation/languages/index.ts
+++ src/modules/translation/languages/index.ts
@@ -1,3 +1,6 @@
+/* TODO: Update the TranslationKeys interface to a recursive structure to properly support imports here
+ * https://github.com/decentraland/decentraland-dapps/blob/master/src/modules/translation/types.ts#L5
+ */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const en = require('./en.json')
 const es = require('./es.json')
