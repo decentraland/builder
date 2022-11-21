6af1769f1427b404a5c1834ae71c9f17a51b2eff
Gabriel Díaz <gabrieldiaz31@gmail.com>
2022-11-21 10:06:32 -0300

    feat: Handle and show errors when uploading file (#2425)
    * feat: Add max emote duration error to the ItemsCreationStep
    
    * feat: Add translations
    
    * fix: 'CreateSingleItemModa' dimmed too much when setting the price to emotes
    
    * feat: Update error messages
    
    * feat: Refactor the getItemData method to get the emote metrics without a WearablePreview component
    
    * chore: Update pre-commit TS_FILES filter

--- src/modules/item/errors.ts
+++ src/modules/item/errors.ts
@@ -1,6 +1,14 @@
+import { ReactNode, createElement } from 'react'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { toMB } from 'lib/file'
-import { MAX_FILE_SIZE, MAX_THUMBNAIL_FILE_SIZE } from 'modules/item/utils'
+import { MAX_EMOTE_DURATION, MAX_FILE_SIZE, MAX_THUMBNAIL_FILE_SIZE } from 'modules/item/utils'
+
+class CustomError {
+  message: ReactNode
+  constructor(message: ReactNode) {
+    this.message = message
+  }
+}
 
 export class ItemTooBigError extends Error {
   constructor() {
@@ -8,9 +16,15 @@ export class ItemTooBigError extends Error {
   }
 }
 
-export class FileTooBigError extends Error {
+export class FileTooBigError extends CustomError {
   constructor() {
-    super(t('create_single_item_modal.error.file_too_big', { size: `${toMB(MAX_FILE_SIZE)}MB` }))
+    super(
+      t('create_single_item_modal.error.file_too_big', {
+        title: createElement('b', null, t('create_single_item_modal.error.file_too_big_title')),
+        enter: createElement('br'),
+        size: `${toMB(MAX_FILE_SIZE)}MB`
+      })
+    )
   }
 }
 
@@ -35,5 +49,23 @@ export class InvalidFilesError extends Error {
 export class MissingModelFileError extends Error {
   constructor() {
     super(t('create_single_item_modal.error.missing_model_file'))
+  }
+}
+
+export class InvalidModelFilesRepresentation extends Error {
+  constructor() {
+    super(t('create_single_item_modal.error.invalid_model_files_representation'))
+  }
+}
+
+export class EmoteDurationTooLongError extends CustomError {
+  constructor() {
+    super(
+      t('create_single_item_modal.error.emote_duration_too_long', {
+        title: createElement('b', null, t('create_single_item_modal.error.emote_duration_too_long_title')),
+        enter: createElement('br'),
+        duration: MAX_EMOTE_DURATION
+      })
+    )
   }
 }
