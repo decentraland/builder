93522e4cefbad24dee27f980a75c48146a9e993e
Gabriel Díaz <gabrieldiaz31@gmail.com>
2022-11-30 16:54:10 -0300

    feat: Send segment userid to intercom (#2450)
    * feat: Send segment anonymousid to Intercom
    
    * chore: Upgrade decentraland-dapps
    
    * feat: Improve segment set ready only when the page load for the first time

---
+++ src/components/Intercom/Intercom.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useCallback, useEffect } from 'react'
+import IntercomWidget from 'decentraland-dapps/dist/components/Intercom'
+import { getAnalytics, getAnonymousId } from 'decentraland-dapps/dist/modules/analytics/utils'
+import { config } from 'config'
+
+const APP_ID = config.get('INTERCOM_APP_ID', '')
+
+export const Intercom: React.FC = () => {
+  const [dclAnonymousUserID, setDclAnonymousUserID] = useState('')
+
+  const analytics = getAnalytics()
+
+  const analyticsReadyCallback = useCallback(() => {
+    const dclAnonymousUserID = getAnonymousId()
+    if (dclAnonymousUserID) {
+      setDclAnonymousUserID(dclAnonymousUserID)
+    }
+  }, [])
+
+  useEffect(() => {
+    analytics.ready(analyticsReadyCallback)
+  }, [])
+
+  return dclAnonymousUserID ? (
+    <IntercomWidget appId={APP_ID} data={{ anonymous_id: dclAnonymousUserID }} settings={{ alignment: 'right' }} />
+  ) : null
+}
+
+export default Intercom
