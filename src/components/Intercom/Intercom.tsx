09054f85692b28858c21cb1765cdbb7d8172fe55
Gabriel Diaz <gabrieldiaz31@gmail.com>
2022-12-02 12:37:07 -0300

    fix: Load intercom when adblockers on

--- src/components/Intercom/Intercom.tsx
+++ src/components/Intercom/Intercom.tsx
@@ -2,27 +2,26 @@ import React, { useState, useCallback, useEffect } from 'react'
 import IntercomWidget from 'decentraland-dapps/dist/components/Intercom'
 import { getAnalytics, getAnonymousId } from 'decentraland-dapps/dist/modules/analytics/utils'
 import { config } from 'config'
+import { IntercomUserData } from './Intercom.types'
 
 const APP_ID = config.get('INTERCOM_APP_ID', '')
 const analytics = getAnalytics()
 
 export const Intercom: React.FC = () => {
-  const [dclAnonymousUserID, setDclAnonymousUserID] = useState('')
+  const [intercomUserData, setIntercomUserData] = useState<IntercomUserData>()
 
   const analyticsReadyCallback = useCallback(() => {
     const dclAnonymousUserID = getAnonymousId()
     if (dclAnonymousUserID) {
-      setDclAnonymousUserID(dclAnonymousUserID)
+      setIntercomUserData({ ...intercomUserData, anon_id: dclAnonymousUserID })
     }
-  }, [])
+  }, [intercomUserData])
 
   useEffect(() => {
     analytics.ready(analyticsReadyCallback)
   }, [analyticsReadyCallback])
 
-  return dclAnonymousUserID ? (
-    <IntercomWidget appId={APP_ID} data={{ anon_id: dclAnonymousUserID }} settings={{ alignment: 'right' }} />
-  ) : null
+  return <IntercomWidget appId={APP_ID} data={intercomUserData} settings={{ alignment: 'right' }} />
 }
 
 export default Intercom
