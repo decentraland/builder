7355772cb26f3f97f56bedd7b9e61f5338e6c7e9
Juanma Hidalgo <juanma06@gmail.com>
2022-12-20 16:59:45 +0100

    feat: use a peer url with no gbc to deploy profiles

--- src/modules/common/utils.ts
+++ src/modules/common/utils.ts
@@ -1,6 +1,7 @@
 import { Task } from 'redux-saga'
 import { Action } from 'redux'
 import { take, fork, ActionPattern, ForkEffect, cancel } from 'redux-saga/effects'
+import { config } from 'config'
 
 export const takeLatestCancellable = <A extends Action>(
   { initializer, cancellable }: { initializer: ActionPattern<A>; cancellable: ActionPattern<A> },
@@ -20,3 +21,23 @@ export const takeLatestCancellable = <A extends Action>(
       }
     }
   })
+
+// This is a temporary util fn to get a node with no Garbage collector so we can deploy profiles directly to a node
+export const getPeerWithNoGBCollectorURL = () => {
+  const environment = config.get('ENVIRONMENT')
+  const urls =
+    environment === 'production'
+      ? [
+          'peer-ec2.decentraland.org',
+          'peer-wc1.decentraland.org',
+          'peer-eu1.decentraland.org',
+          'peer-ec1.decentraland.org',
+          'interconnected.online',
+          'peer.decentral.io',
+          'peer.kyllian.me',
+          'peer.uadevops.com'
+        ]
+      : ['peer.decentraland.zone', 'peer-ue-2.decentraland.zone', 'peer-ap1.decentraland.zone']
+
+  return urls[Math.floor(Math.random() * urls.length)]
+}
