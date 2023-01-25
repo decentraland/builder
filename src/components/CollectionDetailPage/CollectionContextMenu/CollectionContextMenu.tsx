a11bfc711b2c4d50fc1412b3878811d3eaa97bdb
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-01-25 18:34:56 -0300

    feat: Extract elements from the context menu to the collection details page

--- src/components/CollectionDetailPage/CollectionContextMenu/CollectionContextMenu.tsx
+++ src/components/CollectionDetailPage/CollectionContextMenu/CollectionContextMenu.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react'
 import CopyToClipboard from 'react-copy-to-clipboard'
-import { Dropdown, Button, Icon, Popup, Loader } from 'decentraland-ui'
+import { Dropdown, Button, Icon, Popup } from 'decentraland-ui'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
-import { buildCollectionForumPost } from 'modules/forum/utils'
 import { RoleType } from 'modules/collection/types'
 import { getExplorerURL, isOwner as isCollectionOwner, isLocked } from 'modules/collection/utils'
 import ConfirmDelete from 'components/ConfirmDelete'
@@ -23,13 +22,6 @@ export default class CollectionContextMenu extends React.PureComponent<Props> {
       collection
     })
     this.navigateTo(explorerLink, '_blank')
-  }
-
-  handlePostToForum = () => {
-    const { collection, items, name, onPostToForum } = this.props
-    if (!collection.forumLink) {
-      onPostToForum(collection, buildCollectionForumPost(collection, items, name))
-    }
   }
 
   handleUpdateManagers = () => {
@@ -60,7 +52,7 @@ export default class CollectionContextMenu extends React.PureComponent<Props> {
   }
 
   render() {
-    const { collection, wallet, isForumPostLoading } = this.props
+    const { collection, wallet } = this.props
     const isOwner = isCollectionOwner(collection, wallet.address)
     return (
       <Dropdown
@@ -74,8 +66,6 @@ export default class CollectionContextMenu extends React.PureComponent<Props> {
         direction="left"
       >
         <Dropdown.Menu>
-          <Dropdown.Item text={t('collection_context_menu.see_in_world')} onClick={this.handleNavigateToExplorer} />
-
           {collection.isPublished ? (
             isOwner ? (
               <>
@@ -106,42 +96,6 @@ export default class CollectionContextMenu extends React.PureComponent<Props> {
               <CopyToClipboard text={collection.contractAddress!}>
                 <Dropdown.Item disabled={!collection.isPublished} text={t('collection_context_menu.copy_address')} />
               </CopyToClipboard>
-            }
-            hideOnScroll={true}
-            on="hover"
-            inverted
-            flowing
-          />
-
-          <Popup
-            content={
-              !collection.isPublished
-                ? t('collection_context_menu.unpublished')
-                : !collection.forumLink
-                ? t('collection_context_menu.not_posted')
-                : undefined
-            }
-            disabled={collection.isPublished || !!collection.forumLink}
-            position="right center"
-            trigger={
-              !collection.isPublished || collection.forumLink ? (
-                <Dropdown.Item
-                  disabled={!collection.isPublished}
-                  text={t('collection_context_menu.forum_post')}
-                  onClick={this.handleNavigateToForum}
-                />
-              ) : isOwner ? (
-                <Dropdown.Item onClick={this.handlePostToForum} disabled={isForumPostLoading}>
-                  {isForumPostLoading ? (
-                    <div>
-                      {t('collection_context_menu.posting')}&nbsp;&nbsp;
-                      <Loader size="mini" active inline />
-                    </div>
-                  ) : (
-                    t('collection_context_menu.post_to_forum')
-                  )}
-                </Dropdown.Item>
-              ) : null
             }
             hideOnScroll={true}
             on="hover"
