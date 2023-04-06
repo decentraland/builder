74c70e9ad051b521818dcbc96856e0c2e4b4c76c
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-04-06 10:47:27 -0300

    feat: add ff for deploy to worlds epic (#2597)

--- src/modules/features/selectors.spec.ts
+++ src/modules/features/selectors.spec.ts
@@ -1,6 +1,6 @@
 import { getIsFeatureEnabled } from 'decentraland-dapps/dist/modules/features/selectors'
 import { RootState } from 'modules/common/types'
-import { getIsDCLControllerV2Enabled, getIsMaintenanceEnabled } from './selectors'
+import { getIsDCLControllerV2Enabled, getIsDeployToWorldsEnabled, getIsMaintenanceEnabled } from './selectors'
 
 jest.mock('decentraland-dapps/dist/modules/features/selectors')
 
@@ -86,6 +86,49 @@ describe('when getting if dcl controller v2 is enabled', () => {
     it('should return false', () => {
       const state: RootState = {} as any
       const result = getIsDCLControllerV2Enabled(state)
+
+      expect(result).toEqual(false)
+    })
+  })
+})
+
+describe('when getting if deploy to worlds enabled', () => {
+  describe('when getIsFeatureEnabled returns true', () => {
+    beforeEach(() => {
+      mockGetIsFeatureEnabled.mockReturnValueOnce(true)
+    })
+
+    it('should return true', () => {
+      const state: RootState = {} as any
+      const result = getIsDeployToWorldsEnabled(state)
+
+      expect(result).toEqual(true)
+    })
+  })
+
+  describe('when getIsFeatureEnabled returns false', () => {
+    beforeEach(() => {
+      mockGetIsFeatureEnabled.mockReturnValueOnce(false)
+    })
+
+    it('should return false', () => {
+      const state: RootState = {} as any
+      const result = getIsDeployToWorldsEnabled(state)
+
+      expect(result).toEqual(false)
+    })
+  })
+
+  describe('when getIsFeatureEnabled throws an exception', () => {
+    beforeEach(() => {
+      mockGetIsFeatureEnabled.mockImplementationOnce(() => {
+        throw new Error('error')
+      })
+    })
+
+    it('should return false', () => {
+      const state: RootState = {} as any
+      const result = getIsDeployToWorldsEnabled(state)
 
       expect(result).toEqual(false)
     })
