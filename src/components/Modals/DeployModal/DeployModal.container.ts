93b79c996ab9cc5636f52274c35552454810c4fd
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-04-06 16:47:24 -0300

    fix import

--- src/components/Modals/DeployModal/DeployModal.container.ts
+++ src/components/Modals/DeployModal/DeployModal.container.ts
@@ -2,9 +2,9 @@ import { connect } from 'react-redux'
 import { RootState } from 'modules/common/types'
 import { getData as getDeployments } from 'modules/deployment/selectors'
 import { getActivePoolGroup } from 'modules/poolGroup/selectors'
+import { getIsDeployToWorldsEnabled } from 'modules/features/selectors'
 import { MapStateProps, OwnProps } from './DeployModal.types'
 import DeployModal from './DeployModal'
-import { getIsDeployToWorldsEnabled } from 'modules/features/selectors'
 
 const mapState = (state: RootState, ownProps: OwnProps): MapStateProps => ({
   deployment: getDeployments(state)[ownProps.metadata.projectId],
