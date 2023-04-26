2937a41d81532c666503160c2986216f6035be97
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-04-26 18:08:47 -0300

    feat: Add fetchWorldsDeploymentsRequest sagas test

--- src/modules/deployment/sagas.spec.ts
+++ src/modules/deployment/sagas.spec.ts
@@ -1,18 +1,20 @@
 import { CatalystClient, ContentClient } from 'dcl-catalyst-client'
-import { BuilderAPI } from 'lib/api/builder'
-import { getName } from 'modules/profile/selectors'
-import { isLoggedIn } from 'modules/identity/selectors'
-import { getIdentity } from 'modules/identity/utils'
-import { getData } from 'modules/project/selectors'
-import { getSceneByProjectId } from 'modules/scene/utils'
 import { expectSaga } from 'redux-saga-test-plan'
 import * as matchers from 'redux-saga-test-plan/matchers'
-import { deployToWorldRequest } from './actions'
-import { deploymentSaga } from './sagas'
+import { BuilderAPI } from 'lib/api/builder'
+import { getCatalystContentUrl } from 'lib/api/peer'
+import { isLoggedIn } from 'modules/identity/selectors'
+import { getIdentity } from 'modules/identity/utils'
 import { getMedia } from 'modules/media/selectors'
 import { objectURLToBlob } from 'modules/media/utils'
+import { getName } from 'modules/profile/selectors'
+import { getData } from 'modules/project/selectors'
 import { createFiles } from 'modules/project/export'
+import { getSceneByProjectId } from 'modules/scene/utils'
+import { deployToWorldRequest, fetchWorldDeploymentsRequest, fetchWorldDeploymentsSuccess } from './actions'
+import { deploymentSaga } from './sagas'
 import { makeContentFiles } from './contentUtils'
+import { Deployment } from './types'
 
 let builderAPI: BuilderAPI
 let catalystClient: CatalystClient
@@ -69,6 +71,50 @@ describe('when handling deploy to world request', () => {
       .call.like({ fn: ContentClient.prototype.buildEntity, args: [{ type: 'scene', pointers: [], metadata: sceneDefinition, files: {} }] })
       .call.like({ fn: ContentClient.prototype.deployEntity, args: [{ entityId: 'entityId', files: [], authChain: undefined }] })
       .dispatch(deployToWorldRequest('project-id', 'world-name'))
+      .silentRun()
+  })
+})
+
+describe('when handling fetch worlds deployments request', () => {
+  it('should fetch deployments for each world', () => {
+    const worlds = ['my-world.dcl.eth']
+    return expectSaga(deploymentSaga, builderAPI, catalystClient)
+      .provide([
+        [
+          matchers.call.fn(ContentClient.prototype.fetchEntitiesByPointers),
+          [
+            {
+              id: 'deployMyWorldId',
+              timestamp: 1,
+              pointers: ['0,0'],
+              content: [{ file: 'scene-thumbnail.png', hash: 'aThumbnailHash' }],
+              metadata: {
+                display: { title: 'MySceneName', navmapThumbnail: 'scene-thumbnail.png' },
+                scene: { base: '0,0', parcels: ['0,0', '0,1', '1,0', '1,1'] },
+                source: { projectId: 'aProjectId', layout: { rows: 2, cols: 2 } },
+                owner: ''
+              }
+            }
+          ]
+        ]
+      ])
+      .put(
+        fetchWorldDeploymentsSuccess(worlds, [
+          {
+            id: 'deployMyWorldId',
+            timestamp: 1,
+            projectId: 'aProjectId',
+            name: 'MySceneName',
+            thumbnail: getCatalystContentUrl('aThumbnailHash'),
+            placement: { point: { x: 0, y: 0 }, rotation: 'north' },
+            owner: '',
+            layout: { rows: 2, cols: 2 },
+            base: '0,0',
+            parcels: ['0,0', '0,1', '1,0', '1,1']
+          }
+        ] as unknown as Deployment[])
+      )
+      .dispatch(fetchWorldDeploymentsRequest(worlds))
       .silentRun()
   })
 })
