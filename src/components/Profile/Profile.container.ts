aa92dc5ec022daab3b70267d86560018b43239f0
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-05-02 13:30:15 -0300

    fix: Update profile container props

--- src/components/Profile/Profile.container.ts
+++ src/components/Profile/Profile.container.ts
@@ -1,11 +1,16 @@
 import { connect } from 'react-redux'
 import { RootState } from 'modules/common/types'
 import { getAddress } from 'decentraland-dapps/dist/modules/wallet/selectors'
-import { MapStateProps } from './Profile.types'
+import { MapStateProps, OwnProps } from './Profile.types'
 import Profile from './Profile'
 
 const mapState = (state: RootState): MapStateProps => ({
   currentAddress: getAddress(state)
 })
 
-export default connect(mapState, null)(Profile)
+const mergeProps = (mapStateProps: MapStateProps, _mapDispatchProps: null, ownProps: OwnProps) => ({
+  ...mapStateProps,
+  ...ownProps
+})
+
+export default connect(mapState, null, mergeProps)(Profile)
