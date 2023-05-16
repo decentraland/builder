13f41f00047c580cc598b14f6b66bfc35ea86576
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-05-16 20:34:38 -0300

    feat: Add deployment progress and form steps

--- src/components/Modals/DeployModal/DeployToWorld/DeployToWorld.container.ts
+++ src/components/Modals/DeployModal/DeployToWorld/DeployToWorld.container.ts
@@ -4,13 +4,15 @@ import { RootState } from 'modules/common/types'
 import { getCurrentProject } from 'modules/project/selectors'
 import { getENSByWallet } from 'modules/ens/selectors'
 import { deployToWorldRequest } from 'modules/deployment/actions'
-import { isLoading } from 'modules/deployment/selectors'
+import { getDeploymentsByWorlds, getProgress as getUploadProgress, isLoading } from 'modules/deployment/selectors'
 import { MapDispatch, MapDispatchProps, MapStateProps } from './DeployToWorld.types'
 import DeployToWorld from './DeployToWorld'
 
 const mapState = (state: RootState): MapStateProps => ({
   ensList: getENSByWallet(state),
   project: getCurrentProject(state),
+  deployments: getDeploymentsByWorlds(state),
+  deploymentProgress: getUploadProgress(state),
   isLoading: isLoading(state)
 })
 
