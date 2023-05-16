f9bf2819d7cff9e75aaca3a7a1fe69fe93b48d30
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-05-16 20:34:38 -0300

    feat: Add deployment progress and form steps

--- src/components/Modals/DeployModal/DeployToWorld/DeployToWorld.types.ts
+++ src/components/Modals/DeployModal/DeployToWorld/DeployToWorld.types.ts
@@ -3,11 +3,15 @@ import { CallHistoryMethodAction } from 'connected-react-router'
 import { deployToWorldRequest, DeployToWorldRequestAction } from 'modules/deployment/actions'
 import { ENS } from 'modules/ens/types'
 import { Project } from 'modules/project/types'
+import { Deployment } from 'modules/deployment/types'
+import { DeploymentState } from 'modules/deployment/reducer'
 
 export type Props = {
   name: string
   project: Project | null
   ensList: ENS[]
+  deployments: Record<string, Deployment>
+  deploymentProgress: DeploymentState['progress']
   isLoading: boolean
   onClose: () => void
   onBack: () => void
@@ -15,6 +19,13 @@ export type Props = {
   onNavigate: (path: string) => void
 }
 
-export type MapStateProps = Pick<Props, 'ensList' | 'project' | 'isLoading'>
+export type MapStateProps = Pick<Props, 'ensList' | 'project' | 'deployments' | 'deploymentProgress' | 'isLoading'>
 export type MapDispatchProps = Pick<Props, 'onPublish' | 'onNavigate'>
 export type MapDispatch = Dispatch<DeployToWorldRequestAction | CallHistoryMethodAction>
+
+export enum DeployToWorldView {
+  FORM = 'FORM',
+  PROGRESS = 'PROGRESS',
+  SUCCESS = 'SUCCESS',
+  EMPTY = 'EMPTY'
+}
