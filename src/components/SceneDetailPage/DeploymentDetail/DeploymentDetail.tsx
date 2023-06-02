42da0559b829e9224ba9d7822ff319eaea49e025
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-06-02 01:24:15 -0300

    feat: Update DeploymentDetail with deployment location

--- src/components/SceneDetailPage/DeploymentDetail/DeploymentDetail.tsx
+++ src/components/SceneDetailPage/DeploymentDetail/DeploymentDetail.tsx
@@ -71,6 +71,10 @@ export default class DeploymentDetail extends React.PureComponent<Props> {
     if (status === DeploymentStatus.NEEDS_SYNC) {
       statusText = t('scene_detail_page.unsynced')
     }
+    let location = deployment.base
+    if (deployment?.world) {
+      location = deployment.world
+    }
     return (
       <div className={`DeploymentDetail ${landId ? 'clickable' : ''}`} onClick={() => landId && onNavigate(locations.landDetail(landId))}>
         {this.renderThumbnail()}
