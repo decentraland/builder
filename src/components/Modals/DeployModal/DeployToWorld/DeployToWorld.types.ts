c0406726d215ee0cd897968b51b909ce7913fece
Gabriel Díaz <gabrieldiaz31@gmail.com>
2023-06-08 10:23:38 -0300

    feat: Claim name from deploy worlds (#2633)
    * feat: Add translations
    
    * feat: Add translations
    
    * feat: Update DeploymentDetail with deployment location
    
    * feat: Update world detail deployment
    
    * fix: Translations rebase error
    
    * fix: Thumbnail size changing depending on world name
    
    * feat: ClaimName flow between DeployToWorld modal and ClaimNameModal
    
    * fix: Undefined location state
    
    * feat: Remove unused prop
    
    * feat: Handle redirection from ClaimName to DeployToWorld modal
    
    * fix: Missing property when loading
    
    * fix: ClaimName redirection between Scene Editor and ClaimName Page
    
    * refactor: Reaname method name
    
    * feat: Update redirection using queryParams
    
    * fix: DeployModal opening each change after coming from the ClaimName page

--- src/components/Modals/DeployModal/DeployToWorld/DeployToWorld.types.ts
+++ src/components/Modals/DeployModal/DeployToWorld/DeployToWorld.types.ts
@@ -7,6 +7,8 @@ import { Project } from 'modules/project/types'
 import { ModelMetrics } from 'modules/models/types'
 import { DeploymentState } from 'modules/deployment/reducer'
 import { Deployment } from 'modules/deployment/types'
+import { DeployToWorldLocationStateProps } from 'modules/location/types'
+import { DeployModalMetadata } from '../DeployModal.types'
 
 export type Props = {
   name: string
@@ -17,15 +19,17 @@ export type Props = {
   deploymentProgress: DeploymentState['progress']
   error: string | null
   isLoading: boolean
+  claimedName: string | null
   onClose: () => void
   onBack: () => void
   onPublish: typeof deployToWorldRequest
   onRecord: typeof recordMediaRequest
   onNavigate: (path: string) => void
+  onReplace: (path: string, locationState?: DeployToWorldLocationStateProps) => void
 }
 
 export type MapStateProps = Pick<Props, 'ensList' | 'project' | 'metrics' | 'deployments' | 'deploymentProgress' | 'error' | 'isLoading'>
-export type MapDispatchProps = Pick<Props, 'onPublish' | 'onNavigate' | 'onRecord'>
+export type MapDispatchProps = Pick<Props, 'onPublish' | 'onNavigate' | 'onRecord' | 'onReplace'>
 export type MapDispatch = Dispatch<DeployToWorldRequestAction | CallHistoryMethodAction | RecordMediaRequestAction>
 
 export enum DeployToWorldView {
@@ -34,4 +38,9 @@ export enum DeployToWorldView {
   SUCCESS = 'SUCCESS',
   EMPTY = 'EMPTY',
   ERROR = 'ERROR'
+}
+
+export type DeployToWorldModalMetadata = DeployModalMetadata & {
+  projectId: string
+  claimedName: string
 }
