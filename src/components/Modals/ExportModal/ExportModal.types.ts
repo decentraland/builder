b5506459cab4702cf03d2436e5ad00b9406d234a
Melisa Anabella Rossi <merossi@itba.edu.ar>
2023-06-16 16:09:39 -0300

    feat: change download modal

--- src/components/Modals/ExportModal/ExportModal.types.ts
+++ src/components/Modals/ExportModal/ExportModal.types.ts
@@ -5,7 +5,9 @@ export type Props = ModalProps & {
   isLoading: boolean
   progress: number
   total: number
+  isTemplatesEnabled: boolean
   onExport: typeof exportProjectRequest
 }
 
+export type MapStateProps = Pick<Props, 'isTemplatesEnabled'>
 export type MapDispatchProps = Pick<Props, 'onExport'>
