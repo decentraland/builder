180132ee4c89b47b4aa0aff50e20e4fda561dda7
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-06-23 15:37:08 -0300

    feat: Update CustomLayoutTemplate to clone templates

--- src/components/Modals/CustomLayoutModal/CustomLayoutModal.container.ts
+++ src/components/Modals/CustomLayoutModal/CustomLayoutModal.container.ts
@@ -2,9 +2,21 @@ import { connect } from 'react-redux'
 import { push } from 'connected-react-router'
 
 import { locations } from 'routing/locations'
-import { createProjectFromTemplate } from 'modules/project/actions'
-import { MapDispatchProps, MapDispatch } from './CustomLayoutModal.types'
+import { RootState } from 'modules/common/types'
+import { LOAD_MANIFEST_REQUEST, createProjectFromTemplate, duplicateProject } from 'modules/project/actions'
+import { getError as getProjectError, getLoading as getProjectLoading } from 'modules/project/selectors'
+import { MapDispatchProps, MapDispatch, MapStateProps } from './CustomLayoutModal.types'
+
 import CustomLayoutModal from './CustomLayoutModal'
+import { isLoadingType } from 'decentraland-dapps/dist/modules/loading/selectors'
+import { getLoadingProjectIds } from 'modules/sync/selectors'
+
+const mapState = (state: RootState): MapStateProps => {
+  return {
+    isLoading: isLoadingType(getProjectLoading(state), LOAD_MANIFEST_REQUEST) || getLoadingProjectIds(state).length > 0,
+    error: getProjectError(state)
+  }
+}
 
 const mapDispatch = (dispatch: MapDispatch): MapDispatchProps => ({
   onCreateProject: (name, description, template) =>
@@ -14,7 +26,8 @@ const mapDispatch = (dispatch: MapDispatch): MapDispatchProps => ({
         description,
         onSuccess: project => dispatch(push(locations.sceneEditor(project.id)))
       })
-    )
+    ),
+  onDuplicate: project => dispatch(duplicateProject(project))
 })
 
-export default connect(null, mapDispatch)(CustomLayoutModal)
+export default connect(mapState, mapDispatch)(CustomLayoutModal)
--- src/components/Modals/CustomLayoutModal/CustomLayoutModal.types.ts
+++ src/components/Modals/CustomLayoutModal/CustomLayoutModal.types.ts
@@ -2,11 +2,20 @@ import { Dispatch } from 'redux'
 import { ModalProps } from 'decentraland-dapps/dist/providers/ModalProvider/ModalProvider.types'
 import { CallHistoryMethodAction } from 'connected-react-router'
 
-import { CreateProjectFromTemplateAction } from 'modules/project/actions'
+import { CreateProjectFromTemplateAction, DuplicateProjectAction, duplicateProject } from 'modules/project/actions'
 import { Template } from 'modules/template/types'
+import { Project } from 'modules/project/types'
 
 export type Props = ModalProps & {
+  metadata: CustomLayoutModalMetadata
+  error: string | null
+  isLoading: boolean
   onCreateProject: (name: string, description: string, template: Template) => void
+  onDuplicate: typeof duplicateProject
+}
+
+export type CustomLayoutModalMetadata = {
+  template?: Project
 }
 
 export type State = {
@@ -18,5 +27,6 @@ export type State = {
   description: string
 }
 
-export type MapDispatchProps = Pick<Props, 'onCreateProject'>
-export type MapDispatch = Dispatch<CreateProjectFromTemplateAction | CallHistoryMethodAction>
+export type MapStateProps = Pick<Props, 'error' | 'isLoading'>
+export type MapDispatchProps = Pick<Props, 'onCreateProject' | 'onDuplicate'>
+export type MapDispatch = Dispatch<CreateProjectFromTemplateAction | DuplicateProjectAction | CallHistoryMethodAction>
