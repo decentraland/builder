6c44e8185ce81002b287b7799dd405eb30e65c06
Florencia Barreto <florbarrlad@gmail.com>
2023-06-29 18:41:13 +0200

    fix: redirect on identity

--- src/modules/ui/sagas.ts
+++ src/modules/ui/sagas.ts
@@ -1,23 +1,6 @@
-import { all, takeEvery, select, put } from 'redux-saga/effects'
-import { getLocation, push } from 'connected-react-router'
-import { CONNECT_WALLET_SUCCESS, ConnectWalletSuccessAction } from 'decentraland-dapps/dist/modules/wallet/actions'
+import { all } from 'redux-saga/effects'
 import { dashboardSaga } from './dashboard/sagas'
-import { locations } from 'routing/locations'
 
 export function* uiSaga() {
   yield all([dashboardSaga()])
-  yield takeEvery(CONNECT_WALLET_SUCCESS, handleConnectWalletSuccess)
-}
-
-function* handleConnectWalletSuccess(_action: ConnectWalletSuccessAction) {
-  const location: ReturnType<typeof getLocation> = yield select(getLocation)
-  const { pathname, search } = location
-  if (pathname === locations.signIn()) {
-    const redirectTo = new URLSearchParams(search).get('redirectTo')
-    if (redirectTo) {
-      yield put(push(decodeURIComponent(redirectTo)))
-    } else {
-      yield put(push(locations.root()))
-    }
-  }
 }
