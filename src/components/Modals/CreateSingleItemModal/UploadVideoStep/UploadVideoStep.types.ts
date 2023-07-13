c2126124843455d49dfe4ebc7d1269d4555b94eb
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2023-07-13 22:39:27 +0200

    feat: New step to upload video preview (#2744)
    * feat: Handle the video upload in the sgas
    
    * feat: Detect smart wearables data in the CreateSingleItemModal
    
    * refactor: Remove const and return leaving only the return
    
    * feat: Upload video when creating an sw
    
    * feat: Add step to upload video if is a SW
    
    * feat: Add a preview after uploading the video
    
    * fix: Issues with the prop isLoading
    
    * test: Fix test after changing wording
    
    * fix: Sort import and fix action styles
    
    * fix: Button styles and alignment

---
+++ src/components/Modals/CreateSingleItemModal/UploadVideoStep/UploadVideoStep.types.ts
@@ -0,0 +1,19 @@
+import { AcceptedFileProps } from '../CreateSingleItemModal.types'
+
+export type Props = {
+  contents?: Record<string, Blob>
+  title: string
+  onDropAccepted: (acceptedFileProps: AcceptedFileProps) => void
+  onDropRejected?: (files: File[]) => Promise<void>
+  onClose: () => void
+  onSaveVideo: () => void
+}
+
+export type StateData = {
+  id: string
+  error: string
+  isLoading: boolean
+  video: string
+}
+
+export type State = Partial<StateData>
