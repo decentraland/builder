3ab77c81033337efce285107260160088c441126
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-07-14 13:42:51 -0300

    feat: add hands category ftu (#2752)

---
+++ src/components/Modals/HandsCategoryLaunchModal/HandsCategoryLaunchModal.types.ts
@@ -0,0 +1,6 @@
+export type Props = {
+  isLoadingFeatureFlags: boolean
+  isHandsCategoryEnabled: boolean
+}
+
+export type MapStateProps = Pick<Props, 'isHandsCategoryEnabled' | 'isLoadingFeatureFlags'>
