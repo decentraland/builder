90a1d71ea37cb937de40726fcb9b844bb34722ba
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2023-07-21 11:31:30 +0200

    feat: Change smart icon and use the new one in the collection item (#2767)

--- src/components/ItemBadge/ItemBadge.tsx
+++ src/components/ItemBadge/ItemBadge.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
+import { Popup } from 'decentraland-ui'
 
-import { getBodyShapeType, toBodyShapeType } from 'modules/item/utils'
+import { SmartIcon } from 'components/SmartIcon'
+import { getBodyShapeType, isSmart, toBodyShapeType } from 'modules/item/utils'
 import { Props } from './ItemBadge.types'
 import './ItemBadge.css'
 
@@ -9,7 +11,20 @@ export default class ItemBadge extends React.PureComponent<Props> {
   render() {
     const { item, bodyShape: bodyShapeProp, size, className } = this.props
     const bodyShape = bodyShapeProp ? toBodyShapeType(bodyShapeProp) : getBodyShapeType(item)
-    return (
+    const isSmartItem = isSmart(item)
+
+    return isSmartItem ? (
+      <Popup
+        content={t('item_types.smart_wearable')}
+        position="top center"
+        trigger={
+          <div className={`ItemBadge smart ${className ? className : ''} ${size || 'normal'}`}>
+            <SmartIcon />
+          </div>
+        }
+        on="hover"
+      />
+    ) : (
       <div title={t(`body_shapes.${bodyShape}`)} className={`ItemBadge ${className ? className : ''} ${bodyShape} ${size || 'normal'}`} />
     )
   }
