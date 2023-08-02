9ee762773922dabffac61623cbf176f8d8e6178c
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-08-02 13:24:54 +0200

    feat: Add video and change FTU wording

--- src/components/Modals/SmartWearablesAnnouncementModal/SmartWearablesAnnouncementModal.tsx
+++ src/components/Modals/SmartWearablesAnnouncementModal/SmartWearablesAnnouncementModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Close } from 'decentraland-ui'
+import { Button, ModalNavigation } from 'decentraland-ui'
 import { ModalProps } from 'decentraland-dapps/dist/providers/ModalProvider/ModalProvider.types'
 import Modal from 'decentraland-dapps/dist/containers/Modal'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
@@ -7,9 +7,9 @@ import { getLocalStorage } from 'decentraland-dapps/dist/lib/localStorage'
 import { LOCALSTORAGE_SMART_WEARABLES_ANNOUCEMENT } from 'components/CollectionsPage/CollectionsPage'
 import styles from './SmartWearablesAnnouncementModal.module.css'
 
-// const PUBLIC_URL = process.env.PUBLIC_URL
+const PUBLIC_URL = process.env.PUBLIC_URL
 const localStorage = getLocalStorage()
-// const videoSrc = '/videos/template-preview.mp4'
+const videoSrc = '/videos/smart-wearable-preview.mp4'
 
 const SmartWearablesAnnouncementModal: React.FC<ModalProps> = ({ name, onClose }) => {
   const handleClose = () => {
@@ -23,12 +23,16 @@ const SmartWearablesAnnouncementModal: React.FC<ModalProps> = ({ name, onClose }
   }
 
   return (
-    <Modal className={styles.modal} name={name} onClose={handleClose} closeIcon={<Close onClick={handleClose} />}>
+    <Modal className={styles.modal} name={name} onClose={handleClose}>
+      <ModalNavigation title={t('smart_wearables_announcement_modal.title')} onClose={handleClose} />
       <Modal.Content className={styles.content}>
-        <h1 className={styles.title}>{t('smart_wearables_announcement_modal.title')}</h1>
-        <span className={styles.description}>{t('smart_wearables_announcement_modal.description')}</span>
-        {/* TODO: add the video here */}
-        {/* <video autoPlay loop className={styles.thumbnail} src={`${PUBLIC_URL}${videoSrc}`} muted /> */}
+        <video autoPlay loop className={styles.video} src={`${PUBLIC_URL}${videoSrc}`} muted />
+        <span className={styles.description}>
+          {t('smart_wearables_announcement_modal.description', {
+            br: () => <br />,
+            b: (text: string) => <b>{text}</b>
+          })}
+        </span>
       </Modal.Content>
       <Modal.Actions className={styles.actions}>
         <Button primary onClick={handleLearnMore}>
