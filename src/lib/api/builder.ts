51320a9095a7c62d189f52930418d4ad13ce5f7a
Juan Cazala <juancazala@gmail.com>
2023-09-13 13:20:39 -0300

    feat: use index.js from builder-server instead of hardcoded one (#2867)
    * feat: use index.js from builder-server instead of hardcoded one
    
    * chore: move import up and typo

--- src/lib/api/builder.ts
+++ src/lib/api/builder.ts
@@ -1,4 +1,5 @@
 import { AxiosRequestConfig, AxiosError } from 'axios'
+import { Entity } from '@dcl/schemas'
 import { BaseAPI, APIParam, RetryParams } from 'decentraland-dapps/dist/lib/api'
 import { Omit } from 'decentraland-dapps/dist/lib/types'
 import { config } from 'config'
@@ -619,6 +620,17 @@ export class BuilderAPI extends BaseAPI {
       params: formData
     })
     return
+  }
+
+  async fetchMain(projectId: string): Promise<Blob> {
+    const request = async (path: string) => fetch(this.url + path, { headers: await this.authorization.createAuthHeaders('get', path) })
+    const about: { configurations: { scenesUrn: string[] } } = await request(`/projects/${projectId}/about`).then(resp => resp.json())
+    const urn = about.configurations.scenesUrn[0]
+    const hash = urn.split('urn:decentraland:entity:').pop()!.split('?')[0]
+    const entity: Entity = await request(`/projects/${projectId}/contents/${hash}`).then(resp => resp.json())
+    const main = entity.content.find(content => content.file === 'bin/index.js')!
+    const file: string = await request(`/projects/${projectId}/contents/${main.hash}`).then(resp => resp.text())
+    return new Blob([file])
   }
 
   async fetchCrdt(projectId: string): Promise<Blob> {
