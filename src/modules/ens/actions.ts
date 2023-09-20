236d7b10653ab385be590de83b81256e133bd492
Nando <24811313+fzavalia@users.noreply.github.com>
2023-09-20 16:50:48 -0300

    fix: Rename actions

--- src/modules/ens/actions.ts
+++ src/modules/ens/actions.ts
@@ -164,10 +164,10 @@ export const FETCH_EXTERNAL_ENS_NAMES_REQUEST = '[Request] Fetch External ENS Na
 export const FETCH_EXTERNAL_ENS_NAMES_SUCCESS = '[Success] Fetch External ENS Names'
 export const FETCH_EXTERNAL_ENS_NAMES_FAILURE = '[Failure] Fetch External ENS Names'
 
-export const fetchExternadlENSNamesRequest = (owner: string) => action(FETCH_EXTERNAL_ENS_NAMES_REQUEST, { owner })
-export const fetchExternadlENSNamesSuccess = (owner: string, names: string[]) => action(FETCH_EXTERNAL_ENS_NAMES_SUCCESS, { owner, names })
-export const fetchExternadlENSNamesFailure = (owner: string, error: ENSError) => action(FETCH_EXTERNAL_ENS_NAMES_FAILURE, { owner, error })
+export const fetchExternalENSNamesRequest = (owner: string) => action(FETCH_EXTERNAL_ENS_NAMES_REQUEST, { owner })
+export const fetchExternalENSNamesSuccess = (owner: string, names: string[]) => action(FETCH_EXTERNAL_ENS_NAMES_SUCCESS, { owner, names })
+export const fetchExternalENSNamesFailure = (owner: string, error: ENSError) => action(FETCH_EXTERNAL_ENS_NAMES_FAILURE, { owner, error })
 
-export type FetchExternalENSNamesRequestAction = ReturnType<typeof fetchExternadlENSNamesRequest>
-export type FetchExternalENSNamesSuccessAction = ReturnType<typeof fetchExternadlENSNamesSuccess>
-export type FetchExternalENSNamesFailureAction = ReturnType<typeof fetchExternadlENSNamesFailure>
+export type FetchExternalENSNamesRequestAction = ReturnType<typeof fetchExternalENSNamesRequest>
+export type FetchExternalENSNamesSuccessAction = ReturnType<typeof fetchExternalENSNamesSuccess>
+export type FetchExternalENSNamesFailureAction = ReturnType<typeof fetchExternalENSNamesFailure>
