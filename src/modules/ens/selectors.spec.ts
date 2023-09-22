4f21463f471946d2903343371115cbd2cfd8129d
Nando <24811313+fzavalia@users.noreply.github.com>
2023-09-22 13:30:31 -0300

    fix: Test new selectors

---
+++ src/modules/ens/selectors.spec.ts
@@ -0,0 +1,87 @@
+import { WalletState } from 'decentraland-dapps/dist/modules/wallet/reducer'
+import { RootState } from 'modules/common/types'
+import { getExternalNames, getExternalNamesByWallet } from './selectors'
+import { ENSState } from './reducer'
+
+let state: RootState
+let wallet: string
+let externalNames: ENSState['externalNames']
+
+beforeEach(() => {
+  state = {} as RootState
+  wallet = '0x123'
+  externalNames = {
+    [wallet]: ['name1.eth', 'name2.eth']
+  }
+})
+
+describe('when getting the external names', () => {
+  beforeEach(() => {
+    state = {
+      ens: {
+        externalNames
+      } as ENSState
+    } as RootState
+  })
+
+  it('should return a record of names by address', () => {
+    expect(getExternalNames(state)).toEqual(externalNames)
+  })
+})
+
+describe('when getting the external names by wallet', () => {
+  describe('when the external names has an entry for the wallet', () => {
+    beforeEach(() => {
+      state = {
+        ens: {
+          externalNames
+        } as ENSState,
+        wallet: {
+          data: {
+            address: wallet
+          }
+        } as WalletState
+      } as RootState
+    })
+
+    it('should return the names of the wallet', () => {
+      expect(getExternalNamesByWallet(state)).toEqual(externalNames[wallet])
+    })
+  })
+
+  describe('when the external names does not have an entry for the wallet', () => {
+    beforeEach(() => {
+      state = {
+        ens: {
+          externalNames: {}
+        } as ENSState,
+        wallet: {
+          data: {
+            address: wallet
+          }
+        } as WalletState
+      } as RootState
+    })
+
+    it('should return an empty array', () => {
+      expect(getExternalNamesByWallet(state)).toEqual([])
+    })
+  })
+
+  describe('when there is no wallet', () => {
+    beforeEach(() => {
+      state = {
+        ens: {
+          externalNames
+        } as ENSState,
+        wallet: {
+          data: null
+        } as WalletState
+      } as RootState
+    })
+
+    it('should return an empty array', () => {
+      expect(getExternalNamesByWallet(state)).toEqual([])
+    })
+  })
+})
