3c1a6ca5a653b0e4a32bb79080dce53b03ba8474
Nando <24811313+fzavalia@users.noreply.github.com>
2023-09-29 18:27:42 -0300

    fix: Add translations and cast from BigInt to Number

--- src/components/WorldListPage_WorldsForEnsOwnersFeature/WorldsStorage/WorldsStorage.tsx
+++ src/components/WorldListPage_WorldsForEnsOwnersFeature/WorldsStorage/WorldsStorage.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import classNames from 'classnames'
+import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { Props } from './WorldsStorage.types'
 import styles from './WorldsStorage.module.css'
-import classNames from 'classnames'
 
 const WorldsStorage = ({ maxBytes, currentBytes }: Props) => {
-  const maxMbs = BigInt(maxBytes) / BigInt(1000000)
-  const currentMbs = BigInt(currentBytes) / BigInt(1000000)
-  const usedPercentage = (currentMbs * BigInt(100)) / maxMbs
+  const maxMbs = Number(BigInt(maxBytes) / BigInt(1000000))
+  const currentMbs = Number(BigInt(currentBytes) / BigInt(1000000))
+  const usedPercentage = (currentMbs * 100) / maxMbs
 
   return (
     <div className={styles.worldsStorage}>
       <div className={styles.spaceContainer}>
-        <span>Space used</span>
+        <span>{t('worlds_list_page.worlds_storage.space_used')}</span>
         <div>
-          <span className={styles.currentMbs}>{currentMbs.toString()}</span> / {maxMbs.toString()} mb
+          <span className={styles.currentMbs}>{currentMbs}</span> / {maxMbs} mb
         </div>
       </div>
       <div className={styles.barContainer}>
@@ -25,7 +26,7 @@ const WorldsStorage = ({ maxBytes, currentBytes }: Props) => {
           className={classNames(styles.bar, styles.barFront)}
         />
       </div>
-      <div className={styles.viewDetails}>view details</div>
+      <div className={styles.viewDetails}>{t('worlds_list_page.worlds_storage.view_details')}</div>
     </div>
   )
 }
