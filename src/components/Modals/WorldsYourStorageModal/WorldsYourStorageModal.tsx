d28442ab508a30d34116abba6ff551efa460b98c
Nando <24811313+fzavalia@users.noreply.github.com>
2023-10-04 15:30:24 -0300

    feat: Fetch account holdings

--- src/components/Modals/WorldsYourStorageModal/WorldsYourStorageModal.tsx
+++ src/components/Modals/WorldsYourStorageModal/WorldsYourStorageModal.tsx
@@ -1,17 +1,40 @@
 import * as React from 'react'
 import Modal from 'decentraland-dapps/dist/containers/Modal'
 import { Button, ModalContent, ModalNavigation } from 'decentraland-ui'
-import { Props, WorldsYourStorageModalMetadata } from './WorldsYourStorageModal.types'
+import { Props, State, WorldsYourStorageModalMetadata } from './WorldsYourStorageModal.types'
 import { fromBytesToMegabytes } from 'components/WorldListPage_WorldsForEnsOwnersFeature/utils'
 import goodImg from './images/good.svg'
 import styles from './WorldsYourStorageModal.module.css'
 import { InfoIcon } from 'components/InfoIcon'
+import { fetchAccountHoldings, getMbsFromAccountHoldings } from './utils'
 
-export default class WorldsYourStorageModal extends React.PureComponent<Props> {
+export default class WorldsYourStorageModal extends React.PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props)
+
+    this.state = {
+      accountHoldings: null
+    }
+  }
+
+  async componentDidMount() {
+    const { metadata } = this.props
+
+    const { stats } = metadata as WorldsYourStorageModalMetadata
+
+    const accountHoldings = await fetchAccountHoldings(stats.wallet)
+
+    this.setState({ accountHoldings })
+  }
+
   render() {
     const { name, onClose, metadata } = this.props
 
     const { stats } = metadata as WorldsYourStorageModalMetadata
+
+    const { accountHoldings } = this.state
+
+    const mbsFromAccountHoldings = accountHoldings ? getMbsFromAccountHoldings(accountHoldings) : null
 
     return (
       <Modal name={name} onClose={onClose}>
@@ -27,9 +50,12 @@ export default class WorldsYourStorageModal extends React.PureComponent<Props> {
             <div className={styles.texts}>
               <span>MANA</span>
               <span className={styles.subtitle}>Earn 100 MB of storage per 2,000 tokens (Polygon or Ethereum).</span>
-              <span className={styles.amount}>
-                <img src={goodImg} alt="good"></img> You have 100 MB thanks to holding 2728 MANA tokens.
-              </span>
+              {accountHoldings && mbsFromAccountHoldings ? (
+                <span className={styles.amount}>
+                  <img src={goodImg} alt="good"></img> You have {Math.trunc(mbsFromAccountHoldings.manaMbs)} MB thanks to holding{' '}
+                  {Math.trunc(accountHoldings.ownedMana)} MANA tokens.
+                </span>
+              ) : null}
             </div>
             <div>
               <Button primary>BUY MANA</Button>
@@ -38,27 +64,33 @@ export default class WorldsYourStorageModal extends React.PureComponent<Props> {
           <div className={styles.separator} />
           <div className={styles.asset}>
             <div className={styles.texts}>
-              <span>MANA</span>
-              <span className={styles.subtitle}>Earn 100 MB of storage per 2,000 tokens (Polygon or Ethereum).</span>
-              <span className={styles.amount}>
-                <img src={goodImg} alt="good"></img> You have 100 MB thanks to holding 2728 MANA tokens.
-              </span>
+              <span>LANDs</span>
+              <span className={styles.subtitle}>Earn 100 MB of storage per LAND.</span>
+              {accountHoldings && mbsFromAccountHoldings ? (
+                <span className={styles.amount}>
+                  <img src={goodImg} alt="good"></img> You have {mbsFromAccountHoldings.landMbs} MB thanks to holding{' '}
+                  {accountHoldings.ownedLands} LANDs.
+                </span>
+              ) : null}
             </div>
             <div>
-              <Button primary>BUY MANA</Button>
+              <Button primary>BUY LAND</Button>
             </div>
           </div>
           <div className={styles.separator} />
           <div className={styles.asset}>
             <div className={styles.texts}>
-              <span>MANA</span>
-              <span className={styles.subtitle}>Earn 100 MB of storage per 2,000 tokens (Polygon or Ethereum).</span>
-              <span className={styles.amount}>
-                <img src={goodImg} alt="good"></img> You have 100 MB thanks to holding 2728 MANA tokens.
-              </span>
+              <span>NAMEs</span>
+              <span className={styles.subtitle}>Earn 100 MB of storage per NAME.</span>
+              {accountHoldings && mbsFromAccountHoldings ? (
+                <span className={styles.amount}>
+                  <img src={goodImg} alt="good"></img> You have {mbsFromAccountHoldings.nameMbs} MB thanks to holding{' '}
+                  {Math.trunc(accountHoldings.ownedNames)} NAMEs.
+                </span>
+              ) : null}
             </div>
             <div>
-              <Button primary>BUY MANA</Button>
+              <Button primary>BUY NAME</Button>
             </div>
           </div>
           <div className={styles.proposal}>
--- src/components/Modals/WorldsYourStorageModal/WorldsYourStorageModal.types.ts
+++ src/components/Modals/WorldsYourStorageModal/WorldsYourStorageModal.types.ts
@@ -1,9 +1,14 @@
 import { Dispatch } from 'redux'
 import { ModalProps } from 'decentraland-dapps/dist/providers/ModalProvider/ModalProvider.types'
 import { WorldsWalletStats } from 'lib/api/worlds'
+import { AccountHoldings } from './utils'
 
 export type Props = ModalProps & {
   metadata: WorldsYourStorageModalMetadata
+}
+
+export type State = {
+  accountHoldings: AccountHoldings | null
 }
 
 export type WorldsYourStorageModalMetadata = {
