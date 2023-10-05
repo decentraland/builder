13ecf8fa1a33bee170f5b11d376f4d36057f4b6c
Nando <24811313+fzavalia@users.noreply.github.com>
2023-10-05 18:22:29 -0300

    fix: Add tests

--- src/modules/worlds/sagas.spec.ts
+++ src/modules/worlds/sagas.spec.ts
@@ -1,11 +1,14 @@
 import { expectSaga } from 'redux-saga-test-plan'
-import { call } from 'redux-saga/effects'
+import { call, select } from 'redux-saga/effects'
 import { throwError } from 'redux-saga-test-plan/providers'
 import { connectWalletSuccess } from 'decentraland-dapps/dist/modules/wallet/actions'
 import { Wallet } from 'decentraland-dapps/dist/modules/wallet/types'
 import { WorldsWalletStats, content } from 'lib/api/worlds'
 import { fetchWorldsWalletStatsFailure, fetchWorldsWalletStatsRequest, fetchWorldsWalletStatsSuccess } from './actions'
 import { worldsSaga } from './sagas'
+import { clearDeploymentSuccess, deployToWorldSuccess } from 'modules/deployment/actions'
+import { Deployment } from 'modules/deployment/types'
+import { getAddress } from 'decentraland-dapps/dist/modules/wallet/selectors'
 
 let address: string
 
@@ -69,5 +72,49 @@ describe('when the wallet connects', () => {
       .put(fetchWorldsWalletStatsRequest(address))
       .dispatch(connectWalletSuccess({ address } as Wallet))
       .silentRun()
+  })
+})
+
+describe('when handling the deploy to world success action', () => {
+  describe('when there is no address in the store', () => {
+    it('should not put the request action to fetch worlds wallet stats', () => {
+      return expectSaga(worldsSaga)
+        .provide([[select(getAddress), undefined]])
+        .not.put(fetchWorldsWalletStatsRequest(address))
+        .dispatch(deployToWorldSuccess({} as Deployment))
+        .silentRun()
+    })
+  })
+
+  describe('when there is an address in the store', () => {
+    it('should put the request action to fetch worlds wallet stats with the stored address', () => {
+      return expectSaga(worldsSaga)
+        .provide([[select(getAddress), address]])
+        .put(fetchWorldsWalletStatsRequest(address))
+        .dispatch(deployToWorldSuccess({} as Deployment))
+        .silentRun()
+    })
+  })
+})
+
+describe('when handling the clear deployment success action', () => {
+  describe('when there is no address in the store', () => {
+    it('should not put the request action to fetch worlds wallet stats', () => {
+      return expectSaga(worldsSaga)
+        .provide([[select(getAddress), undefined]])
+        .not.put(fetchWorldsWalletStatsRequest(address))
+        .dispatch(clearDeploymentSuccess(''))
+        .silentRun()
+    })
+  })
+
+  describe('when there is an address in the store', () => {
+    it('should put the request action to fetch worlds wallet stats with the stored address', () => {
+      return expectSaga(worldsSaga)
+        .provide([[select(getAddress), address]])
+        .put(fetchWorldsWalletStatsRequest(address))
+        .dispatch(clearDeploymentSuccess(''))
+        .silentRun()
+    })
   })
 })
