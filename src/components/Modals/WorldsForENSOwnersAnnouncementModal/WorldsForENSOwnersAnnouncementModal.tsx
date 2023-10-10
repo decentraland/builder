93f2fc63ebe32b169d6531c35571567ff37e99b4
Nando <24811313+fzavalia@users.noreply.github.com>
2023-10-10 17:44:42 -0300

    fix: Track worlds for ens announcement moal actions

--- src/components/Modals/WorldsForENSOwnersAnnouncementModal/WorldsForENSOwnersAnnouncementModal.tsx
+++ src/components/Modals/WorldsForENSOwnersAnnouncementModal/WorldsForENSOwnersAnnouncementModal.tsx
@@ -2,6 +2,7 @@ import React, { useCallback } from 'react'
 import { Button, ModalActions, ModalContent, ModalNavigation } from 'decentraland-ui'
 import Modal from 'decentraland-dapps/dist/containers/Modal'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
+import { track } from 'modules/analytics/sagas'
 import { Props } from './WorldsForENSOwnersAnnouncementModal.types'
 import { persistCanOpenWorldsForENSOwnersAnnouncementModal } from './utils'
 import ensImg from './images/ens.svg'
@@ -15,11 +16,25 @@ const WorldsForENSOwnersAnnouncementModal: React.FC<Props> = ({ name, onClose })
     onClose()
   }, [onClose])
 
+  const handleOnModalClose = useCallback(() => {
+    track('Worlds For ENS Owners Announcement Modal - Close Modal', {})
+    handleOnClose()
+  }, [handleOnClose])
+
+  const handleOnStartBuilding = useCallback(() => {
+    track('Worlds For ENS Owners Announcement Modal - Click Start Building', {})
+    handleOnClose()
+  }, [handleOnClose])
+
+  const handleOnLearnMore = useCallback(() => {
+    track('Worlds For ENS Owners Announcement Modal - Click Learn More', {})
+  }, [])
+
   return (
-    <Modal name={name} onClose={handleOnClose}>
+    <Modal name={name} onClose={handleOnModalClose}>
       <ModalNavigation
         title={<div className={styles.title}>{t('worlds_for_ens_owners_announcement_modal.title')}</div>}
-        onClose={handleOnClose}
+        onClose={handleOnModalClose}
       />
       <ModalContent>
         <div className={styles.img}>
@@ -35,10 +50,10 @@ const WorldsForENSOwnersAnnouncementModal: React.FC<Props> = ({ name, onClose })
         </div>
       </ModalContent>
       <ModalActions className={styles.actions}>
-        <Button primary onClick={handleOnClose}>
+        <Button primary onClick={handleOnStartBuilding}>
           {t('worlds_for_ens_owners_announcement_modal.start_building')}
         </Button>
-        <Button as="a" href={DOCUMENTATION_URL} target="_blank" rel="noopener noreferrer">
+        <Button as="a" href={DOCUMENTATION_URL} target="_blank" rel="noopener noreferrer" onClick={handleOnLearnMore}>
           {t('worlds_for_ens_owners_announcement_modal.learn_more')}
         </Button>
       </ModalActions>
