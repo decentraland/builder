3d0369742f627844bf9fecefdc81b1d99ad5334e
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2023-10-11 13:01:05 -0300

    fix: Add tracking to the UserInformation component (#2969)

--- src/components/UserInformation/UserInformation.tsx
+++ src/components/UserInformation/UserInformation.tsx
@@ -3,9 +3,9 @@ import { UserInformation as BaseUserMenu } from 'decentraland-dapps/dist/contain
 import { Props } from './UserInformation.types'
 
 const UserInformation = (props: Props) => {
-  const { onClickMyAssets, onClickSettings, onSignIn, ...baseProps } = props
+  const { onClickActivity, onClickSettings, onSignIn, ...baseProps } = props
 
-  return <BaseUserMenu {...baseProps} onClickMyAssets={onClickMyAssets} onClickSettings={onClickSettings} onSignIn={onSignIn} />
+  return <BaseUserMenu {...baseProps} onClickActivity={onClickActivity} onClickSettings={onClickSettings} onSignIn={onSignIn} />
 }
 
 export default React.memo(UserInformation)
