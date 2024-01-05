b5a666bf80fca44126094320e17500419d3cb523
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-01-05 17:24:18 +0100

    feat: Update navbar

--- src/components/Navbar/Navbar.types.ts
+++ src/components/Navbar/Navbar.types.ts
@@ -1,9 +1,9 @@
 import { NavbarProps } from 'decentraland-ui'
 import { Dispatch } from 'redux'
 
-export type Props = Partial<NavbarProps> & { isNewNavbarEnabled: boolean }
+export type Props = Partial<NavbarProps> & { isNewNavbarEnabled: boolean; isNavbarV2Enabled: boolean }
 
-export type MapStateProps = Pick<Props, 'isConnected' | 'isNewNavbarEnabled'>
+export type MapStateProps = Pick<Props, 'isConnected' | 'isNewNavbarEnabled' | 'isNavbarV2Enabled'>
 export type MapDispatchProps = Pick<Props, 'onSignIn'>
 export type MapDispatch = Dispatch
 export type OwnProps = Partial<Props>
