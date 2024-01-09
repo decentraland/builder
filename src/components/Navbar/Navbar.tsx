f6f7ac57a7f272b162c4c9eec45710a26fe2c7a4
Braian Mellor <braianj@gmail.com>
2024-01-09 16:50:47 -0300

    fix: navbar width (#3009)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,6 +5,8 @@ import { Navbar2Pages } from 'decentraland-ui/dist/components/Navbar2/Navbar2.ty
 import UserInformation from 'components/UserInformation'
 import { Props } from './Navbar.types'
 
+import './Navbar.css'
+
 const Navbar: React.FC<Props> = ({ hasPendingTransactions, isNavbarV2Enabled, ...props }: Props) => {
   return isNavbarV2Enabled ? (
     <BaseNavbar2 activePage={Navbar2Pages.CREATE} {...props} hasActivity={hasPendingTransactions} />
