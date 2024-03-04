0d4fb07d52874dd329206230201d36111d576481
Lautaro Petaccio <lausuper@gmail.com>
2024-03-04 13:30:10 -0300

    fix: Remove features from enum type

--- src/modules/features/types.ts
+++ src/modules/features/types.ts
@@ -1,14 +1,7 @@
 export enum FeatureName {
   MAINTENANCE = 'maintenance',
-  RENTALS = 'rentals',
   CAMPAIGN = 'campaign',
-  DCL_CONTROLLER_V2 = 'dcl-controller-v2',
-  NEW_NAVBAR_DROPDOWN = 'new-navbar-dropdown',
-  EMOTES_V2 = 'emotes-2.0',
-  SMART_ITEMS = 'smart-items',
-  WORLDS_FOR_ENS_OWNERS = 'worlds-for-ens-owners',
   SDK7_TEMPLATES = 'sdk7-templates',
   CREATE_SCENE_ONLY_SDK7 = 'create-scene-only-sdk7',
-  AUTH_DAPP = 'auth-dapp',
   ENS_ADDRESS = 'ens-address'
 }
