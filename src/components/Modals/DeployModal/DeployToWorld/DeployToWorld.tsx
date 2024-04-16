35304d66a5176de0ed208cc772c2f58b77a8caa8
Juan Cazala <juancazala@gmail.com>
2024-04-16 16:29:06 -0300

    fix: world urls (#3082)

--- src/components/Modals/DeployModal/DeployToWorld/DeployToWorld.tsx
+++ src/components/Modals/DeployModal/DeployToWorld/DeployToWorld.tsx
@@ -150,7 +150,7 @@ export default function DeployToWorld({
     if (isDevelopment) {
       return `${EXPLORER_URL}/?realm=${WORLDS_CONTENT_SERVER_URL}/world/${world}&NETWORK=sepolia`
     }
-    return `${EXPLORER_URL}/world/${world}`
+    return `${EXPLORER_URL}?realm=${world}`
   }, [world])
 
   const nameTypeOptions = useMemo(
