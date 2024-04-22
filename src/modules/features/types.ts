e3febccb7e75a3a532225c3e14446e270a91cf59
Lautaro Petaccio <lausuper@gmail.com>
2024-04-22 14:14:52 -0300

    fix: Remove exotic feature name

--- src/modules/features/types.ts
+++ src/modules/features/types.ts
@@ -5,6 +5,5 @@ export enum FeatureName {
   CREATE_SCENE_ONLY_SDK7 = 'create-scene-only-sdk7',
   ENS_ADDRESS = 'ens-address',
   PUBLISH_COLLECTIONS_WERT = 'publish-collections-wert',
-  EXOTIC_RARITY = 'exotic-rarity',
   VRM_OPTOUT = 'vrm-optout'
 }
