2d814f20c23443a39612f613164164dde7fd31a3
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2024-06-10 12:38:41 -0300

    fix: ens indefinitely loading (#3118)

--- src/components/ENSDetailPage/ENSDetailPage.container.ts
+++ src/components/ENSDetailPage/ENSDetailPage.container.ts
@@ -1,14 +1,15 @@
 import { connect } from 'react-redux'
 import { Dispatch } from 'redux'
+import { isLoadingType } from 'decentraland-dapps/dist/modules/loading/selectors'
+import { FETCH_ENS_REQUEST, fetchENSRequest } from 'modules/ens/actions'
+import { getAvatar, getName } from 'modules/profile/selectors'
+import { getWallet } from 'modules/wallet/selectors'
 import { openModal } from 'decentraland-dapps/dist/modules/modal/actions'
 import { getENSBySubdomain, getLoading } from 'modules/ens/selectors'
 import { RootState } from 'modules/common/types'
 import { getENSName } from 'modules/location/selectors'
 import { MapStateProps, MapDispatchProps } from './ENSDetailPage.types'
 import ENSDetailPage from './ENSDetailPage'
-import { isLoadingType } from 'decentraland-dapps/dist/modules/loading/selectors'
-import { FETCH_ENS_REQUEST, fetchENSRequest } from 'modules/ens/actions'
-import { getAvatar, getName } from 'modules/profile/selectors'
 
 const mapState = (state: RootState): MapStateProps => {
   const name = getENSName(state)
@@ -17,7 +18,8 @@ const mapState = (state: RootState): MapStateProps => {
     ens: name ? getENSBySubdomain(state, `${name}.dcl.eth`) : null,
     isLoading: isLoadingType(getLoading(state), FETCH_ENS_REQUEST),
     alias: getName(state),
-    avatar: getAvatar(state)
+    avatar: getAvatar(state),
+    wallet: getWallet(state)
   }
 }
 
