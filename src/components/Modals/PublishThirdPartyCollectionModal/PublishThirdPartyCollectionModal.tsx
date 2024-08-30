9d1c884bf5646d889e2d9f4de23250017735920a
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2024-08-30 16:45:30 -0300

    Feat: Improve the TP publishing modal (#3166)
    * feat: Add TP publish modal
    
    * feat: Improve TP publishing modal
    
    * fix: Restore builder site

--- src/components/Modals/PublishThirdPartyCollectionModal/PublishThirdPartyCollectionModal.tsx
+++ src/components/Modals/PublishThirdPartyCollectionModal/PublishThirdPartyCollectionModal.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react'
-import { ModalNavigation, Button, Progress } from 'decentraland-ui'
+import { ModalNavigation, Button, Icon, Loader } from 'decentraland-ui'
 import Modal from 'decentraland-dapps/dist/containers/Modal'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { getItemsToPublish, getItemsWithChanges } from 'modules/item/utils'
+import { PublishThirdPartyCollectionModalStep } from 'modules/ui/thirdparty/types'
 import { PublishButtonAction } from 'components/ThirdPartyCollectionDetailPage/CollectionPublishButton/CollectionPublishButton.types'
 import { getTPButtonActionLabel } from 'components/ThirdPartyCollectionDetailPage/CollectionPublishButton/CollectionPublishButton'
 import { Props } from './PublishThirdPartyCollectionModal.types'
+import styles from './PublishThirdPartyCollectionModal.module.css'
+import classNames from 'classnames'
 
 export default class PublishThirdPartyCollectionModal extends React.PureComponent<Props> {
   handleSubmit = () => {
@@ -32,6 +35,18 @@ export default class PublishThirdPartyCollectionModal extends React.PureComponen
       default:
         onPublish(thirdParty, items)
         break
+    }
+  }
+
+  getSubmittedItemsCount = () => {
+    const { items, itemsStatus, itemCurations } = this.props
+    return getItemsToPublish(items, itemsStatus).length + getItemsWithChanges(items, itemsStatus, itemCurations).length
+  }
+
+  handleViewForumPost = () => {
+    const { collection } = this.props
+    if (collection) {
+      window.open(collection.forumLink, '_blank', 'noopener noreferrer')
     }
   }
 
@@ -70,33 +85,57 @@ export default class PublishThirdPartyCollectionModal extends React.PureComponen
       isPublishLoading,
       onClose,
       pushChangesProgress,
-      metadata: { action }
+      metadata: { action, step }
     } = this.props
 
     return (
-      <Modal className="PublishThirdPartyCollectionModal" size="tiny" onClose={onClose}>
-        <ModalNavigation title={t('publish_third_party_collection_modal.title')} onClose={onClose} />
+      <Modal className="PublishThirdPartyCollectionModal" size="large" onClose={isPublishLoading ? undefined : onClose}>
+        <ModalNavigation title={''} onClose={isPublishLoading ? undefined : onClose} />
         <Modal.Content>
-          {isPublishLoading && [PublishButtonAction.PUSH_CHANGES, PublishButtonAction.PUBLISH_AND_PUSH_CHANGES].includes(action) ? (
+          <div className={styles.content}>
+            {step !== PublishThirdPartyCollectionModalStep.SUCCESS ? (
+              <>
+                <div className={classNames(styles.step, { [styles.visible]: isPublishLoading, [styles.hidden]: !isPublishLoading })}>
+                  <Loader active inline size="massive" />
+                  <div>% {pushChangesProgress}</div>
+                </div>
+                <h2>{t('publish_third_party_collection_modal.publishing_title', { count: this.getSubmittedItemsCount() })}</h2>
+                <div className={styles.description}>{this.getModalDescriptionText()}</div>
+              </>
+            ) : (
+              <>
+                <div className={styles.step}>
+                  <div className={styles.checkWrapper}>
+                    <Icon name="check circle" size="huge" className={styles.check} />
+                  </div>
+                </div>
+                <h2>{t('publish_third_party_collection_modal.success_title')}</h2>
+                <div className={styles.description}>{t('publish_third_party_collection_modal.success_description')}</div>
+              </>
+            )}
+          </div>
+        </Modal.Content>
+        <Modal.Actions>
+          {step !== PublishThirdPartyCollectionModalStep.SUCCESS ? (
             <>
-              <div className="progressBarContainer">
-                <Progress percent={pushChangesProgress} className="progressBar" progress />
-              </div>
+              <Button secondary disabled={isPublishLoading} onClick={onClose}>
+                {t('global.cancel')}
+              </Button>
+              <Button primary disabled={isPublishLoading} onClick={this.handleSubmit}>
+                {getTPButtonActionLabel(action)}
+              </Button>
             </>
           ) : (
             <>
-              <div>{this.getModalDescriptionText()}</div>
-              <br />
-              <Button primary fluid loading={isPublishLoading} onClick={this.handleSubmit}>
-                {getTPButtonActionLabel(action)}
+              <Button secondary disabled={isPublishLoading} onClick={this.handleViewForumPost}>
+                {t('publish_third_party_collection_modal.view_forum')}
               </Button>
-              <br />
-              <Button secondary fluid onClick={onClose}>
-                {t('global.cancel')}
+              <Button primary disabled={isPublishLoading} onClick={onClose}>
+                {t('publish_third_party_collection_modal.finish')}
               </Button>
             </>
           )}
-        </Modal.Content>
+        </Modal.Actions>
       </Modal>
     )
   }
--- src/modules/ui/thirdparty/types.ts
+++ src/modules/ui/thirdparty/types.ts
@@ -2,3 +2,8 @@ export enum ThirdPartyAction {
   PUSH_CHANGES,
   APPROVE_COLLECTION
 }
+
+export enum PublishThirdPartyCollectionModalStep {
+  PUBLISHING = 'publishing',
+  SUCCESS = 'success'
+}
