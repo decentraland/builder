56f1bc9f477208087c3009d1ff0eac8b9b66f81c
LautaroPetaccio <lausuper@gmail.com>
2024-11-12 15:04:15 -0300

    fix: Remove forced FF

--- src/modules/features/selectors.ts
+++ src/modules/features/selectors.ts
@@ -16,7 +16,7 @@ export const getIsMaintenanceEnabled = (state: RootState) => {
 
 export const getIsCampaignEnabled = (state: RootState) => {
   try {
-    return getIsFeatureEnabled(state, ApplicationName.BUILDER, FeatureName.CAMPAIGN) || true
+    return getIsFeatureEnabled(state, ApplicationName.BUILDER, FeatureName.CAMPAIGN)
   } catch (e) {
     return false
   }
