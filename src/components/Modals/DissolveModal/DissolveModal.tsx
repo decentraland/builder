22c3843591fc988bc37c9f955964a3ab39ce5596
Alejandra Levy <alelevy15@gmail.com>
2025-06-26 17:23:48 +0200

    Feat/revert wrong commit (#3276)
    * feat: Revert master commit
    
    * feat: Revert master commit

--- src/components/Modals/DissolveModal/DissolveModal.tsx
+++ src/components/Modals/DissolveModal/DissolveModal.tsx
@@ -3,6 +3,7 @@ import { Button, ModalContent, ModalActions, ModalHeader } from 'decentraland-ui
 import Modal from 'decentraland-dapps/dist/containers/Modal'
 import { t, T } from 'decentraland-dapps/dist/modules/translation/utils'
 
+import { MAX_PARCELS_PER_TX } from 'modules/land/utils'
 import { Props } from './DissolveModal.types'
 
 export default class DissolveModal extends React.PureComponent<Props> {
@@ -15,11 +16,17 @@ export default class DissolveModal extends React.PureComponent<Props> {
     const { name, metadata, onClose } = this.props
     const { land } = metadata
 
+    const isTooBig = land.parcels!.length > MAX_PARCELS_PER_TX
+
     return (
       <Modal name={name} onClose={onClose}>
         <ModalHeader>{t('dissolve_modal.title')}</ModalHeader>
         <ModalContent>
-          <T id="dissolve_modal.confirm" values={{ name: <strong>{land.name}</strong> }} />)
+          {isTooBig ? (
+            <T id="dissolve_modal.too_big" values={{ max: MAX_PARCELS_PER_TX }} />
+          ) : (
+            <T id="dissolve_modal.confirm" values={{ name: <strong>{land.name}</strong> }} />
+          )}
         </ModalContent>
         <ModalActions>
           <Button onClick={onClose}>{t('global.cancel')}</Button>
