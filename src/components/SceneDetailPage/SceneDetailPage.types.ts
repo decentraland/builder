6f6114f474deda9788ca9d5cdbc5876a61acf504
LautaroPetaccio <lausuper@gmail.com>
2025-08-04 10:13:24 -0300

    feat: Refactor SceneDetailPage container

--- src/components/SceneDetailPage/SceneDetailPage.types.ts
+++ src/components/SceneDetailPage/SceneDetailPage.types.ts
@@ -1,15 +1,7 @@
-import { Dispatch } from 'redux'
 import { Project } from 'modules/project/types'
+import { deleteProject, duplicateProjectRequest, loadProjectSceneRequest } from 'modules/project/actions'
+import { openModal } from 'decentraland-dapps/dist/modules/modal/actions'
 import { Deployment } from 'modules/deployment/types'
-import { openModal, OpenModalAction } from 'decentraland-dapps/dist/modules/modal/actions'
-import {
-  deleteProject,
-  duplicateProjectRequest,
-  DeleteProjectAction,
-  DuplicateProjectRequestAction,
-  loadProjectSceneRequest,
-  LoadProjectSceneRequestAction
-} from 'modules/project/actions'
 import { Scene } from 'modules/scene/types'
 
 export type Props = {
@@ -18,12 +10,8 @@ export type Props = {
   isLoading: boolean
   isLoadingDeployments: boolean
   scene: Scene | null
-  onOpenModal: typeof openModal
-  onDelete: typeof deleteProject
-  onDuplicate: typeof duplicateProjectRequest
-  onLoadProjectScene: typeof loadProjectSceneRequest
+  onOpenModal: ActionFunction<typeof openModal>
+  onDelete: ActionFunction<typeof deleteProject>
+  onDuplicate: ActionFunction<typeof duplicateProjectRequest>
+  onLoadProjectScene: ActionFunction<typeof loadProjectSceneRequest>
 }
-
-export type MapStateProps = Pick<Props, 'project' | 'deployments' | 'isLoading' | 'isLoadingDeployments' | 'scene'>
-export type MapDispatchProps = Pick<Props, 'onDelete' | 'onDuplicate' | 'onOpenModal' | 'onLoadProjectScene'>
-export type MapDispatch = Dispatch<DeleteProjectAction | DuplicateProjectRequestAction | OpenModalAction | LoadProjectSceneRequestAction>
