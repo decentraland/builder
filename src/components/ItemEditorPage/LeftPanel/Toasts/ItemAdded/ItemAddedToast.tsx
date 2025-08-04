9d77451783e6bcf92deaed85c0c719e04b6b9a6e
LautaroPetaccio <lausuper@gmail.com>
2025-08-04 10:21:18 -0300

    fix: Miscellaneous

--- src/components/ItemEditorPage/LeftPanel/Toasts/ItemAdded/ItemAddedToast.tsx
+++ src/components/ItemEditorPage/LeftPanel/Toasts/ItemAdded/ItemAddedToast.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { Button, Toast, ToastType } from 'decentraland-ui'
 import { Link, useHistory, useLocation } from 'react-router-dom'
-import { getNewItemName } from 'modules/item/selectors'
+import { getNewItemNameFromSearchParams } from 'modules/location/url-parsers'
 import { locations } from 'routing/locations'
 import { T, t } from 'decentraland-dapps/dist/modules/translation/utils'
 
@@ -16,7 +16,7 @@ const ItemAddedToast: React.FC<Props> = props => {
   const [item, setItem] = useState<string | null>(null)
   const history = useHistory()
   const location = useLocation()
-  const itemName = getNewItemName()
+  const itemName = useMemo(() => getNewItemNameFromSearchParams(location.search), [location.search])
 
   useEffect(() => {
     if (itemName) {
