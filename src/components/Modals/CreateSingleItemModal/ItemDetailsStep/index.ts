bbc2b13e2329255a4bdbc4fe873ad21ba44e8dc3
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-09-07 01:13:45 -0300

    feat: Refactor CreateSingleItemModal into smaller components

---
+++ src/components/Modals/CreateSingleItemModal/ItemDetailsStep/index.ts
@@ -0,0 +1,4 @@
+export { default as ItemDetailsStep } from './ItemDetailsStep'
+export { default as WearableDetails } from './WearableDetails'
+export { default as EmoteDetails } from './EmoteDetails'
+export { default as SmartWearableDetails } from './SmartWearableDetails'
--- src/components/Modals/CreateSingleItemModal/utils.ts
+++ src/components/Modals/CreateSingleItemModal/utils.ts
@@ -1,6 +1,20 @@
-import { BodyShape, EmoteCategory, EmoteWithBlobs, WearableCategory, WearableWithBlobs } from '@dcl/schemas'
+import {
+  BodyShape,
+  ContractAddress,
+  ContractNetwork,
+  EmoteCategory,
+  EmoteWithBlobs,
+  Mapping,
+  MappingType,
+  WearableCategory,
+  WearableWithBlobs
+} from '@dcl/schemas'
 import { ThumbnailType } from 'lib/getModelData'
+import { LinkedContract } from 'modules/thirdParty/types'
 import { isImageFile, isModelFile } from 'modules/item/utils'
+import { Collection } from 'modules/collection/types'
+import { BodyShapeType, THUMBNAIL_PATH, VIDEO_PATH, WearableRepresentation } from 'modules/item/types'
+import { SortedContent } from './CreateSingleItemModal.types'
 
 export const THUMBNAIL_WIDTH = 1024
 export const THUMBNAIL_HEIGHT = 1024
@@ -95,5 +109,182 @@ export function toEmoteWithBlobs({ contents, file }: { contents?: Record<string,
       ],
       loop: false
     }
+  }
+}
+
+/**
+ * Prefixes the content name by adding the adding the body shape name to it.
+ *
+ * @param bodyShape - The body shaped used to prefix the content name.
+ * @param contentKey - The content key or name to be prefixed.
+ */
+const prefixContentName = (bodyShape: BodyShapeType, contentKey: string): string => {
+  return `${bodyShape}/${contentKey}`
+}
+
+/**
+ * Creates a new contents record with the names of the contents blobs record prefixed.
+ * The names need to be prefixed so they won't collide with other
+ * pre-uploaded models. The name of the content is the name of the uploaded file.
+ *
+ * @param bodyShape - The body shaped used to prefix the content names.
+ * @param contents - The contents which keys are going to be prefixed.
+ */
+const prefixContents = (bodyShape: BodyShapeType, contents: Record<string, Blob>): Record<string, Blob> => {
+  return Object.keys(contents).reduce((newContents: Record<string, Blob>, key: string) => {
+    // Do not include the thumbnail, scenes, and video in each of the body shapes
+    if ([THUMBNAIL_PATH, VIDEO_PATH].includes(key)) {
+      return newContents
+    }
+    newContents[prefixContentName(bodyShape, key)] = contents[key]
+    return newContents
+  }, {})
+}
+
+/**
+ * Sorts the content into "male", "female" and "all" taking into consideration the body shape.
+ * All contains the item thumbnail and both male and female representations according to the shape.
+ * If the body representation is male, "female" will be an empty object and viceversa.
+ *
+ * @param bodyShape - The body shaped used to sort the content.
+ * @param contents - The contents to be sorted.
+ */
+export const sortContent = (bodyShape: BodyShapeType, contents: Record<string, Blob>): SortedContent => {
+  const male = bodyShape === BodyShapeType.BOTH || bodyShape === BodyShapeType.MALE ? prefixContents(BodyShapeType.MALE, contents) : {}
+  const female =
+    bodyShape === BodyShapeType.BOTH || bodyShape === BodyShapeType.FEMALE ? prefixContents(BodyShapeType.FEMALE, contents) : {}
+
+  const all: Record<string, Blob> = {
+    [THUMBNAIL_PATH]: contents[THUMBNAIL_PATH],
+    ...male,
+    ...female
+  }
+
+  if (contents[VIDEO_PATH]) {
+    all[VIDEO_PATH] = contents[VIDEO_PATH]
+  }
+
+  return { male, female, all }
+}
+
+export const sortContentZipBothBodyShape = (bodyShape: BodyShapeType, contents: Record<string, Blob>): SortedContent => {
+  let male: Record<string, Blob> = {}
+  let female: Record<string, Blob> = {}
+  const both: Record<string, Blob> = {}
+
+  for (const [key, value] of Object.entries(contents)) {
+    if (key.startsWith('male/') && (bodyShape === BodyShapeType.BOTH || bodyShape === BodyShapeType.MALE)) {
+      male[key] = value
+    } else if (key.startsWith('female/') && (bodyShape === BodyShapeType.BOTH || bodyShape === BodyShapeType.FEMALE)) {
+      female[key] = value
+    } else {
+      both[key] = value
+    }
+  }
+
+  male = {
+    ...male,
+    ...(bodyShape === BodyShapeType.BOTH || bodyShape === BodyShapeType.MALE ? prefixContents(BodyShapeType.MALE, both) : {})
+  }
+
+  female = {
+    ...female,
+    ...(bodyShape === BodyShapeType.BOTH || bodyShape === BodyShapeType.FEMALE ? prefixContents(BodyShapeType.FEMALE, both) : {})
+  }
+
+  const all = {
+    [THUMBNAIL_PATH]: contents[THUMBNAIL_PATH],
+    ...male,
+    ...female
+  }
+
+  return { male, female, all }
+}
+
+export const buildRepresentations = (bodyShape: BodyShapeType, model: string, contents: SortedContent): WearableRepresentation[] => {
+  const representations: WearableRepresentation[] = []
+
+  // add male representation
+  if (bodyShape === BodyShapeType.MALE || bodyShape === BodyShapeType.BOTH) {
+    representations.push({
+      bodyShapes: [BodyShape.MALE],
+      mainFile: prefixContentName(BodyShapeType.MALE, model),
+      contents: Object.keys(contents.male),
+      overrideHides: [],
+      overrideReplaces: []
+    })
+  }
+
+  // add female representation
+  if (bodyShape === BodyShapeType.FEMALE || bodyShape === BodyShapeType.BOTH) {
+    representations.push({
+      bodyShapes: [BodyShape.FEMALE],
+      mainFile: prefixContentName(BodyShapeType.FEMALE, model),
+      contents: Object.keys(contents.female),
+      overrideHides: [],
+      overrideReplaces: []
+    })
+  }
+
+  return representations
+}
+
+export const buildRepresentationsZipBothBodyshape = (bodyShape: BodyShapeType, contents: SortedContent): WearableRepresentation[] => {
+  const representations: WearableRepresentation[] = []
+
+  // add male representation
+  if (bodyShape === BodyShapeType.MALE || bodyShape === BodyShapeType.BOTH) {
+    representations.push({
+      bodyShapes: [BodyShape.MALE],
+      mainFile: Object.keys(contents.male).find(content => content.includes('glb'))!,
+      contents: Object.keys(contents.male),
+      overrideHides: [],
+      overrideReplaces: []
+    })
+  }
+
+  // add female representation
+  if (bodyShape === BodyShapeType.FEMALE || bodyShape === BodyShapeType.BOTH) {
+    representations.push({
+      bodyShapes: [BodyShape.FEMALE],
+      mainFile: Object.keys(contents.female).find(content => content.includes('glb'))!,
+      contents: Object.keys(contents.female),
+      overrideHides: [],
+      overrideReplaces: []
+    })
+  }
+
+  return representations
+}
+
+/**
+ * Gets the default mappings for a third party contract
+ */
+export const getDefaultMappings = (
+  contract: LinkedContract | undefined,
+  isThirdPartyV2Enabled: boolean
+): Partial<Record<ContractNetwork, Record<ContractAddress, Mapping[]>>> | undefined => {
+  if (!isThirdPartyV2Enabled || !contract) {
+    return undefined
+  }
+
+  return {
+    [contract.network]: {
+      [contract.address]: [{ type: MappingType.ANY }]
+    }
+  }
+}
+
+/**
+ * Gets the linked contract from a collection
+ */
+export const getLinkedContract = (collection: Collection | undefined | null): LinkedContract | undefined => {
+  if (!collection?.linkedContractAddress || !collection?.linkedContractNetwork) {
+    return undefined
+  }
+
+  return {
+    address: collection.linkedContractAddress,
+    network: collection.linkedContractNetwork
   }
 }
