9baf167a4988b555d434c72683bee8285da5f520
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-09-26 18:50:49 -0300

    fix: Animation data

--- src/components/ItemProvider/ItemProvider.types.ts
+++ src/components/ItemProvider/ItemProvider.types.ts
@@ -1,5 +1,13 @@
+import { AnimationClip, Object3D } from 'three'
 import { Item } from 'modules/item/types'
 import { Collection } from 'modules/collection/types'
+
+export type AnimationData = {
+  animations: AnimationClip[]
+  armatures: Object3D[]
+  isLoaded: boolean
+  error?: string
+}
 
 export type Props = {
   item: Item | null
@@ -9,10 +17,11 @@ export type Props = {
   id: string | null
   onFetchItem: (id: string) => void
   onFetchCollection: (id: string) => void
-  children: (item: Item | null, collection: Collection | null, isLoading: boolean) => React.ReactNode
+  children: (item: Item | null, collection: Collection | null, isLoading: boolean, animationData: AnimationData) => React.ReactNode
 }
 export type State = {
   loadedItemId: string | undefined
+  animationData: AnimationData
 }
 
 export type ContainerProps = Pick<Props, 'id' | 'children'>
