45b6210a0462a00161ab63d19c37ef088d907c55
Braian Mellor <braianj@gmail.com>
2025-12-18 11:49:34 -0300

    feat: remove process from node.ts

--- src/node.ts
+++ src/node.ts
@@ -1,9 +1,6 @@
 import buffer from 'buffer'
-import process from 'process'
 
 const Buffer = buffer.Buffer
 
 ;(globalThis as any).Buffer = Buffer
-;(globalThis as any).process = process
 ;(window as any).Buffer = Buffer
-;(window as any).process = process
