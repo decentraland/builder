19104d625cb518271b646af498ebc2ece57f11f8
Braian Mellor <braianj@gmail.com>
2025-12-18 12:05:41 -0300

    fix: type

--- src/components/NetworkIcon/NetworkIcon.tsx
+++ src/components/NetworkIcon/NetworkIcon.tsx
@@ -2,5 +2,5 @@ import { Props } from './NetworkIcon.types'
 import { imgSrcByNetwork, NETWORK_ICON_DATA_TEST_ID } from './utils'
 
 export const NetworkIcon = (props: Props) => (
-  <img data-testid={NETWORK_ICON_DATA_TEST_ID} src={imgSrcByNetwork[props.network]} alt={props.network} className={props.className} />
+  <img data-testid={NETWORK_ICON_DATA_TEST_ID} src={imgSrcByNetwork[props.network as keyof typeof imgSrcByNetwork]} alt={props.network} className={props.className} />
 )
