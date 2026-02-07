178ddcb35ca061a7834f7dd512e9cc17bb7b0e9d
LautaroPetaccio <lausuper@gmail.com>
2026-02-07 16:53:46 -0300

    feat: Micro fron end implementation

---
+++ src/bootstrap.tsx
@@ -0,0 +1,47 @@
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { Router } from 'react-router-dom'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
+import ModalProvider from 'decentraland-dapps/dist/providers/ModalProvider'
+import ToastProvider from 'decentraland-dapps/dist/providers/ToastProvider'
+import TranslationProvider from 'decentraland-dapps/dist/providers/TranslationProvider'
+import { Web2TransactionModal } from 'decentraland-dapps/dist/containers'
+import WalletProvider from 'decentraland-dapps/dist/providers/WalletProvider'
+import { darkTheme, DclThemeProvider } from 'decentraland-ui2'
+
+import { store, history } from 'modules/common/store'
+import * as modals from 'components/Modals'
+import * as languages from 'modules/translation/languages'
+import Routes from 'routing'
+
+import './modules/analytics/track'
+import './modules/analytics/sentry'
+import './themes'
+import './index.css'
+
+const container = document.getElementById('root')
+if (!container) throw new Error('Failed to find the root element')
+const root = createRoot(container)
+
+root.render(
+  <Provider store={store}>
+    <DndProvider backend={HTML5Backend}>
+      <TranslationProvider locales={Object.keys(languages)}>
+        <WalletProvider>
+          <Router history={history}>
+            <DclThemeProvider theme={darkTheme}>
+              <ToastProvider>
+                <ModalProvider components={modals}>
+                  <Routes />
+                </ModalProvider>
+              </ToastProvider>
+              <Web2TransactionModal />
+            </DclThemeProvider>
+          </Router>
+        </WalletProvider>
+      </TranslationProvider>
+    </DndProvider>
+  </Provider>
+)
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,60 @@ import react from '@vitejs/plugin-react-swc'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
+import { federation } from '@module-federation/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const envVariables = loadEnv(mode, process.cwd())
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return {
-    plugins: [react(), nodePolyfills()],
+    plugins: [
+      react(),
+      nodePolyfills(),
+      federation({
+        name: 'builder',
+        filename: 'remoteEntry.js',
+        manifest: true,
+        dts: false,
+        exposes: {
+          './App': './src/MFApp.tsx',
+        },
+        shared: {
+          // Core React
+          react: { singleton: true },
+          'react-dom': { singleton: true },
+          // Routing (v5)
+          'react-router': { singleton: true },
+          'react-router-dom': { singleton: true },
+          history: { singleton: true },
+          // Decentraland UI
+          'decentraland-ui': { singleton: true },
+          'decentraland-ui2': { singleton: true },
+          'semantic-ui-css': { singleton: true },
+          // Decentraland core libraries
+          'decentraland-dapps': { singleton: true },
+          'decentraland-connect': { singleton: true },
+          'decentraland-transactions': { singleton: true },
+          'decentraland-crypto-fetch': { singleton: true },
+          '@dcl/single-sign-on-client': { singleton: true },
+          '@dcl/crypto': { singleton: true },
+          'dcl-catalyst-client': { singleton: true },
+          // State management
+          'react-redux': { singleton: true },
+          'redux-saga': { singleton: true },
+          reselect: { singleton: true },
+          // Large shared libraries
+          ethers: { singleton: true },
+          thirdweb: { singleton: true },
+          '@sentry/react': { singleton: true },
+          'lottie-react': { singleton: true },
+          // Utilities
+          classnames: { singleton: true },
+          'date-fns': { singleton: true },
+          buffer: { singleton: true },
+        },
+      }),
+    ],
     // Required because the CatalystClient tries to access it
     define: {
       // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -39,6 +86,8 @@ export default defineConfig(({ mode }) => {
       }
     },
     server: {
+      port: 3002,
+      origin: 'http://localhost:3002',
       proxy: {
         // eslint-disable-next-line @typescript-eslint/naming-convention
         '/auth': {
@@ -59,6 +108,7 @@ export default defineConfig(({ mode }) => {
       }
     },
     build: {
+      target: 'chrome89',
       commonjsOptions: {
         transformMixedEsModules: true
       },
