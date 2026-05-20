6b9142fc5ccf8ce9c0eeeefaf14ed99b6bf3b3b8
Rocío Mena <rociocmena@gmail.com>
2026-05-20 11:18:19 -0300

    Remove placeholder logos

--- src/components/NetworkIcon/utils.ts
+++ src/components/NetworkIcon/utils.ts
@@ -4,18 +4,9 @@ import polygonSvg from '../../icons/polygon.svg'
 
 export const NETWORK_ICON_DATA_TEST_ID = 'network-icon'
 
-// TODO: Replace ethereumSvg fallbacks below with dedicated icons once the
-// design team provides assets for Base, ApeChain, and Monad networks.
-// Tracked in: https://github.com/decentraland/builder/issues/TBD
 export const imgSrcByNetwork = {
   [ContractNetwork.MAINNET]: ethereumSvg,
   [ContractNetwork.MATIC]: polygonSvg,
   [ContractNetwork.SEPOLIA]: ethereumSvg,
-  [ContractNetwork.AMOY]: polygonSvg,
-  [ContractNetwork.BASE_MAINNET]: ethereumSvg,
-  [ContractNetwork.BASE_SEPOLIA]: ethereumSvg,
-  [ContractNetwork.APE_MAINNET]: ethereumSvg,
-  [ContractNetwork.APE_CALDERA]: ethereumSvg,
-  [ContractNetwork.MONAD_MAINNET]: ethereumSvg,
-  [ContractNetwork.MONAD_TESTNET]: ethereumSvg
+  [ContractNetwork.AMOY]: polygonSvg
 }
