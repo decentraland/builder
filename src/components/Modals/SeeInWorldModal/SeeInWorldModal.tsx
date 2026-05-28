adacb3f9ef42618971d0a132704278b2cb56e109
Braian Mellor <braianj@gmail.com>
2026-05-28 09:07:44 +0200

    feat: open desktop explorer via deep-link in see-in-world modal

--- src/components/Modals/SeeInWorldModal/SeeInWorldModal.tsx
+++ src/components/Modals/SeeInWorldModal/SeeInWorldModal.tsx
@@ -1,21 +1,55 @@
 import React from 'react'
 import classNames from 'classnames'
 import { Button, Header, ModalContent, ModalNavigation } from 'decentraland-ui'
+import { DownloadModal } from 'decentraland-ui2'
 import Modal from 'decentraland-dapps/dist/containers/Modal'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
-import { getExplorerURL } from 'modules/collection/utils'
+import { launchExplorerForCollection } from 'modules/collection/explorerDeepLink'
 import styles from './SeeInWorldModal.module.css'
 import { Props } from './SeeInWorldModal.types'
 
-export default class SeeInWorldModal extends React.PureComponent<Props> {
-  handleNavigateToExplorer = (x: number, y: number) => {
+const DOWNLOAD_URL = 'https://decentraland.org/download'
+
+type State = {
+  isDownloadOpen: boolean
+  isLaunching: boolean
+}
+
+export default class SeeInWorldModal extends React.PureComponent<Props, State> {
+  state: State = { isDownloadOpen: false, isLaunching: false }
+
+  handleNavigateToExplorer = async (x: number, y: number) => {
     const { metadata, onClose } = this.props
-    const url = getExplorerURL({ collectionId: metadata.collectionId, item_ids: metadata.itemIds, position: { x, y } })
-    window.open(url, '_blank,noreferrer')
-    onClose()
+    // The deep-link only injects an unreleased collection on a cold start of the desktop
+    // client; the item-set variant (legacy WITH_ITEMS) isn't supported by the modern explorer,
+    // so we bail and close on item-only invocations rather than launch a no-op preview.
+    if (!metadata.collectionId) {
+      onClose()
+      return
+    }
+
+    this.setState({ isLaunching: true })
+    const launched = await launchExplorerForCollection({ collectionId: metadata.collectionId, position: { x, y } })
+    this.setState({ isLaunching: false })
+
+    if (launched) {
+      onClose()
+    } else {
+      this.setState({ isDownloadOpen: true })
+    }
+  }
+
+  handleCloseDownload = () => {
+    this.setState({ isDownloadOpen: false })
+    this.props.onClose()
+  }
+
+  handleDownloadClick = () => {
+    window.open(DOWNLOAD_URL, '_blank', 'noopener,noreferrer')
   }
 
   renderOption = (title: string, subtitle: string, position: { x: number; y: number }, imageClass: string) => {
+    const { isLaunching } = this.state
     return (
       <div className={styles.option}>
         <Header className={styles.headers} as="h2">
@@ -27,7 +61,7 @@ export default class SeeInWorldModal extends React.PureComponent<Props> {
         <div className={styles.imageWrapper}>
           <div className={classNames(styles.image, imageClass)}></div>
         </div>
-        <Button primary fluid size="small" onClick={() => this.handleNavigateToExplorer(position.x, position.y)}>
+        <Button primary fluid size="small" disabled={isLaunching} onClick={() => this.handleNavigateToExplorer(position.x, position.y)}>
           {t('see_in_world_modal.jump_in')}
         </Button>
       </div>
@@ -36,27 +70,38 @@ export default class SeeInWorldModal extends React.PureComponent<Props> {
 
   render() {
     const { onClose } = this.props
+    const { isDownloadOpen } = this.state
 
     return (
-      <Modal size="small" onClose={onClose} aria-modal role="dialog">
-        <ModalNavigation title={t('see_in_world_modal.title')} subtitle={t('see_in_world_modal.subtitle')} onClose={onClose} />
-        <ModalContent>
-          <div className={styles.content}>
-            {this.renderOption(
-              t('see_in_world_modal.empty_parcel.title'),
-              t('see_in_world_modal.empty_parcel.subtitle'),
-              { x: 150, y: -150 },
-              styles.emptyParcel
-            )}
-            {this.renderOption(
-              t('see_in_world_modal.genesis.title'),
-              t('see_in_world_modal.genesis.subtitle'),
-              { x: 0, y: 0 },
-              styles.genesis
-            )}
-          </div>
-        </ModalContent>
-      </Modal>
+      <>
+        <Modal size="small" onClose={onClose} aria-modal role="dialog">
+          <ModalNavigation title={t('see_in_world_modal.title')} subtitle={t('see_in_world_modal.subtitle')} onClose={onClose} />
+          <ModalContent>
+            <div className={styles.content}>
+              {this.renderOption(
+                t('see_in_world_modal.empty_parcel.title'),
+                t('see_in_world_modal.empty_parcel.subtitle'),
+                { x: 150, y: -150 },
+                styles.emptyParcel
+              )}
+              {this.renderOption(
+                t('see_in_world_modal.genesis.title'),
+                t('see_in_world_modal.genesis.subtitle'),
+                { x: 0, y: 0 },
+                styles.genesis
+              )}
+            </div>
+          </ModalContent>
+        </Modal>
+        <DownloadModal
+          open={isDownloadOpen}
+          onClose={this.handleCloseDownload}
+          title={t('see_in_world_modal.download.title')}
+          description={t('see_in_world_modal.download.description')}
+          buttonLabel={t('see_in_world_modal.download.button')}
+          onDownloadClick={this.handleDownloadClick}
+        />
+      </>
     )
   }
 }
